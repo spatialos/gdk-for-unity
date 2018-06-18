523857d94041ce52c60885a8d6307d3f2d0a5bc2
Jamie Brynes <jamiebrynes@improbable.io>
2018-06-18 15:29:15 +0100

    add docs linter

---
+++ tools/DocsLinter/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Markdig;
+
+namespace DocsLinter
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Contains("--help") || args.Contains("/?") || args.Contains("/help"))
+            {
+                Console.WriteLine(
+                    "A cross-platform helper that lints markdown documents in the current working directory.");
+                Console.WriteLine("This linter currently supports checking local links and images.");
+                Environment.Exit(0);
+            }
+
+            try
+            {
+                var allFiles = Directory.GetFiles(Directory.GetCurrentDirectory(), "*.*", SearchOption.AllDirectories);
+                var markdownFiles = GetMarkdownFiles(allFiles);
+                var areLinksValid = true;
+                foreach (var markdownFilePair in markdownFiles)
+                {
+                    areLinksValid &=
+                        CheckMarkdownFile(markdownFilePair.Key, markdownFilePair.Value, markdownFiles);
+                }
+
+                Environment.Exit(areLinksValid ? 0 : 1);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.ToString());
+                if (ex.InnerException != null)
+                {
+                    Console.Error.WriteLine(ex.InnerException.ToString());
+                }
+
+                Environment.Exit(1);
+            }
+        }
+
+        private static bool CheckMarkdownFile(string markdownFilePath, SimplifiedMarkdownDoc markdownFileContents,
+            Dictionary<string, SimplifiedMarkdownDoc> markdownFiles)
+        {
+            var allLinksValid = true;
+            foreach (var link in markdownFileContents.Links)
+            {
+                if (link.LocalLink.HasValue)
+                {
+                    var localLink = link.LocalLink.Value;
+                    if (localLink.FilePath == null)
+                    {
+                        // Same file reference. Must have a heading! E.g. - "#documentation"
+                        if (!localLink.Heading.HasValue)
+                        {
+                            LogMissingLink(markdownFilePath, localLink);
+                            continue;
+                        }
+
+                        if (!markdownFileContents.Headings.Contains(localLink.Heading.Value))
+                        {
+                            allLinksValid = false;
+                            LogMissingLink(markdownFilePath, localLink);
+                        }
+                    }
+                    else
+                    {
+                        // Other file reference.
+                        var fullyQualifiedFilePath =
+                            Path.GetFullPath(Path.Combine(Path.GetDirectoryName(markdownFilePath), localLink.FilePath));
+
+                        if (localLink.Heading.HasValue)
+                        {
+                            // Other markdown file reference. Must have a heading. E.g - "../README.md" or "../README.md#documentation"
+                            SimplifiedMarkdownDoc otherMarkdownDoc;
+                            if (!markdownFiles.TryGetValue(fullyQualifiedFilePath,
+                                out otherMarkdownDoc))
+                            {
+                                allLinksValid = false;
+                                LogMissingLink(markdownFilePath, localLink);
+                                continue;
+                            }
+
+                            if (!otherMarkdownDoc.Headings.Contains(localLink.Heading.Value))
+                            {
+                                allLinksValid = false;
+                                LogMissingLink(markdownFilePath, localLink);
+                            }
+                        }
+                        else
+                        {
+                            // Other file reference. May be a markdown file or another file. E.g. - "../README.md" or "../../spatialos.json".
+                            if (!markdownFiles.ContainsKey(fullyQualifiedFilePath) &&
+                                !File.Exists(fullyQualifiedFilePath))
+                            {
+                                allLinksValid = false;
+                                LogMissingLink(markdownFilePath, localLink);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return allLinksValid;
+        }
+
+        private static void LogMissingLink(string markdownFilePath, Link.LocalReference localLink)
+        {
+            Console.WriteLine($"Error in {markdownFilePath}. The link: {localLink} is invalid");
+        }
+
+        private static Dictionary<string, SimplifiedMarkdownDoc> GetMarkdownFiles(string[] allFiles)
+        {
+            var markdownFiles = new Dictionary<string, SimplifiedMarkdownDoc>();
+
+            foreach (var filePath in allFiles)
+            {
+                if (Path.GetExtension(filePath) == ".md")
+                {
+                    var markdownDoc = Markdown.Parse(File.ReadAllText(filePath));
+                    markdownFiles.Add(Path.GetFullPath(filePath),
+                        new SimplifiedMarkdownDoc(markdownDoc));
+                }
+            }
+
+            return markdownFiles;
+        }
+    }
+}
---
+++ tools/DocsLinter/SimplifiedMarkdownDoc.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Markdig.Syntax;
+using Markdig.Syntax.Inlines;
+
+namespace DocsLinter
+{
+    public class SimplifiedMarkdownDoc
+    {
+        public List<Link> Links = new List<Link>();
+        public List<Heading> Headings = new List<Heading>();
+
+        public SimplifiedMarkdownDoc(MarkdownDocument markdownDoc)
+        {
+            foreach (var child in markdownDoc.Descendants())
+            {
+                if (child is LinkInline link)
+                {
+                    Links.Add(new Link(link));
+                }
+                else if (child is HeadingBlock heading)
+                {
+                    Headings.Add(new Heading(heading));
+                }
+            }
+        }
+    }
+
+    public class Link
+    {
+        public RemoteUrl? RemoteLink;
+        public LocalReference? LocalLink;
+
+        public Link(LinkInline linkInline)
+        {
+            if (linkInline.Url.StartsWith("http") || linkInline.Url.StartsWith("www"))
+            {
+                RemoteLink = new RemoteUrl
+                {
+                    Url = linkInline.Url,
+                };
+            }
+            else
+            {
+                LocalLink = new LocalReference(linkInline.Url);
+            }
+        }
+
+        public override string ToString()
+        {
+            if (RemoteLink.HasValue)
+            {
+                return RemoteLink.Value.Url;
+            }
+
+            if (LocalLink.HasValue)
+            {
+                return LocalLink.Value.ToString();
+            }
+
+            return "Empty link!";
+        }
+
+        public struct RemoteUrl
+        {
+            public string Url;
+        }
+
+        public struct LocalReference
+        {
+            public string FilePath;
+            public Heading? Heading;
+
+            public LocalReference(string path)
+            {
+                if (!path.Contains("#"))
+                {
+                    FilePath = path;
+                    Heading = null;
+                }
+                else
+                {
+                    if (path.StartsWith("#"))
+                    {
+                        FilePath = null;
+                        Heading = new Heading(path);
+                    }
+                    else
+                    {
+                        FilePath = path.Split('#')[0];
+                        Heading = new Heading(path.Remove(0, FilePath.Length));
+                    }
+                }
+            }
+
+            public override string ToString()
+            {
+                return $"{FilePath ?? ""}{Heading?.ToString() ?? ""}";
+            }
+        }
+    }
+
+    public struct Heading
+    {
+        private static Regex sanitizationRegex = new Regex("[^a-zA-Z -]");
+
+        public string Title;
+
+        public Heading(HeadingBlock headingBlock)
+        {
+            Title = "";
+            for (var inline = headingBlock.Inline.FirstChild; inline != null; inline = inline.NextSibling)
+            {
+                if (inline is CodeInline codeInline)
+                {
+                    Title += codeInline.Content;
+                }
+                else
+                {
+                    Title += inline.ToString();
+                }
+            }
+
+            SanitizeTitle();
+        }
+
+        public Heading(string heading)
+        {
+            Title = "";
+            for (var i = 0; i < heading.Length; i++)
+            {
+                if (heading[i] == '#')
+                {
+                    continue;
+                }
+
+                Title = heading.Substring(i, heading.Length - i);
+                SanitizeTitle();
+                break;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"#{Title}";
+        }
+
+        private void SanitizeTitle()
+        {
+            var sanitized = sanitizationRegex.Replace(Title.ToLower(), "");
+            Title = sanitized.Trim().Replace(" ", "-");
+        }
+    }
+}
