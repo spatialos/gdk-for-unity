1361f210ad69ece5035ceb8ea62aa6ebf0bbb485
Jamie Brynes <jamiebrynes@improbable.io>
2018-06-19 16:24:03 +0100

    add tests and building to CI

--- ci/build.sh
+++ ci/build.sh
@@ -10,6 +10,15 @@ PROJECT_DIR="$(pwd)"
 
 markStartOfBlock "$0"
 
+markStartOfBlock "Building Tools"
+"${MSBUILD}" tools/ImpNuget/ImpNuget.csproj //property:Configuration=Release //clp:ErrorsOnly //nologo //m 1>&2
+pushd "tools/DocsLinter"
+    "../../bin/ImpNuget/ImpNuget.exe"
+popd
+
+"${MSBUILD}" tools/DocsLinter/DocsLinter.csproj //property:Configuration=Release //clp:ErrorsOnly //nologo //m 1>&2
+markEndOfBlock "Building Tools"
+
 markStartOfBlock "Code Generation"
 spatial codegen
 markEndOfBlock "Code Generation"
