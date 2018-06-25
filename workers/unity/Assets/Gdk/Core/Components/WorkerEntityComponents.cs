57df2f25fe6179e4f5373764cdc5815efea240f9
sami <sami@improbable.io>
2018-06-25 18:20:25 +0100

    Added an attribute to remove non-codegen components without hard coding them

--- workers/unity/Assets/Gdk/Core/Components/WorkerEntityComponents.cs
+++ workers/unity/Assets/Gdk/Core/Components/WorkerEntityComponents.cs
@@ -2,20 +2,40 @@ using Unity.Entities;
 
 namespace Improbable.Gdk.Core
 {
+    /// <summary>
+    /// Component denoting a worker entity
+    /// </summary>
     public struct WorkerEntityTag : IComponentData
     {
     }
 
+    /// <summary>
+    /// Component added to the worker entity immediately after establishing a connection to a SpatialOS deployment
+    /// Removed immediately after disconnecting
+    /// </summary>
     public struct IsConnected : IComponentData
     {
     }
 
+    /// <summary>
+    /// Component added to the worker entity immediately after establishing a connection to a SpatialOS deployment
+    /// Removed at the end of the tick it was added
+    /// </summary>
+    [RemoveAtEndOfTick]
     public struct OnConnected : IComponentData
     {
     }
 
+    /// <summary>
+    /// Component added to the worker entity immediately after disconnecting from SpatialOS
+    /// Removed at the end of the tick it was added
+    /// </summary>
+    [RemoveAtEndOfTick]
     public struct OnDisconnected : ISharedComponentData
     {
+        /// <summary>
+        /// The reported reason for disconnecting
+        /// </summary>
         public string ReasonForDisconnect;
     }
 }
