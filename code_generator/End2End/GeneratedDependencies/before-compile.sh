07694f5f32368c91d57030b63540c5b24c24b855
Matt Young <matthewyoung@improbable.io>
2018-06-26 14:42:01 +0100

    Run generation commands directly in BeforeCompile Target so we can remove before-compile.sh

--- code_generator/End2End/GeneratedDependencies/before-compile.sh
+++
@@ -1,6 +0,0 @@
-ROOT=`dirname $0`
-
-spatial schema generate -i "${ROOT}/../schema" -L ast_json -o "${ROOT}/../schema_json_ast"
-spatial schema generate -i "${ROOT}/../schema" -L csharp -o "${ROOT}/Generated_Csharp_Worker"
-
-"${ROOT}/../../bin/Release/GdkCodeGenerator.exe" --json-dir "${ROOT}/../schema_json_ast" --output-dir "${ROOT}/Generated"
