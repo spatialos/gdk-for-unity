a00758f792917f457aa70d199681c8b92d9c180d
Martin Zlocha <zlocha.martin@gmail.com>
2018-07-03 16:18:58 +0200

    Fix reliance on origin being 0. (#56)

--- workers/unity/Assets/Playground/Scripts/Cubes/CubeMovementSystem.cs
+++ workers/unity/Assets/Playground/Scripts/Cubes/CubeMovementSystem.cs
@@ -21,17 +21,27 @@ namespace Playground
 
         private static Vector3 speed = new Vector3(2, 0, 0);
 
+        private Vector3 origin;
+
+        protected override void OnCreateManager(int capacity)
+        {
+            base.OnCreateManager(capacity);
+
+            var worker = WorkerRegistry.GetWorkerForWorld(World);
+            origin = worker.Origin;
+        }
+
         protected override void OnUpdate()
         {
             for (var i = 0; i < data.Length; i++)
             {
                 var rigidbodyComponent = data.Rigidbody[i];
-                if (rigidbodyComponent.position.x > 10)
+                if (rigidbodyComponent.position.x - origin.x > 10)
                 {
                     speed = new Vector3(-2, 0, 0);
                 }
 
-                if (rigidbodyComponent.position.x < -10)
+                if (rigidbodyComponent.position.x - origin.x < -10)
                 {
                     speed = new Vector3(2, 0, 0);
                 }
