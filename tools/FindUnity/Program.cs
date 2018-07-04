02ce6d0b155118f9a3895685747277ec2a65b710
Jared Hefty <jared@improbable.io>
2018-07-04 09:26:41 +0100

    Mac test

--- tools/FindUnity/Program.cs
+++ tools/FindUnity/Program.cs
@@ -6,23 +6,24 @@ namespace FindUnity
 {
     internal static class Program
     {
-        // If SpecialFolder.ProgramFiles returns the (x86) variant instead of "Program Files",
-        // check that "Prefer 32 bit" is unticked in the project's Build settings.
-        private static readonly string ProgramFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
-        private static readonly string UnityHubPath = Path.Combine(ProgramFiles, "Unity", "Hub");
-        private static readonly string UnityPath = Path.Combine(ProgramFiles, "Unity");
-        private static readonly string ImprobableUnityPath = Path.Combine(@"c:\", "Unity");
         private const string UnityHomeEnvVar = "UNITY_HOME";
 
         private static void Main(string[] args)
         {
+            // If SpecialFolder.ProgramFiles returns the (x86) variant instead of "Program Files",
+            // check that "Prefer 32 bit" is unticked in the project's Build settings.
+            var programFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+            var unityHubPath = Path.Combine(programFiles, "Unity", "Hub");
+            var unityPath = Path.Combine(programFiles, "Unity");
+            var improbableUnityRootPath = programFiles == "/Applications" ? "/Applications" : Path.Combine(@"c:\", "Unity");
+
             if (args.Contains("--help") || args.Contains("/?") || args.Contains("/help"))
             {
                 Console.Out.WriteLine("Looks for a path to Unity in the following locations, in order:");
                 Console.Out.WriteLine($"  1) The path set in the environment variable {UnityHomeEnvVar}");
-                Console.Out.WriteLine($"  2) The default installation path for the Unity Hub ({UnityHubPath})");
-                Console.Out.WriteLine($"  3) The default installation path for Unity ({UnityPath})");
-                Console.Out.WriteLine($"  4) An Improbable-specific installation path for Unity ({ImprobableUnityPath})");
+                Console.Out.WriteLine($"  2) The default installation path for the Unity Hub ({unityHubPath})");
+                Console.Out.WriteLine($"  3) The default installation path for Unity ({unityPath})");
+                Console.Out.WriteLine($"  4) An Improbable-specific installation path for Unity ({improbableUnityRootPath})");
                 Environment.Exit(0);
             }
 
@@ -48,28 +49,28 @@ namespace FindUnity
                 var projectVersion = File.ReadAllText(Path.Combine("ProjectSettings", "ProjectVersion.txt"))
                     .Remove(0, "m_EditorVersion:".Length).Trim();
 
-                var hubPath = Path.Combine(UnityHubPath, "Editor", projectVersion);
+                var hubPath = Path.Combine(unityHubPath, "Editor", projectVersion);
                 if (Directory.Exists(hubPath))
                 {
                     Console.Out.WriteLine(hubPath);
                     Environment.Exit(0);
                 }
 
-                if (Directory.Exists(Path.Combine(UnityPath, "Editor")))
+                if (Directory.Exists(Path.Combine(unityPath, "Editor")))
                 {
                     // The UnityPath may contain other folders like "Hub", so sanity check that there's an Editor folder in it.
-                    Console.Out.WriteLine(UnityPath);
+                    Console.Out.WriteLine(unityPath);
                     Environment.Exit(0);
                 }
 
-                var improbableUnityPath = Path.Combine(ImprobableUnityPath, $"Unity-{projectVersion}");
+                var improbableUnityPath = Path.Combine(improbableUnityRootPath, $"Unity-{projectVersion}");
                 if (Directory.Exists(improbableUnityPath))
                 {
                     Console.Out.WriteLine(improbableUnityPath);
                     Environment.Exit(0);
                 }
 
-                throw new Exception($"Could not find Unity in\n  {UnityHomeEnvVar}={unityHome}\n  {hubPath}\n  {UnityPath}");
+                throw new Exception($"Could not find Unity in\n  {UnityHomeEnvVar}={unityHome}\n  {hubPath}\n  {unityPath}");
             }
             catch (Exception e)
             {
