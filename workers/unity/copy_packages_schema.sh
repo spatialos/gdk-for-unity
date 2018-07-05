e5c461dd10cf1d01024d0ef9054a224af59217f9
Matt Young <matthewyoung@improbable.io>
2018-07-05 17:06:42 +0100

    Move schema copying to tools project, actually use manifest.json to find packages

--- workers/unity/copy_packages_schema.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd "$(dirname "$0")"
-
-rm -Rf ../../schema/from_gdk_packages
-
-mkdir ../../schema/from_gdk_packages
-
-[[ -n `find -name "*.schema" -print -quit` ]] && cp `find -name "*.schema"` ../../schema/from_gdk_packages
