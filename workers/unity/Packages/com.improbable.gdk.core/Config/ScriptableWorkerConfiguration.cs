ec78e6d6a38992614e2bf9ba12f7b66e377c3f7f
Matt Young <matthewyoung@improbable.io>
2018-07-09 12:01:17 +0100

    Move WorkerConfiguration to Assets/Config/.

--- workers/unity/Packages/com.improbable.gdk.core/Config/ScriptableWorkerConfiguration.cs
+++ workers/unity/Packages/com.improbable.gdk.core/Config/ScriptableWorkerConfiguration.cs
@@ -5,7 +5,7 @@ namespace Improbable.Gdk.Core
 {
     public class ScriptableWorkerConfiguration : ScriptableObject
     {
-        public static readonly string AssetPath = "Assets/ScriptableWorkerConfiguration.asset";
+        public static readonly string AssetPath = "Assets/Config/ScriptableWorkerConfiguration.asset";
 
         public List<WorkerConfiguration> WorkerConfigurations = new List<WorkerConfiguration>();
 
