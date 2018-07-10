eaf3fffc7de8b2761a38fe141fe7a14ed3ef30aa
Jamie Brynes <jamiebrynes@improbable.io>
2018-07-10 17:52:38 +0100

    update packages

--- pull-worker-packages.sh
+++ pull-worker-packages.sh
@@ -6,9 +6,10 @@ cd "$(dirname "$0")"
 
 source ci/includes/pinned-tools.sh
 
-WORKER_BUILD_SNAPSHOT="14.0-b5142-2899b-WORKER-SNAPSHOT"
+WORKER_BUILD_SNAPSHOT="14.0-b5171-2d1a5-WORKER-SNAPSHOT"
 
 CORE_API_DIR="$(pwd)/workers/unity/Assets/Plugins/Improbable/Core"
+SDK_DIR="$(pwd)/workers/unity/Assets/Plugins/Improbable/Sdk"
 
 TEMP_DIR="$(pwd)/tmp/worker_sdk"
 
@@ -17,15 +18,16 @@ rm -rf ${TEMP_DIR}
 mkdir -p ${TEMP_DIR}
 
 spatial package retrieve "worker_sdk" "c-dynamic-x86_64-msvc_mt-win32" "${WORKER_BUILD_SNAPSHOT}" "${TEMP_DIR}/core-api-win"
-spatial package retrieve "worker_sdk" "c-dynamic-x86_64-clang_libcpp-macos" "${WORKER_BUILD_SNAPSHOT}" "${TEMP_DIR}/core-api-macos"
-spatial package retrieve "worker_sdk" "c-dynamic-x86_64-gcc_libstdcpp-linux" "${WORKER_BUILD_SNAPSHOT}" "${TEMP_DIR}/core-api-linux"
-
+spatial package retrieve "worker_sdk" "csharp_core" "${WORKER_BUILD_SNAPSHOT}" "${TEMP_DIR}/csharp_core"
 
 rm -rf ${CORE_API_DIR}
 mkdir -p ${CORE_API_DIR}
 
 unpackTo "${TEMP_DIR}/core-api-win" "${CORE_API_DIR}/Windows/x86_64/"
-unpackTo "${TEMP_DIR}/core-api-macos" "${CORE_API_DIR}/OSX/"
-unpackTo "${TEMP_DIR}/core-api-linux" "${CORE_API_DIR}/Linux/x86_64/"
+
+rm -rf ${SDK_DIR}
+mkdir -p ${SDK_DIR}
+
+unpackTo "${TEMP_DIR}/csharp_core" "${SDK_DIR}"
 
 rm -rf ${TEMP_DIR}
