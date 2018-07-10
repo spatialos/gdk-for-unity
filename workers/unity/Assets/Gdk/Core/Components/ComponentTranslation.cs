069aaa6f7cb2aa3157d23edabf0ba251dbe83525
Jamie Brynes <jamiebrynes@improbable.io>
2018-07-10 14:35:40 +0100

    start fixing stuff

--- workers/unity/Assets/Gdk/Core/Components/ComponentTranslation.cs
+++ workers/unity/Assets/Gdk/Core/Components/ComponentTranslation.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using Improbable.Worker;
+using Improbable.Worker.Core;
 using Unity.Entities;
 using UnityEngine;
 using Entity = Unity.Entities.Entity;
--- workers/unity/Assets/Gdk/Core/Utility/ConnectionUtility.cs
+++ workers/unity/Assets/Gdk/Core/Utility/ConnectionUtility.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using Improbable.Worker;
+using Improbable.Worker.Core;
 using UnityEngine;
 
 namespace Improbable.Gdk.Core
