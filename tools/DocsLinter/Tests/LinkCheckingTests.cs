07b2090f5831130b3edb15675547d7480f4c72f7
Jamie Brynes <jamiebrynes7@gmail.com>
2018-07-12 14:33:46 +0100

    use http

--- tools/DocsLinter/Tests/LinkCheckingTests.cs
+++ tools/DocsLinter/Tests/LinkCheckingTests.cs
@@ -16,7 +16,7 @@ namespace DocsLinter.Tests
 
         private static RemoteLink GetUrlForStatusCode(int statusCode)
         {
-            var url = $"https://httpstat.us/{statusCode}";
+            var url = $"http://httpstat.us/{statusCode}";
             return new RemoteLink(new LinkInline(url, ""));
         }
 
