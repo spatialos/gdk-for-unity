892ac6d35a575e88b8692f005026cc49fe174e32
Jamie Brynes <jamiebrynes@improbable.io>
2018-07-18 17:26:49 +0100

    add base classes
    edit codegen glue
    
    edit glue
    
    edit to glue
    
    PR feedback on base classes
    
    linting base classes

---
+++ workers/unity/Packages/com.improbable.gdk.core/CodegenAdapters/ComponentDispatcherHandler.cs
@@ -0,0 +1,24 @@
+using Improbable.Worker.Core;
+using Unity.Entities;
+
+namespace Improbable.Gdk.Core.CodegenAdapters
+{
+    public abstract class ComponentDispatcherHandler
+    {
+        public abstract uint ComponentId { get; }
+        public abstract void OnAddComponent(AddComponentOp op);
+        public abstract void OnRemoveComponent(RemoveComponentOp op);
+        public abstract void OnComponentUpdate(ComponentUpdateOp op);
+        public abstract void OnAuthorityChange(AuthorityChangeOp op);
+
+        public abstract void OnCommandRequest(CommandRequestOp op);
+        public abstract void OnCommandResponse(CommandResponseOp op);
+
+        protected EntityManager EntityManager;
+
+        protected ComponentDispatcherHandler(EntityManager entityManager)
+        {
+            EntityManager = entityManager;
+        }
+    }
+}
