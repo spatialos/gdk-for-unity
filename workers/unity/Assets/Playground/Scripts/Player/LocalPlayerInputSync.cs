722c25b388d5cfc89bb76b5b19898e0f6bd2b6de
Danail Penev <danail@improbable.io>
2018-07-23 17:21:55 +0100

    WIP Controllers

--- workers/unity/Assets/Playground/Scripts/Player/LocalPlayerInputSync.cs
+++ workers/unity/Assets/Playground/Scripts/Player/LocalPlayerInputSync.cs
@@ -2,6 +2,7 @@ using Generated.Playground;
 using Improbable.Gdk.Core;
 using Unity.Entities;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Playground
 {
@@ -16,7 +17,15 @@ namespace Playground
             public ComponentDataArray<Authoritative<SpatialOSPlayerInput>> PlayerInputAuthority;
         }
 
+        public VirtualJoystick VirtualJoystick;
+
         [Inject] private PlayerInputData playerInputData;
+
+        protected override void OnCreateManager(int capacity)
+        {
+            base.OnCreateManager(capacity);
+            VirtualJoystick = GameObject.FindGameObjectWithTag("GameController").GetComponent<VirtualJoystick>();
+        }
 
         protected override void OnUpdate()
         {
@@ -25,7 +34,11 @@ namespace Playground
                 var cameraTransform = playerInputData.CameraTransform[i];
                 var forward = cameraTransform.Rotation * Vector3.up;
                 var right = cameraTransform.Rotation * Vector3.right;
-                var input = Input.GetAxisRaw("Horizontal") * right + Input.GetAxisRaw("Vertical") * forward;
+                var input = Vector3.zero;
+                if (VirtualJoystick.InputDirection != Vector3.zero)
+                {
+                    input = VirtualJoystick.InputDirection.x * forward + VirtualJoystick.InputDirection.z * right;
+                }
 
                 var newPlayerInput = new SpatialOSPlayerInput
                 {
---
+++ workers/unity/Assets/Playground/Scripts/Player/VirtualJoystick.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class VirtualJoystick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+    private Image bgImg;
+    private Image joystickImg;
+
+    public Vector3 InputDirection { set; get; }
+
+    public void Start()
+    {
+        bgImg = GetComponent<Image>();
+        joystickImg = transform.GetChild(0).GetComponent<Image>();
+        InputDirection = Vector3.zero;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 pos = Vector2.zero;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(bgImg.rectTransform, eventData.position,
+            eventData.pressEventCamera, out pos))
+        {
+            pos.x = (pos.x / bgImg.rectTransform.sizeDelta.x);
+            pos.y = (pos.y / bgImg.rectTransform.sizeDelta.y);
+
+            float x = (bgImg.rectTransform.pivot.x == 1) ? pos.x * 2 + 1 : pos.x * 2 - 1;
+            float y = (bgImg.rectTransform.pivot.y == 1) ? pos.y * 2 + 1 : pos.y * 2 - 1;
+
+            InputDirection = new Vector3(x, 0, y);
+            InputDirection = (InputDirection.magnitude > 1) ? InputDirection.normalized : InputDirection;
+
+            joystickImg.rectTransform.anchoredPosition =
+                new Vector3(InputDirection.x * (bgImg.rectTransform.sizeDelta.x / 3),
+                    InputDirection.z * (bgImg.rectTransform.sizeDelta.y / 3));
+
+            Debug.Log(InputDirection);
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        InputDirection = Vector3.zero;
+        joystickImg.rectTransform.anchoredPosition = Vector3.zero;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        OnDrag(eventData);
+    }
+}
