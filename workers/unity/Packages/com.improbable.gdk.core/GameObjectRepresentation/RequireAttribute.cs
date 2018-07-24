ef7757bdfab2ffd7c8091b96c9401e310fff52da
Peter Mernyei <petermernyei@improbable.io>
2018-07-24 09:50:53 +0100

    Disallowed require attribute on properties

--- workers/unity/Packages/com.improbable.gdk.core/GameObjectRepresentation/RequireAttribute.cs
+++ workers/unity/Packages/com.improbable.gdk.core/GameObjectRepresentation/RequireAttribute.cs
@@ -5,7 +5,7 @@ namespace Improbable.Gdk.Core
     /// <summary>
     ///     Marks fields/properties of MonoBehaviours that require a Reader or Writer to be injected into them.
     /// </summary>
-    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    [AttributeUsage(AttributeTargets.Field)]
     public class RequireAttribute : Attribute
     {
     }
