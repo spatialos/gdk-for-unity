12da595dca1c0ea2474ab70bfa1fb23d37e358bc
Firtina O <firtina@improbable.io>
2018-07-24 11:00:35 +0100

    UTY-709 Add interfaces to be used by MonoBehaviours

--- workers/unity/Packages/com.improbable.gdk.core/Components/ISpatialComponentUpdate.cs
+++ workers/unity/Packages/com.improbable.gdk.core/Components/ISpatialComponentUpdate.cs
@@ -1,6 +1,13 @@
+using Unity.Entities;
+
 namespace Improbable.Gdk.Core
 {
     public interface ISpatialComponentUpdate
+    {
+    }
+
+    public interface ISpatialComponentUpdate<TComponent> : ISpatialComponentUpdate
+        where TComponent : ISpatialComponentData, IComponentData
     {
     }
 }
---
+++ workers/unity/Packages/com.improbable.gdk.core/GameObjectRepresentation/IReader.cs
@@ -0,0 +1,21 @@
+﻿using Improbable.Worker;
+using Unity.Entities;
+
+namespace Improbable.Gdk.Core.MonoBehaviours
+{
+    /// <summary>
+    /// The Reader interface is used by MonoBehaviours to query information about a SpatialOS component and register
+    /// callbacks for updates, events and commands for component instances.
+    /// </summary>
+    /// <typeparam name="TComponent">The data type for the SpatialOS component.</typeparam>
+    /// <typeparam name="TComponentUpdate">The data type for the SpatialOS component's update.</typeparam>
+    public interface IReader<TComponent, out TComponentUpdate>
+        where TComponent : ISpatialComponentData, IComponentData
+        where TComponentUpdate : ISpatialComponentUpdate<TComponent>
+    {
+        Authority Authority { get; }
+
+        event AuthorityChangedDelegate AuthorityChanged;
+        event ComponentUpdateDelegate<TComponentUpdate> ComponentUpdated;
+    }
+}
