456b1c3fb93406137fc7635f4ac5215faa55818b
Peter Mernyei <petermernyei@improbable.io>
2018-07-24 09:53:17 +0100

    Made sure comments are up to date too...

--- workers/unity/Packages/com.improbable.gdk.core/GameObjectRepresentation/RequireAttribute.cs
+++ workers/unity/Packages/com.improbable.gdk.core/GameObjectRepresentation/RequireAttribute.cs
@@ -3,7 +3,7 @@
 namespace Improbable.Gdk.Core
 {
     /// <summary>
-    ///     Marks fields/properties of MonoBehaviours that require a Reader or Writer to be injected into them.
+    ///     Marks fields of MonoBehaviours that require a Reader or Writer to be injected into them.
     /// </summary>
     [AttributeUsage(AttributeTargets.Field)]
     public class RequireAttribute : Attribute
