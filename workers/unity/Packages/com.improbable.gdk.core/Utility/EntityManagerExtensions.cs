0104eb86109d5e891d4e99522f2478ef27bc4d3d
Firtina O <firtina@improbable.io>
2018-07-27 09:19:09 +0100

    PR Feedback

--- workers/unity/Packages/com.improbable.gdk.core/Utility/EntityManagerExtensions.cs
+++ workers/unity/Packages/com.improbable.gdk.core/Utility/EntityManagerExtensions.cs
@@ -28,43 +28,21 @@ namespace Improbable.Gdk.Core
                     new Type[] { typeof(Entity), typeof(ComponentType), typeof(object) },
                     new ParameterModifier[] { });
 
-            ValidateParameters(setComponentObjectMethodInfo);
+            if (setComponentObjectMethodInfo == null)
+            {
+                throw new MissingMethodException(
+                    "Could not find the EntityManager.SetComponentObject(Entity, ComponentType, object) method.\n" +
+                    "Please ensure you are using a compatible version of the Unity.Entities package.");
+            }
 
             SetComponentObjectAction =
                 (SetComponentObjectDelegate) Delegate.CreateDelegate(typeof(SetComponentObjectDelegate),
                     setComponentObjectMethodInfo);
         }
 
-        [Conditional("UNITY_EDITOR")]
-        [Conditional("DEVELOPMENT_BUILD")]
-        private static void ValidateParameters(MethodInfo setComponentObjectMethodInfo)
-        {
-            const string errorMessage =
-                "Could not find the EntityManager.SetComponentObject(Entity, ComponentType, object) method.\n" +
-                "Please ensure you are using a compatible version of the Unity.Entities package.";
-
-            if (setComponentObjectMethodInfo == null)
-            {
-                throw new MissingMethodException(errorMessage);
-            }
-
-            var parameters = setComponentObjectMethodInfo.GetParameters();
-
-            if (parameters.Length == 3 &&
-                parameters[0].ParameterType == typeof(Entity) &&
-                parameters[1].ParameterType == typeof(ComponentType) &&
-                parameters[2].ParameterType == typeof(object))
-            {
-                // All good!
-                return;
-            }
-
-            throw new MissingMethodException(errorMessage);
-        }
-
         public static void SetComponentObject<T>(this EntityManager entityManager, Entity entity, T component)
         {
-            SetComponentObjectAction(entityManager, entity, typeof(T), component);
+            SetComponentObjectAction(entityManager, entity, ComponentType.Create<T>(), component);
         }
 
         public static void SetComponentObject(this EntityManager entityManager, Entity entity,
