9aaf0cd673dbf0c6d9962452f1c1974492bfe6fe
Firtina O <firtina@improbable.io>
2018-07-27 16:21:02 +0100

    Allow exception messags to be passed into Unity's logger properly and add logging tests

--- workers/unity/Packages/com.improbable.gdk.core/Logging/ForwardingDispatcher.cs
+++ workers/unity/Packages/com.improbable.gdk.core/Logging/ForwardingDispatcher.cs
@@ -45,7 +45,22 @@ namespace Improbable.Gdk.Core
 
         public void HandleLog(LogType type, LogEvent logEvent)
         {
-            Debug.unityLogger.Log(type, logEvent);
+            if (type == LogType.Exception)
+            {
+                // For exception types, Unity expects an exception object to be passed.
+                // Otherwise, it will not be displayed.
+                Exception exception = logEvent.Exception ?? new Exception(logEvent.ToString());
+
+                Debug.unityLogger.LogException(exception, logEvent.Context);
+            }
+            else
+            {
+                Debug.unityLogger.Log(
+                    logType: type,
+                    message: logEvent,
+                    context: logEvent.Context);
+            }
+
             LogLevel logLevel = LogTypeMapping[type];
 
             if (connection == null || logLevel < minimumLogLevel)
--- workers/unity/Packages/com.improbable.gdk.core/Logging/LogEvent.cs
+++ workers/unity/Packages/com.improbable.gdk.core/Logging/LogEvent.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,10 +9,15 @@ namespace Improbable.Gdk.Core
         public string Message;
         public Dictionary<string, object> Data;
 
+        public Exception Exception;
+        public UnityEngine.Object Context;
+
         public LogEvent(string message)
         {
             Message = message;
             Data = new Dictionary<string, object>();
+            Exception = null;
+            Context = null;
         }
 
         public LogEvent WithField(string key, object value)
@@ -20,14 +26,28 @@ namespace Improbable.Gdk.Core
             return this;
         }
 
+        public LogEvent WithContext(UnityEngine.Object context)
+        {
+            Context = context;
+            return this;
+        }
+
+        public LogEvent WithException(Exception exception)
+        {
+            Exception = exception;
+            return this;
+        }
+
         public override string ToString()
         {
             var builder = new StringBuilder();
 
-            builder.AppendLine(Message);
+            builder.Append(Message);
 
             if (Data.Count > 0)
             {
+                builder.AppendLine();
+                
                 builder.AppendLine("Log event context:");
 
                 foreach (var entry in Data)
