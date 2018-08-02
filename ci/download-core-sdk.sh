fcd93e67fee19918ee4baf7cc742a06df44c6ef9
Jamie Brynes <jamiebrynes7@gmail.com>
2018-08-02 13:46:57 +0100

    use existing script (#165)
    * use existing script
    
    update packages
    
    * remove header files

--- ci/download-core-sdk.sh
+++ ci/download-core-sdk.sh
@@ -31,28 +31,33 @@ mkdir -p "${CORE_SDK_DIR}/schema"
 mkdir -p "${CORE_SDK_DIR}/tools"
 mkdir -p "${CORE_SDK_DIR}/worker_sdk"
 
-spatial package retrieve "worker_sdk" "core-dynamic-x86-win32"       "${PINNED_CORE_SDK_VERSION}" "${CORE_SDK_DIR}/worker_sdk/core-dynamic-x86-win32"
-spatial package retrieve "worker_sdk" "core-dynamic-x86_64-win32"    "${PINNED_CORE_SDK_VERSION}" "${CORE_SDK_DIR}/worker_sdk/core-dynamic-x86_64-win32"
-spatial package retrieve "worker_sdk" "core-dynamic-x86_64-linux"    "${PINNED_CORE_SDK_VERSION}" "${CORE_SDK_DIR}/worker_sdk/core-dynamic-x86_64-linux"
-spatial package retrieve "worker_sdk" "core-bundle-x86_64-macos"     "${PINNED_CORE_SDK_VERSION}" "${CORE_SDK_DIR}/worker_sdk/core-bundle-x86_64-macos"
-spatial package retrieve "worker_sdk" "csharp"                       "${PINNED_CORE_SDK_VERSION}" "${CORE_SDK_DIR}/worker_sdk/csharp"
-spatial package retrieve "tools"      "schema_compiler-x86_64-win32" "${PINNED_CORE_SDK_VERSION}" "${CORE_SDK_DIR}/tools/schema_compiler-x86_64-win32"
-spatial package retrieve "tools"      "schema_compiler-x86_64-macos" "${PINNED_CORE_SDK_VERSION}" "${CORE_SDK_DIR}/tools/schema_compiler-x86_64-macos"
-spatial package retrieve "schema"     "standard_library"             "${PINNED_CORE_SDK_VERSION}" "${CORE_SDK_DIR}/schema/standard_library"
+spatial package retrieve "worker_sdk" "c-dynamic-x86_64-msvc_mt-win32"        "${PINNED_CORE_SDK_VERSION}" "${CORE_SDK_DIR}/worker_sdk/c-dynamic-x86_64-msvc_mt-win32"
+spatial package retrieve "worker_sdk" "c-dynamic-x86_64-gcc_libstdcpp-linux"  "${PINNED_CORE_SDK_VERSION}" "${CORE_SDK_DIR}/worker_sdk/c-dynamic-x86_64-gcc_libstdcpp-linux"
+spatial package retrieve "worker_sdk" "c-dynamic-x86_64-clang_libcpp-macos"   "${PINNED_CORE_SDK_VERSION}" "${CORE_SDK_DIR}/worker_sdk/c-dynamic-x86_64-clang_libcpp-macos"
+spatial package retrieve "worker_sdk" "csharp_core"                           "${PINNED_CORE_SDK_VERSION}" "${CORE_SDK_DIR}/worker_sdk/csharp"
+spatial package retrieve "tools"      "schema_compiler-x86_64-win32"          "${PINNED_CORE_SDK_VERSION}" "${CORE_SDK_DIR}/tools/schema_compiler-x86_64-win32"
+spatial package retrieve "tools"      "schema_compiler-x86_64-macos"          "${PINNED_CORE_SDK_VERSION}" "${CORE_SDK_DIR}/tools/schema_compiler-x86_64-macos"
+spatial package retrieve "schema"     "standard_library"                      "${PINNED_CORE_SDK_VERSION}" "${CORE_SDK_DIR}/schema/standard_library"
 
-cleanAndUnpackTo "${CORE_SDK_DIR}/worker_sdk/core-dynamic-x86-win32"         "${NATIVE_DEPENDENCIES_PATH}/Windows/x86"
-cleanAndUnpackTo "${CORE_SDK_DIR}/worker_sdk/core-dynamic-x86_64-win32"      "${NATIVE_DEPENDENCIES_PATH}/Windows/x86_64"
-cleanAndUnpackTo "${CORE_SDK_DIR}/worker_sdk/core-dynamic-x86_64-linux"      "${NATIVE_DEPENDENCIES_PATH}/Linux/x86_64"
-cleanAndUnpackTo "${CORE_SDK_DIR}/worker_sdk/core-bundle-x86_64-macos"       "${NATIVE_DEPENDENCIES_PATH}/OSX"
-cleanAndUnpackTo "${CORE_SDK_DIR}/worker_sdk/csharp"                         "${MANAGED_DEPENDENCIES_PATH}"
-cleanAndUnpackTo "${CORE_SDK_DIR}/tools/schema_compiler-x86_64-win32"        "tools/schema_compiler/win"
-cleanAndUnpackTo "${CORE_SDK_DIR}/tools/schema_compiler-x86_64-macos"        "tools/schema_compiler/macos"
+cleanAndUnpackTo "${CORE_SDK_DIR}/worker_sdk/c-dynamic-x86_64-msvc_mt-win32"        "${NATIVE_DEPENDENCIES_PATH}/Windows/x86_64"
+cleanAndUnpackTo "${CORE_SDK_DIR}/worker_sdk/c-dynamic-x86_64-gcc_libstdcpp-linux"  "${NATIVE_DEPENDENCIES_PATH}/Linux/x86_64"
+cleanAndUnpackTo "${CORE_SDK_DIR}/worker_sdk/c-dynamic-x86_64-clang_libcpp-macos"   "${NATIVE_DEPENDENCIES_PATH}/OSX"
+cleanAndUnpackTo "${CORE_SDK_DIR}/worker_sdk/csharp"                                "${MANAGED_DEPENDENCIES_PATH}"
+cleanAndUnpackTo "${CORE_SDK_DIR}/tools/schema_compiler-x86_64-win32"               "tools/schema_compiler/win"
+cleanAndUnpackTo "${CORE_SDK_DIR}/tools/schema_compiler-x86_64-macos"               "tools/schema_compiler/macos"
 
 # `spatial local launch` and `spatial upload` require these to be here, until the new project structure is applied.
-cleanAndUnpackTo "${CORE_SDK_DIR}/build/schema/standard_library"             "build/dependencies/schema/standard_library"
+cleanAndUnpackTo "${CORE_SDK_DIR}/schema/standard_library"             "build/dependencies/schema/standard_library"
 
 # Remove unused tools and files.
 rm tools/schema_compiler/win/protoc.exe
-rm tools/schema_compiler/macos/protoc.exe
+rm tools/schema_compiler/macos/protoc
 rm -rf tools/schema_compiler/win/proto
 rm -rf tools/schema_compiler/macos/proto
+
+# Remove header files in C API
+rm -rf "${NATIVE_DEPENDENCIES_PATH}/Windows/x86_64/include"
+rm -rf "${NATIVE_DEPENDENCIES_PATH}/Linux/x86_64/include"
+rm -rf "${NATIVE_DEPENDENCIES_PATH}/OSX/include"
+
+rm -rf "${CORE_SDK_DIR}"
