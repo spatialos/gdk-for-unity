ed41d639be4a178b0c965c869fc8fb5a91c309b9
Peter Mounce <petermounce@improbable.io>
2018-08-03 22:52:09 +0100

    set -x behind DEBUG env var for sensible verbosity

--- ci/docs-lint.sh
+++ ci/docs-lint.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-set -e -u -x -o pipefail
+set -e -u -o pipefail
+if [[ -n "${DEBUG-}" ]]; then
+  set -x
+fi
 
 cd "$(dirname "$0")/../"
 
@@ -7,5 +10,5 @@ source ci/includes/pinned-tools.sh
 source ci/includes/profiling.sh
 
 markStartOfBlock "Running Docs Linter"
-dotnet run -p tools/DocsLinter/DocsLinter.csproj 
+dotnet run -p tools/DocsLinter/DocsLinter.csproj
 markEndOfBlock "Running Docs Linter"
