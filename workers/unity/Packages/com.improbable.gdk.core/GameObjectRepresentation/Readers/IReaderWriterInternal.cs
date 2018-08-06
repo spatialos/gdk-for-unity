da72621efcf71d0fd49ca910e456d0d10dba12c6
Firtina O <firtina@improbable.io>
2018-08-06 14:58:25 +0100

    PR Feedback
    Rename file and comments

---
+++ workers/unity/Packages/com.improbable.gdk.core/GameObjectRepresentation/Readers/IReaderWriterInternal.cs
@@ -0,0 +1,11 @@
+﻿using Improbable.Worker;
+
+namespace Improbable.Gdk.Core.MonoBehaviours
+{
+    /// <summary>
+    ///     The interface is used to restrict which types a <see cref="IReaderWriterCreator"/> can create.
+    /// </summary>
+    internal interface IReaderWriterInternal
+    {
+    }
+}
