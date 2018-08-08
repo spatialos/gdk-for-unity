c6a0da20c69f562f0abf308055abecf86602aca9
Peter Mernyei <petermernyei@improbable.io>
2018-08-08 11:46:24 +0100

    Filled in remaining exception rethrowing

---
+++ workers/unity/Packages/com.improbable.gdk.core/GameObjectRepresentation/Writers/WriterDataUpdateFailedException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Improbable.Gdk.Core.MonoBehaviours
+{
+    public class WriterDataUpdateFailedException : Exception
+    {
+        private const string FailureMessage = "Failed to update data for writer";
+
+        public WriterDataUpdateFailedException(Exception innerException)
+            : base(FailureMessage, innerException)
+        {
+        }
+    }
+}
