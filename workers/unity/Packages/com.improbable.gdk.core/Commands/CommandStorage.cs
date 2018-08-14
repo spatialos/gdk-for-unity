90cb5aa8affa77ed2646afb090b2aeeb6b5f9223
Jamie Brynes <jamiebrynes@improbable.io>
2018-08-14 12:04:43 +0100

    add plumbing for command storage
    storage fix

---
+++ workers/unity/Packages/com.improbable.gdk.core/Commands/CommandStorage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Unity.Entities;
+
+namespace Improbable.Gdk.Core.Commands
+{
+    public abstract class CommandStorage
+    {
+        public abstract Type CommandType { get; }
+    }
+
+    public struct CommandRequestStore<T>
+    {
+        public Entity Entity;
+        public T Request;
+        public object Context;
+
+        public CommandRequestStore(Entity entity, T request, object context)
+        {
+            Entity = entity;
+            Request = request;
+            Context = context;
+        }
+    }
+}
