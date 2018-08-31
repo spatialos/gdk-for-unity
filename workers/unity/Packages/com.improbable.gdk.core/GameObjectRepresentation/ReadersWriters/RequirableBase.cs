3179621118dad322a8727344b4a48ebfea7f409a
Jonas Hou <jonas@improbable.io>
2018-08-31 17:53:59 +0100

    Refined error message.

--- workers/unity/Packages/com.improbable.gdk.core/GameObjectRepresentation/ReadersWriters/RequirableBase.cs
+++ workers/unity/Packages/com.improbable.gdk.core/GameObjectRepresentation/ReadersWriters/RequirableBase.cs
@@ -6,7 +6,9 @@ namespace Improbable.Gdk.Core.GameObjectRepresentation
     public class RequirableBase : IInjectable, IDisposable
     {
         private const string AccessedDisposedRequirable =
-            "Tried to access a requirable SpatialOS MonoBehaviour API object (e.g. Reader, Writer, CommandRequestSender, CommandRequestHandler etc.) that is already disposed.";
+            "Tried to access a disposed [Require] SpatialOS MonoBehaviour API object (e.g. Reader, Writer, CommandRequestSender, CommandRequestHandler etc.). " +
+            "Note that [Require] objects are already disposed at the time OnDisable() is called. " +
+            "Also note that user callbacks are deregistered automatically so there is no need to manually deregister them in OnDisable().";
 
         private readonly ILogDispatcher logDispatcher;
         private bool isDisposed;
@@ -29,7 +31,6 @@ namespace Improbable.Gdk.Core.GameObjectRepresentation
 
         public void Dispose()
         {
-            Debug.LogWarning("Disposing this " + this);
             isDisposed = true;
         }
     }
