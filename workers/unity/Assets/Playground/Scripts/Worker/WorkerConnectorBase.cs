19a0528a5942e7471a48f11a8b35c5611695c10b
Callum Lawson <lawsonathome@gmail.com>
2018-09-05 13:58:07 +0100

    Added comment to explain that warning is a stop gap

--- workers/unity/Assets/Playground/Scripts/Worker/WorkerConnectorBase.cs
+++ workers/unity/Assets/Playground/Scripts/Worker/WorkerConnectorBase.cs
@@ -55,6 +55,7 @@ namespace Playground
                             .WithField("WorkerType", workerType)
                             .WithField("Reason", "Required worker failed to connect"));
 #if UNITY_EDITOR
+                    //Temporary warning to be replaced when we can reliably detect if a local runtime is running, or not. 
                     logger.HandleLog(LogType.Warning,
                         new LogEvent("Is a local runtime running? If not, you can start one from 'SpaitalOS -> Local launch' or by pressing Cmd/Ctrl-L")                      
                             .WithField("Reason", "A worker running in the editor failing to connect was observed"));
