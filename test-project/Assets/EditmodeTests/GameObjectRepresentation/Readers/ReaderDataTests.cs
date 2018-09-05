3181d2bdfd47501aa697cc05ac887835640ae702
Jamie Brynes <jamiebrynes@improbable.io>
2018-09-05 13:13:06 +0100

    add tests
    add tests

---
+++ test-project/Assets/EditmodeTests/GameObjectRepresentation/Readers/ReaderDataTests.cs
@@ -0,0 +1,20 @@
+﻿using Generated.Improbable.Gdk.Tests.BlittableTypes;
+using NUnit.Framework;
+
+namespace Improbable.Gdk.Generated.EditmodeTests.MonoBehaviours.Readers
+{
+    [TestFixture]
+    internal class ReaderDataTests : ReaderWriterTestsBase
+    {
+        [Test]
+        public void Data_returns_component_instance()
+        {
+            EntityManager.SetComponentData(Entity, new BlittableComponent.Component { FloatField = 5.0f });
+
+            var data = EntityManager.GetComponentData<BlittableComponent.Component>(Entity);
+
+            Assert.AreEqual(data, ReaderPublic.Data);
+            Assert.AreEqual(data.FloatField, ReaderPublic.Data.FloatField);
+        }
+    }
+}
