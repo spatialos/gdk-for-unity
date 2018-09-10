callumlawson/FastPlatformer

First pass over new transfroms module

workers/unity/Packages/com.improbable.gdk.transformsynchronization/Systems/PositionSendSystem.cs
@@ -14,45 +14,63 @@ namespace Improbable.Gdk.TransformSynchronization
         {
             public readonly int Length;
             public ComponentDataArray<Position.Component> Position;
+            public ComponentDataArray<LastPositionSentData> LastPositionSent;
             [ReadOnly] public ComponentDataArray<Authoritative<Position.Component>> PositionAuthority;
             [ReadOnly] public ComponentDataArray<SpatialEntityId> SpatialEntityIds;
         }
 
         [Inject] private PositionData positionData;
 
-        // Number of position sends per second.
-        private const float SendRateHz = 1.0f;
-
-        private float timeSinceLastSend = 0.0f;
-
         protected override void OnUpdate()
         {
-            // Send update at SendRateHz.
-            timeSinceLastSend += Time.deltaTime;
-            if (timeSinceLastSend < 1.0f / SendRateHz)
-            {
-                return;
-            }
-
-            timeSinceLastSend = 0.0f;
-
             for (var i = 0; i < positionData.Length; i++)
             {
-                var component = positionData.Position[i];
+                var position = positionData.Position[i];
 
-                if (component.DirtyBit != true)
+                if (position.DirtyBit != true)
                 {
                     continue;
                 }
 
+                var lastPositionSent = positionData.LastPositionSent[i];
+                lastPositionSent.TimeSinceLastUpdate += Time.deltaTime;
+                positionData.LastPositionSent[i] = lastPositionSent;
+
+                if (lastPositionSent.TimeSinceLastUpdate <
+                    1.0f / TransformSynchronizationConfig.MaxPositionUpdateRateHz)
+                {
+                    continue;
+                }
+
+                var squareDisatnce =
+                    TransformUtils.SquareDisatnce(lastPositionSent.Position.Coords, positionData.Position[i].Coords);
+
+                if (squareDisatnce == 0.0f)
+                {
+                    continue;
+                }
+
+                if (lastPositionSent.TimeSinceLastUpdate <
+                    TransformSynchronizationConfig.MaxTimeForStalePositionWithoutUpdateS)
+                {
+                    if (squareDisatnce < TransformSynchronizationConfig.MaxSquarePositionChangeWithoutUpdate)
+                    {
+                        continue;
+                    }
+                }
+
                 var entityId = positionData.SpatialEntityIds[i].EntityId;
 
-                var update = new SchemaComponentUpdate(component.ComponentId);
-                Position.Serialization.Serialize(component, update.GetFields());
+                var update = new SchemaComponentUpdate(position.ComponentId);
+                Position.Serialization.Serialize(position, update.GetFields());
                 WorkerSystem.Connection.SendComponentUpdate(entityId, new ComponentUpdate(update));
 
-                component.DirtyBit = false;
-                positionData.Position[i] = component;
+                position.DirtyBit = false;
+                positionData.Position[i] = position;
+
+                lastPositionSent.TimeSinceLastUpdate = 0.0f;
+                lastPositionSent.Position = position;
+                positionData.LastPositionSent[i] = lastPositionSent;
             }
         }
     }

workers/unity/Packages/com.improbable.gdk.transformsynchronization/Systems/TickSystem.cs
@@ -1,16 +1,35 @@
+using Generated.Improbable.Transform;
+using Improbable.Gdk.Core;
+using Unity.Collections;
 using Unity.Entities;
+using UnityEngine.Experimental.PlayerLoop;
 
 namespace Improbable.Gdk.TransformSynchronization
 {
     [DisableAutoCreation]
-    [UpdateInGroup(typeof(TransformSynchronizationGroup))]
+    [UpdateBefore(typeof(DefaultUpdateLatestTransformSystem))]
+    [UpdateInGroup(typeof(FixedUpdate))]
     public class TickSystem : ComponentSystem
     {
-        public uint GlobalTick = 0;
+        private struct Data
+        {
+            [ReadOnly] public readonly int Length;
+            [ReadOnly] public ComponentDataArray<Transform.Component> Transform;
+            public ComponentDataArray<TicksSinceLastUpdate> TicksSinceLastUpdate;
+
+            [ReadOnly] public ComponentDataArray<Authoritative<Transform.Component>> DenotesAuthority;
+        }
+
+        [Inject] private Data data;
 
         protected override void OnUpdate()
         {
-            GlobalTick++;
+            for (int i = 0; i < data.Length; ++i)
+            {
+                TicksSinceLastUpdate t = data.TicksSinceLastUpdate[i];
+                t.NumberOfTicks += 1;
+                data.TicksSinceLastUpdate[i] = t;
+            }
         }
     }
 }

workers/unity/Packages/com.improbable.gdk.transformsynchronization/TransformUtils.cs
@@ -1,0 +1,59 @@
+﻿using Generated.Improbable;
+using Generated.Improbable.Transform;
+using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Transform = Generated.Improbable.Transform.Transform;
+
+namespace Improbable.Gdk.TransformSynchronization
+{
+    public static class TransformUtils
+    {
+        // Can avoid these distance methods by having them at the intermediate layer rather than a custom send system
+        // Should leave the custom send system for purely time based things
+        public static float SquareDisatnce(Coordinates a, Coordinates b)
+        {
+            return (float) ((a.X - b.X) * (a.X - b.X) + (a.Y - b.Y) * (a.Y - b.Y) + (a.Z - b.Z) * (a.Z - b.Z));
+        }
+
+        public static float SquareDisatnce(Location a, Location b)
+        {
+            return (a.X - b.X) * (a.X - b.X) + (a.Y - b.Y) * (a.Y - b.Y) + (a.Z - b.Z) * (a.Z - b.Z);
+        }
+
+        public static float SquareDisatnce(Velocity a, Velocity b)
+        {
+            return (a.X - b.X) * (a.X - b.X) + (a.Y - b.Y) * (a.Y - b.Y) + (a.Z - b.Z) * (a.Z - b.Z);
+        }
+
+        public static Quaternion ToUnityQuaternion(this Generated.Improbable.Transform.Quaternion quaternion)
+        {
+            return new Quaternion(quaternion.X, quaternion.Y, quaternion.Z, quaternion.W);
+        }
+
+        public static Generated.Improbable.Transform.Quaternion ToImprobableQuaternion(this Quaternion quaternion)
+        {
+            return new Generated.Improbable.Transform.Quaternion(quaternion.w, quaternion.x, quaternion.y,
+                quaternion.z);
+        }
+
+        public static Vector3 ToUnityVector3(this Location location)
+        {
+            return new Vector3(location.X, location.Y, location.Z);
+        }
+
+        public static Location ToImprobableLocation(this Vector3 vector)
+        {
+            return new Location(vector.x, vector.y, vector.z);
+        }
+
+        public static Vector3 ToUnityVector3(this Velocity velocity)
+        {
+            return new Vector3(velocity.X, velocity.Y, velocity.Z);
+        }
+
+        public static Velocity ToImprobableVelocity(this Vector3 velocity)
+        {
+            return new Velocity(velocity.x, velocity.y, velocity.z);
+        }
+    }
+}

spatialos/gdk-for-unity

workers/unity/Packages/com.improbable.gdk.transformsynchronization/Systems/PositionSendSystem.cs
@@ -14,45 +14,63 @@ namespace Improbable.Gdk.TransformSynchronization
         {
             public readonly int Length;
             public ComponentDataArray<Position.Component> Position;
+            public ComponentDataArray<LastPositionSentData> LastPositionSent;
             [ReadOnly] public ComponentDataArray<Authoritative<Position.Component>> PositionAuthority;
             [ReadOnly] public ComponentDataArray<SpatialEntityId> SpatialEntityIds;
         }
 
         [Inject] private PositionData positionData;
 
-        // Number of position sends per second.
-        private const float SendRateHz = 1.0f;
-
-        private float timeSinceLastSend = 0.0f;
-
         protected override void OnUpdate()
         {
-            // Send update at SendRateHz.
-            timeSinceLastSend += Time.deltaTime;
-            if (timeSinceLastSend < 1.0f / SendRateHz)
-            {
-                return;
-            }
-
-            timeSinceLastSend = 0.0f;
-
             for (var i = 0; i < positionData.Length; i++)
             {
-                var component = positionData.Position[i];
+                var position = positionData.Position[i];
 
-                if (component.DirtyBit != true)
+                if (position.DirtyBit != true)
                 {
                     continue;
                 }
 
+                var lastPositionSent = positionData.LastPositionSent[i];
+                lastPositionSent.TimeSinceLastUpdate += Time.deltaTime;
+                positionData.LastPositionSent[i] = lastPositionSent;
+
+                if (lastPositionSent.TimeSinceLastUpdate <
+                    1.0f / TransformSynchronizationConfig.MaxPositionUpdateRateHz)
+                {
+                    continue;
+                }
+
+                var squareDisatnce =
+                    TransformUtils.SquareDistance(lastPositionSent.Position.Coords, positionData.Position[i].Coords);
+
+                if (squareDisatnce == 0.0f)
+                {
+                    continue;
+                }
+
+                if (lastPositionSent.TimeSinceLastUpdate <
+                    TransformSynchronizationConfig.MaxTimeForStalePositionWithoutUpdateS)
+                {
+                    if (squareDisatnce < TransformSynchronizationConfig.MaxSquarePositionChangeWithoutUpdate)
+                    {
+                        continue;
+                    }
+                }
+
                 var entityId = positionData.SpatialEntityIds[i].EntityId;
 
-                var update = new SchemaComponentUpdate(component.ComponentId);
-                Position.Serialization.Serialize(component, update.GetFields());
+                var update = new SchemaComponentUpdate(position.ComponentId);
+                Position.Serialization.Serialize(position, update.GetFields());
                 WorkerSystem.Connection.SendComponentUpdate(entityId, new ComponentUpdate(update));
 
-                component.DirtyBit = false;
-                positionData.Position[i] = component;
+                position.DirtyBit = false;
+                positionData.Position[i] = position;
+
+                lastPositionSent.TimeSinceLastUpdate = 0.0f;
+                lastPositionSent.Position = position;
+                positionData.LastPositionSent[i] = lastPositionSent;
             }
         }
     }

workers/unity/Packages/com.improbable.gdk.transformsynchronization/Systems/TickSystem.cs
@@ -1,16 +1,35 @@
+using Generated.Improbable.Transform;
+using Improbable.Gdk.Core;
+using Unity.Collections;
 using Unity.Entities;
+using UnityEngine.Experimental.PlayerLoop;
 
 namespace Improbable.Gdk.TransformSynchronization
 {
     [DisableAutoCreation]
-    [UpdateInGroup(typeof(TransformSynchronizationGroup))]
+    [UpdateBefore(typeof(DefaultUpdateLatestTransformSystem))]
+    [UpdateInGroup(typeof(FixedUpdate))]
     public class TickSystem : ComponentSystem
     {
-        public uint GlobalTick = 0;
+        private struct Data
+        {
+            public readonly int Length;
+            [ReadOnly] public ComponentDataArray<TransformInternal.Component> Transform;
+            public ComponentDataArray<TicksSinceLastUpdate> TicksSinceLastUpdate;
+
+            [ReadOnly] public ComponentDataArray<Authoritative<Transform.Component>> DenotesAuthority;
+        }
+
+        [Inject] private Data data;
 
         protected override void OnUpdate()
         {
-            GlobalTick++;
+            for (int i = 0; i < data.Length; ++i)
+            {
+                TicksSinceLastUpdate t = data.TicksSinceLastUpdate[i];
+                t.NumberOfTicks += 1;
+                data.TicksSinceLastUpdate[i] = t;
+            }
         }
     }
 }

workers/unity/Packages/com.improbable.gdk.transformsynchronization/TransformUtils.cs
@@ -1,0 +1,59 @@
+﻿using Generated.Improbable;
+using Generated.Improbable.Transform;
+using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Transform = Generated.Improbable.Transform.Transform;
+
+namespace Improbable.Gdk.TransformSynchronization
+{
+    public static class TransformUtils
+    {
+        // Can avoid these distance methods by having them at the intermediate layer rather than a custom send system
+        // Should leave the custom send system for purely time based things
+        public static float SquareDistance(Coordinates a, Coordinates b)
+        {
+            return (float) ((a.X - b.X) * (a.X - b.X) + (a.Y - b.Y) * (a.Y - b.Y) + (a.Z - b.Z) * (a.Z - b.Z));
+        }
+
+        public static float SquareDistance(Location a, Location b)
+        {
+            return (a.X - b.X) * (a.X - b.X) + (a.Y - b.Y) * (a.Y - b.Y) + (a.Z - b.Z) * (a.Z - b.Z);
+        }
+
+        public static float SquareDistance(Velocity a, Velocity b)
+        {
+            return (a.X - b.X) * (a.X - b.X) + (a.Y - b.Y) * (a.Y - b.Y) + (a.Z - b.Z) * (a.Z - b.Z);
+        }
+
+        public static Quaternion ToUnityQuaternion(this Generated.Improbable.Transform.Quaternion quaternion)
+        {
+            return new Quaternion(quaternion.X, quaternion.Y, quaternion.Z, quaternion.W);
+        }
+
+        public static Generated.Improbable.Transform.Quaternion ToImprobableQuaternion(this Quaternion quaternion)
+        {
+            return new Generated.Improbable.Transform.Quaternion(quaternion.w, quaternion.x, quaternion.y,
+                quaternion.z);
+        }
+
+        public static Vector3 ToUnityVector3(this Location location)
+        {
+            return new Vector3(location.X, location.Y, location.Z);
+        }
+
+        public static Location ToImprobableLocation(this Vector3 vector)
+        {
+            return new Location(vector.x, vector.y, vector.z);
+        }
+
+        public static Vector3 ToUnityVector3(this Velocity velocity)
+        {
+            return new Vector3(velocity.X, velocity.Y, velocity.Z);
+        }
+
+        public static Velocity ToImprobableVelocity(this Vector3 velocity)
+        {
+            return new Velocity(velocity.x, velocity.y, velocity.z);
+        }
+    }
+}