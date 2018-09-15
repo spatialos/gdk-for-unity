bd7ef896a6f7176b2125af6144a91004c5a1aa82
Jamie Brynes <jamiebrynes7@gmail.com>
2018-09-15 21:14:37 +0100

    add protected method

--- workers/unity/Packages/com.improbable.gdk.core/CodegenAdapters/ComponentDispatcherHandler.cs
+++ workers/unity/Packages/com.improbable.gdk.core/CodegenAdapters/ComponentDispatcherHandler.cs
@@ -1,4 +1,5 @@
 using System;
+using Improbable.Worker;
 using Improbable.Worker.Core;
 using Unity.Entities;
 using Entity = Unity.Entities.Entity;
@@ -39,6 +40,25 @@ namespace Improbable.Gdk.Core.CodegenAdapters
             LogDispatcher = worker.LogDispatcher;
             World = world;
             Worker = worker;
+        }
+
+        /// <summary>
+        ///     A helper method that wraps TryGetEntity on the Worker. This throws if the entity does not exist.
+        ///     The reason is throws is that this method is called on receiving ops and not having that Entity Id is
+        ///     strictly incorrect.
+        /// </summary>
+        /// <param name="entityId">The Spatial EntityId to try and get.</param>
+        /// <returns>The Unity ECS entity that corresponds to the Spatial Entity ID.</returns>
+        /// <exception cref="InvalidSpatialEntityStateException"></exception>
+        protected Entity TryGetEntityFromEntityId(EntityId entityId)
+        {
+            if (!Worker.TryGetEntity(entityId, out var entity))
+            {
+                throw new InvalidSpatialEntityStateException(
+                    $"Could not find Unity Entity for Spatial Entity ID {entityId.Id}.");
+            }
+
+            return entity;
         }
     }
 }
