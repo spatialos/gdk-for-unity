9fdaab7f9ddaeed01b0f12ad497f9e9e5ccf1b75
Jamie Brynes <jamiebrynes@improbable.io>
2018-09-18 15:11:37 +0100

    put in namespace

--- workers/unity/Packages/com.improbable.gdk.standardlibraryutils/CoordinatesExtensions.cs
+++ workers/unity/Packages/com.improbable.gdk.standardlibraryutils/CoordinatesExtensions.cs
@@ -1,27 +1,29 @@
-﻿using Improbable;
-using UnityEngine;
+﻿using UnityEngine;
 
-public static class CoordinatesExtensions
+namespace Improbable
 {
-    /// <summary>
-    ///     Conversion method from Unity Vector3 to Spatial Coordinates.
-    /// </summary>
-    /// <param name="vector">The Vector3 to convert.</param>
-    /// <returns>The converted Coordinates.</returns>
-    public static Coordinates ToSpatialCoordinates(this Vector3 vector)
+    public static class CoordinatesExtensions
     {
-        return new Coordinates(vector.x, vector.y, vector.z);
-    }
+        /// <summary>
+        ///     Conversion method from Unity Vector3 to Spatial Coordinates.
+        /// </summary>
+        /// <param name="vector">The Vector3 to convert.</param>
+        /// <returns>The converted Coordinates.</returns>
+        public static Coordinates ToSpatialCoordinates(this Vector3 vector)
+        {
+            return new Coordinates(vector.x, vector.y, vector.z);
+        }
 
-    /// <summary>
-    ///     Conversion method from Spatial Coordinates to Unity Vector3.
-    ///     Be aware this involves a downcast from double to float and
-    ///     overflow may occur.
-    /// </summary>
-    /// <param name="coordinates">The Coordinates to convert.</param>
-    /// <returns>The converted Vector3.</returns>
-    public static Vector3 NarrowToUnityVector(this Coordinates coordinates)
-    {
-        return new Vector3((float) coordinates.X, (float) coordinates.Y, (float) coordinates.Z);
+        /// <summary>
+        ///     Conversion method from Spatial Coordinates to Unity Vector3.
+        ///     Be aware this involves a downcast from double to float and
+        ///     overflow may occur.
+        /// </summary>
+        /// <param name="coordinates">The Coordinates to convert.</param>
+        /// <returns>The converted Vector3.</returns>
+        public static Vector3 NarrowToUnityVector(this Coordinates coordinates)
+        {
+            return new Vector3((float) coordinates.X, (float) coordinates.Y, (float) coordinates.Z);
+        }
     }
 }
--- workers/unity/Packages/com.improbable.gdk.standardlibraryutils/Vector3fExtensions.cs
+++ workers/unity/Packages/com.improbable.gdk.standardlibraryutils/Vector3fExtensions.cs
@@ -1,25 +1,27 @@
-﻿using Improbable;
-using UnityEngine;
+﻿using UnityEngine;
 
-public static class Vector3fExtensions
+namespace Improbable
 {
-    /// <summary>
-    ///     Conversion method from Unity Vector3 to Spatial Vector3f.
-    /// </summary>
-    /// <param name="vector">The Vector3 to convert.</param>
-    /// <returns>The converted Vector3f.</returns>
-    public static Vector3f ToSpatialVector3f(this Vector3 vector)
+    public static class Vector3fExtensions
     {
-        return new Vector3f(vector.x, vector.y, vector.z);
-    }
+        /// <summary>
+        ///     Conversion method from Unity Vector3 to Spatial Vector3f.
+        /// </summary>
+        /// <param name="vector">The Vector3 to convert.</param>
+        /// <returns>The converted Vector3f.</returns>
+        public static Vector3f ToSpatialVector3f(this Vector3 vector)
+        {
+            return new Vector3f(vector.x, vector.y, vector.z);
+        }
 
-    /// <summary>
-    ///     Conversion method from Spatial Vector3f to Unity Vector3
-    /// </summary>
-    /// <param name="vector">The Vector3f to convert.</param>
-    /// <returns>The converted Vector3.</returns>
-    public static Vector3 ToUnityVector(this Vector3f vector)
-    {
-        return new Vector3(vector.X, vector.Y, vector.Z);
+        /// <summary>
+        ///     Conversion method from Spatial Vector3f to Unity Vector3
+        /// </summary>
+        /// <param name="vector">The Vector3f to convert.</param>
+        /// <returns>The converted Vector3.</returns>
+        public static Vector3 ToUnityVector(this Vector3f vector)
+        {
+            return new Vector3(vector.X, vector.Y, vector.Z);
+        }
     }
 }
