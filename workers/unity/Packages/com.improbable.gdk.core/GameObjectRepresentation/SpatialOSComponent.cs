7a3de8dfede23b13f6b9b3d19518afa1419aea25
jessicafalk <31853332+jessicafalk@users.noreply.github.com>
2018-09-19 18:51:08 +0100

    UTY-1182: adding TryGetGameObjectForEntity to SpatialOSComponent (#397)
    * adding helper method to get game object from SpatialOSComponent
    
    * PR feedback
    
    * initializing entitymanager only once
    
    * PR feedback

--- workers/unity/Packages/com.improbable.gdk.core/GameObjectRepresentation/SpatialOSComponent.cs
+++ workers/unity/Packages/com.improbable.gdk.core/GameObjectRepresentation/SpatialOSComponent.cs
@@ -12,9 +12,29 @@ namespace Improbable.Gdk.GameObjectRepresentation
         public World World;
         public WorkerSystem Worker;
 
+        private EntityManager entityManager;
+
         public bool IsEntityOnThisWorker(EntityId entityId)
         {
             return Worker.EntityIdToEntity.ContainsKey(entityId);
+        }
+
+        public bool TryGetGameObjectForSpatialOSEntityId(EntityId entityId, out GameObject linkedGameObject)
+        {
+            linkedGameObject = default(GameObject);
+            if (!Worker.TryGetEntity(entityId, out var entity))
+            {
+                return false;
+            }
+
+            entityManager = entityManager ?? World.GetOrCreateManager<EntityManager>();
+            if (!entityManager.HasComponent<GameObjectReference>(entity))
+            {
+                return false;
+            }
+
+            linkedGameObject = entityManager.GetComponentObject<GameObjectReference>(entity).GameObject;
+            return true;
         }
     }
 }
