b1a5eb156510227f9199be297a983b0b9b854478
Boyan Asenov <boyan@improbable.io>
2018-09-19 13:39:48 +0100

    Refactor to use Improbable.Common Empty

--- workers/unity/Assets/Playground/Scripts/MonoBehaviours/SpawnCubeCommandReceiver.cs
+++ workers/unity/Assets/Playground/Scripts/MonoBehaviours/SpawnCubeCommandReceiver.cs
@@ -1,5 +1,6 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using Improbable;
+using Improbable.Common;
 using Improbable.Gdk.Core;
 using Improbable.Gdk.Core.Commands;
 using Improbable.Gdk.GameObjectRepresentation;
