3c5f297c0215b91f9cc65f94b0d1ac7552705d10
Peter Mernyei <petermernyei@improbable.io>
2018-09-19 16:04:57 +0100

    Added error logging

--- workers/unity/Packages/com.improbable.gdk.gameobjectcreation/GameObjectCreationSystemHelper.cs
+++ workers/unity/Packages/com.improbable.gdk.gameobjectcreation/GameObjectCreationSystemHelper.cs
@@ -17,10 +17,27 @@ namespace Improbable.Gdk.GameObjectCreation
         public static void EnableStandardGameObjectCreation(World world, IEntityGameObjectCreator creator,
             GameObject workerGameObject = null)
         {
+            var workerSystem = world.GetExistingManager<WorkerSystem>();
+            var entityManager = world.GetOrCreateManager<EntityManager>();
             world.CreateManager<GameObjectInitializationSystem>(creator);
             if (workerGameObject != null)
             {
-                world.CreateManager<WorkerEntityGameObjectLinkerSystem>(workerGameObject);
+                if (world.GetExistingManager<WorkerEntityGameObjectLinkerSystem>() != null)
+                {
+                    workerSystem.LogDispatcher.HandleLog(LogType.Error, new LogEvent(
+                            "You have already added a worker GameObject, calling it the second time will be ignored")
+                        .WithField(LoggingUtils.LoggerName, nameof(GameObjectCreationSystemHelper)));
+                }
+                else if (!entityManager.HasComponent<OnConnected>(workerSystem.WorkerEntity))
+                {
+                    workerSystem.LogDispatcher.HandleLog(LogType.Error, new LogEvent("You cannot set the Worker " +
+                            "GameObject once the World has already started running")
+                        .WithField(LoggingUtils.LoggerName, nameof(GameObjectCreationSystemHelper)));
+                }
+                else
+                {
+                    world.CreateManager<WorkerEntityGameObjectLinkerSystem>(workerGameObject);
+                }
             }
         }
     }
