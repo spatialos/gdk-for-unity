e614e4b4cfd10bbb50ec5f6c487bee4ff75f2489
Jessica Falk <jessica@improbable.io>
2018-09-24 10:16:39 +0100

    adding additional checks and more tests

--- workers/unity/Packages/com.improbable.gdk.core/GameObjectRepresentation/SpatialOSComponent.cs
+++ workers/unity/Packages/com.improbable.gdk.core/GameObjectRepresentation/SpatialOSComponent.cs
@@ -33,27 +33,41 @@ namespace Improbable.Gdk.GameObjectRepresentation
                 return false;
             }
 
-            linkedGameObject = entityManager.GetComponentObject<GameObjectReference>(entity).GameObject;
+            var retrievedGameObject = entityManager.GetComponentObject<GameObjectReference>(entity).GameObject;
+            var component = retrievedGameObject.GetComponent<SpatialOSComponent>();
+            if (component == null || component.SpatialEntityId != entityId)
+            {
+                return false;
+            }
+
+            linkedGameObject = retrievedGameObject;
             return true;
         }
 
         public bool TryGetSpatialOSEntityIdForGameObject(GameObject linkedGameObject, out EntityId entityId)
         {
+            entityId = default(EntityId);
             var component = linkedGameObject.GetComponent<SpatialOSComponent>();
-            if (component == null)
+            if (component == null || Worker != component.Worker)
             {
-                entityId = default(EntityId);
                 return false;
             }
 
-            if (Worker != component.Worker)
+            entityManager = entityManager ?? World.GetOrCreateManager<EntityManager>();
+            if (!entityManager.HasComponent<GameObjectReference>(component.Entity))
             {
-                entityId = default(EntityId);
+                return false;
+            }
+
+            var retrievedGameObject = entityManager.GetComponentObject<GameObjectReference>(component.Entity).GameObject;
+            if (linkedGameObject != retrievedGameObject)
+            {
                 return false;
             }
 
             entityId = component.SpatialEntityId;
             return true;
+
         }
     }
 }
