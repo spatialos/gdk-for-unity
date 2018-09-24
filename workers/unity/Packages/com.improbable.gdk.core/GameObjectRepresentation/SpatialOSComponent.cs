cafc9a08f7012669d1f13fc7ce8bd6c3563adc91
jessicafalk <31853332+jessicafalk@users.noreply.github.com>
2018-09-24 11:16:36 +0100

    adding TryGetSpatialOSEntityIdForGameObject (#412)
    * adding TryGetSpatialOSEntityIdForGameObject

--- workers/unity/Packages/com.improbable.gdk.core/GameObjectRepresentation/SpatialOSComponent.cs
+++ workers/unity/Packages/com.improbable.gdk.core/GameObjectRepresentation/SpatialOSComponent.cs
@@ -33,8 +33,41 @@ namespace Improbable.Gdk.GameObjectRepresentation
                 return false;
             }
 
-            linkedGameObject = entityManager.GetComponentObject<GameObjectReference>(entity).GameObject;
+            var retrievedGameObject = entityManager.GetComponentObject<GameObjectReference>(entity).GameObject;
+            var component = retrievedGameObject.GetComponent<SpatialOSComponent>();
+            if (component == null || component.SpatialEntityId != entityId)
+            {
+                return false;
+            }
+
+            linkedGameObject = retrievedGameObject;
             return true;
+        }
+
+        public bool TryGetSpatialOSEntityIdForGameObject(GameObject linkedGameObject, out EntityId entityId)
+        {
+            entityId = default(EntityId);
+            var component = linkedGameObject.GetComponent<SpatialOSComponent>();
+            if (component == null || Worker != component.Worker)
+            {
+                return false;
+            }
+
+            entityManager = entityManager ?? World.GetOrCreateManager<EntityManager>();
+            if (!entityManager.HasComponent<GameObjectReference>(component.Entity))
+            {
+                return false;
+            }
+
+            var retrievedGameObject = entityManager.GetComponentObject<GameObjectReference>(component.Entity).GameObject;
+            if (linkedGameObject != retrievedGameObject)
+            {
+                return false;
+            }
+
+            entityId = component.SpatialEntityId;
+            return true;
+
         }
     }
 }
