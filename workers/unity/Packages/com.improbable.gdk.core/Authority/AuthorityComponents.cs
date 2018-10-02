8cb3c9ac3293aa695d40d361b226f4281851ee2a
Jamie Brynes <jamiebrynes@improbable.io>
2018-10-02 16:42:59 +0100

    authority docs
    authority docs pr feedback

--- workers/unity/Packages/com.improbable.gdk.core/Authority/AuthorityComponents.cs
+++ workers/unity/Packages/com.improbable.gdk.core/Authority/AuthorityComponents.cs
@@ -2,14 +2,27 @@ using Unity.Entities;
 
 namespace Improbable.Gdk.Core
 {
+    /// <summary>
+    ///     This ECS component denotes that this worker is authoritative over T.
+    /// </summary>
+    /// <typeparam name="T">The SpatialOS component.</typeparam>
     public struct Authoritative<T> : IComponentData where T : ISpatialComponentData
     {
     }
 
+    /// <summary>
+    ///     This ECS component denotes that this worker is not authoritative over T.
+    /// </summary>
+    /// <typeparam name="T">The SpatialOS component.</typeparam>
     public struct NotAuthoritative<T> : IComponentData where T : ISpatialComponentData
     {
     }
 
+    /// <summary>
+    ///     This ECS component denotes that this worker will lose authority over T imminently.
+    ///     Note that this worker is still authoritative over this component.
+    /// </summary>
+    /// <typeparam name="T">The SpatialOS component.</typeparam>
     public struct AuthorityLossImminent<T> : IComponentData where T : ISpatialComponentData
     {
     }
