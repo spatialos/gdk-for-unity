5baebabbc204d9499be2182b624c545ec52f8353
sami <sami@improbable.io>
2018-10-11 18:37:50 +0100

    Changed the clean system to use an archetype query for the reactive components

--- workers/unity/Packages/com.improbable.gdk.core/CodegenAdapters/ComponentCleanupHandler.cs
+++ workers/unity/Packages/com.improbable.gdk.core/CodegenAdapters/ComponentCleanupHandler.cs
@@ -4,15 +4,9 @@ namespace Improbable.Gdk.Core.CodegenAdapters
 {
     public abstract class ComponentCleanupHandler
     {
-        public abstract ComponentType[] CleanUpComponentTypes { get; }
-        public abstract ComponentType[] EventComponentTypes { get; }
-        public abstract ComponentType ComponentUpdateType { get; }
-        public abstract ComponentType AuthorityChangesType { get; }
-        public abstract ComponentType[] CommandReactiveTypes { get; }
+        public abstract EntityArchetypeQuery CleanupArchetypeQuery { get; }
 
-        public abstract void CleanupUpdates(ComponentGroup updateGroup, ref EntityCommandBuffer buffer);
-        public abstract void CleanupAuthChanges(ComponentGroup authorityChangesGroup, ref EntityCommandBuffer buffer);
-        public abstract void CleanupEvents(ComponentGroup[] eventGroups, ref EntityCommandBuffer buffer);
-        public abstract void CleanupCommands(ComponentGroup[] commandCleanupGroups, ref EntityCommandBuffer buffer);
+        public abstract void CleanComponents(ComponentGroup group, ComponentSystemBase system,
+            EntityCommandBuffer buffer);
     }
 }
