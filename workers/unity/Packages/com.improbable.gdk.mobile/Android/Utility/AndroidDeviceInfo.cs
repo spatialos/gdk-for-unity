387eccc25ef57476fe8a58e3ddfe666c016d99c5
Georgiy Kassabli <georgiykassabli@improbable.io>
2018-10-17 17:22:07 +0100

    Lazily initialise AndroidDeviceInfo
    Rework AndroidDeviceInfo to be lazily initialise and dispose unneeded objects immediately

--- workers/unity/Packages/com.improbable.gdk.mobile/Android/Utility/AndroidDeviceInfo.cs
+++ workers/unity/Packages/com.improbable.gdk.mobile/Android/Utility/AndroidDeviceInfo.cs
@@ -6,25 +6,34 @@ namespace Improbable.Gdk.Mobile.Android
     {
         public const string AndroidStudioEmulatorDefaultCallbackIp = "10.0.2.2";
 
-        private static readonly AndroidJavaObject Build = new AndroidJavaObject("android.os.Build");
-        private static readonly string Fingerprint = GetValue("FINGERPRINT");
-        private static readonly string Model = GetValue("MODEL");
-        private static readonly string Brand = GetValue("BRAND");
-        private static readonly string Device = GetValue("DEVICE");
-        private static readonly string Product = GetValue("PRODUCT");
+        private static bool isInitialised = false;
+        private static bool isEmulator = false;
 
         public static bool IsAndroidStudioEmulator()
         {
-            return Fingerprint.StartsWith("generic") || Fingerprint.StartsWith("unknown") ||
-                Model.Contains("google_sdk") || Model.Contains("Emulator") ||
-                Model.Contains("Android SDK built for x86") ||
-                Brand.StartsWith("generic") && Device.StartsWith("generic") ||
-                Product.Equals("google_sdk");
+            if (!isInitialised)
+            {
+                AndroidJavaObject build = new AndroidJavaObject("android.os.Build");
+                string fingerprint = GetValue(build, "FINGERPRINT");
+                string model = GetValue(build, "MODEL");
+                string brand = GetValue(build, "BRAND");
+                string device = GetValue(build, "DEVICE");
+                string product = GetValue(build, "PRODUCT");
+
+                isEmulator = fingerprint.StartsWith("generic") || fingerprint.StartsWith("unknown") ||
+                    model.Contains("google_sdk") || model.Contains("Emulator") ||
+                    model.Contains("Android SDK built for x86") ||
+                    brand.StartsWith("generic") && device.StartsWith("generic") ||
+                    product.Equals("google_sdk");
+                isInitialised = true;
+            }
+
+            return isEmulator;
         }
 
-        private static string GetValue(string parameter)
+        private static string GetValue(AndroidJavaObject build, string parameter)
         {
-            return Build.GetStatic<string>(parameter);
+            return build.GetStatic<string>(parameter);
         }
     }
 }
