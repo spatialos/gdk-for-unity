9f09a3f5f1ecfe4962e8c85399b6b00c1dfb5ab6
gkassabli <georgiykassabli@improbable.io>
2018-10-18 18:29:55 +0100

    Mobile local dev: Adding mobile connection workers (#521)
    * Adding mobile connection workers
    
    * Adding iOS and Android scenes with workers
    
    * First round of feedback
    
    * Use the property instead of the removed method
    
    * Fix variables and names
    
    * Addressing PR review
    
    * Interface for dealing with the splash screen
    
    * Changed callback names to be consistent
    
    * Show an error message if the worker connector is not instantiated
    
    * Whitespace fixes
    
    * Fixed PlayerPrefs names
    
    * Improved exception messages in both connectors
    
    * Changed interface name to be more representative
    
    * Throw an exception if the WorkerConnector behaviour is null
    
    * Add a new line in asmdef
    
    * Change equality comparison to avoid nulls
    
    * adding dispose back in

---
+++ workers/unity/Assets/Playground/Scripts/Worker/AndroidClientWorkerConnector.cs
@@ -0,0 +1,70 @@
+using Improbable.Gdk.Core;
+using Improbable.Gdk.Mobile;
+#if UNITY_ANDROID
+using Improbable.Gdk.Mobile.Android;
+#endif
+using System;
+using UnityEngine;
+
+
+namespace Playground
+{
+    public class AndroidClientWorkerConnector : MobileWorkerConnector, IMobileConnectionController
+    {
+        public string IpAddress { get; set; }
+        public ConnectionScreenController ConnectionScreenController { get; set; }
+
+        [SerializeField] private GameObject level;
+
+        private GameObject levelInstance;
+
+        public async void TryConnect()
+        {
+            await Connect(WorkerUtils.AndroidClient, new ForwardingDispatcher()).ConfigureAwait(false);
+        }
+
+        protected override void HandleWorkerConnectionEstablished()
+        {
+            ConnectionScreenController.OnConnectionSucceeded();
+            WorkerUtils.AddClientSystems(Worker.World);
+
+            if (level == null)
+            {
+                return;
+            }
+
+            levelInstance = Instantiate(level, transform);
+            levelInstance.transform.SetParent(null);
+        }
+
+        protected override void HandleWorkerConnectionFailure()
+        {
+            ConnectionScreenController.OnConnectionFailed();
+        }
+
+        protected override string GetHostIp()
+        {
+#if UNITY_ANDROID
+            if (Application.isMobilePlatform && DeviceInfo.IsAndroidStudioEmulator() && string.Empty.Equals(IpAddress))
+            {
+                return DeviceInfo.AndroidStudioEmulatorDefaultCallbackIp;
+            }
+
+            return IpAddress;
+#else
+            throw new PlatformNotSupportedException(
+                "AndroidClientWorkerConnector can only be used for the Android platform. Please check your build settings.");
+#endif
+        }
+
+        public override void Dispose()
+        {
+            if (levelInstance != null)
+            {
+                Destroy(levelInstance);
+            }
+
+            base.Dispose();
+        }
+    }
+}
