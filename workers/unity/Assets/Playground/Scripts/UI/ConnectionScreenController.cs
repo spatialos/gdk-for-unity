7df70e0c86127fba86540ecb773f9fa9b281e7a2
firtina-improbable <firtina@improbable.io>
2018-10-22 17:22:30 +0100

    Workaround iOS UI input crash
    UI can’t crash
    
    if we don’t use it

--- workers/unity/Assets/Playground/Scripts/UI/ConnectionScreenController.cs
+++ workers/unity/Assets/Playground/Scripts/UI/ConnectionScreenController.cs
@@ -1,32 +1,86 @@
 using Improbable.Gdk.Core;
-using Improbable.Gdk.Mobile;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace Playground
 {
+    [ExecuteInEditMode]
     public class ConnectionScreenController : MonoBehaviour
     {
         private const string HostIpPlayerPrefsKey = "SpatialOSHostIp";
+        private const int GuiPadding = 10;
 
-        [SerializeField] private GameObject connectionPanel;
-        [SerializeField] private InputField ipAddressInput;
-        [SerializeField] private Button connectButton;
-        [SerializeField] private Text errorMessage;
         [SerializeField] private GameObject clientWorkerPrefab;
+        [SerializeField] private Font font;
+        [SerializeField] private int wantedFontSize = 20;
+
+        private bool showConnectionGui = true;
+        private bool isConnecting = false;
+        private string ipAddressText = null;
+        private string errorMessage = "";
 
         private GameObject worker;
 
-        private string IpAddress => ipAddressInput != null ? ipAddressInput.text : null;
-
         public void Awake()
         {
-            ipAddressInput.text = PlayerPrefs.GetString(HostIpPlayerPrefsKey);
-            connectButton.onClick.AddListener(TryConnect);
+            ipAddressText = PlayerPrefs.GetString(HostIpPlayerPrefsKey);
+        }
+
+        public void OnGUI()
+        {
+            if (!showConnectionGui)
+            {
+                return;
+            }
+
+            using (new GUILayout.AreaScope(new Rect(
+                GuiPadding,
+                GuiPadding,
+                Screen.width - GuiPadding * 2,
+                Screen.height - GuiPadding * 2)))
+            {
+                var guiEnabled = GUI.enabled;
+
+                GUI.enabled = !isConnecting;
+                try
+                {
+                    using (new ResizedGui(font, wantedFontSize,
+                        GUI.skin.label,
+                        GUI.skin.textField,
+                        GUI.skin.button,
+                        GUI.skin.textArea
+                    ))
+                    {
+                        ResizedGui.Label("Enter IP address:");
+                        ipAddressText = ResizedGui.TextField(ipAddressText);
+
+                        if (ResizedGui.Button("Connect"))
+                        {
+                            if (Application.isPlaying)
+                            {
+                                TryConnect();
+                            }
+                        }
+
+                        if (!string.IsNullOrEmpty(errorMessage))
+                        {
+                            ResizedGui.Label("Error:");
+
+                            ResizedGui.TextArea(errorMessage);
+                        }
+                    }
+                }
+                finally
+                {
+                    GUI.enabled = guiEnabled;
+                }
+            }
         }
 
         private void TryConnect()
         {
+            errorMessage = "";
+            isConnecting = true;
+
             worker = Instantiate(clientWorkerPrefab);
             var workerConnector = worker.GetComponent<IMobileConnectionController>();
 
@@ -36,23 +90,25 @@ namespace Playground
                 throw new MissingComponentException("The WorkerConnector behaviour was not found on the worker prefab");
             }
 
-            workerConnector.IpAddress = IpAddress;
+            workerConnector.IpAddress = ipAddressText;
             workerConnector.ConnectionScreenController = this;
             workerConnector.TryConnect();
         }
 
         public void OnConnectionSucceeded()
         {
-            PlayerPrefs.SetString(HostIpPlayerPrefsKey, IpAddress);
+            PlayerPrefs.SetString(HostIpPlayerPrefsKey, ipAddressText);
             PlayerPrefs.Save();
 
-            connectionPanel.SetActive(false);
+            showConnectionGui = false;
+            isConnecting = false;
         }
 
         public void OnConnectionFailed(string connectionError)
         {
             UnityObjectDestroyer.Destroy(worker);
-            errorMessage.text = $"Connection failed. Please check the IP address entered. {connectionError}";
+            errorMessage = $"Connection failed. Please check the IP address entered. {connectionError}";
+            isConnecting = false;
         }
     }
 }
---
+++ workers/unity/Assets/Playground/Scripts/UI/ResizedGui.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using UnityEngine;
+
+namespace Playground
+{
+    internal struct ResizedGui : IDisposable
+    {
+        private readonly GUIStyle[] styles;
+        private readonly Font[] fontCache;
+        private readonly int[] fontSizeCache;
+
+        public ResizedGui(Font font, int fontSize, params GUIStyle[] skins)
+        {
+            styles = skins;
+
+            fontCache = new Font[skins.Length];
+            fontSizeCache = new int[skins.Length];
+
+            for (var i = 0; i < styles.Length; i++)
+            {
+                var guiStyle = styles[i];
+                fontCache[i] = guiStyle.font;
+                fontSizeCache[i] = guiStyle.fontSize;
+
+                guiStyle.font = font;
+                guiStyle.fontSize = fontSize;
+            }
+        }
+
+        public void Dispose()
+        {
+            for (var i = 0; i < styles.Length; i++)
+            {
+                var guiStyle = styles[i];
+                guiStyle.font = fontCache[i];
+                guiStyle.fontSize = fontSizeCache[i];
+            }
+        }
+
+        private static readonly GUIContent TempGuiContent = new GUIContent();
+
+        public static void Label(string labelText)
+        {
+            TempGuiContent.text = labelText;
+            GUILayout.Label(labelText, GUILayout.Height(GUI.skin.label.CalcSize(TempGuiContent).y));
+        }
+
+        public static bool Button(string text)
+        {
+            TempGuiContent.text = text;
+            return GUILayout.Button(text, GUILayout.Height(GUI.skin.button.CalcSize(TempGuiContent).y));
+        }
+
+        public static string TextField(string text)
+        {
+            TempGuiContent.text = text;
+            return GUILayout.TextField(text, GUILayout.Height(GUI.skin.textField.CalcSize(TempGuiContent).y));
+        }
+
+        public static string TextArea(string text)
+        {
+            TempGuiContent.text = text;
+            return GUILayout.TextArea(text, 
+                GUILayout.Height(GUI.skin.textArea.CalcHeight(TempGuiContent, Screen.width)));
+        }
+    }
+}
