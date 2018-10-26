cf4e677531f74afc8b4f1aa841408430b300f2c7
Jessica Falk <jessica@improbable.io>
2018-10-26 15:23:00 +0100

    actually fixing merge conflicts

--- ci/build-test.sh
+++ ci/build-test.sh
@@ -17,17 +17,11 @@ source ".shared-ci/scripts/pinned-tools.sh"
 ci/test.sh
 .shared-ci/scripts/build.sh "workers/unity" UnityClient local "$(pwd)/logs/UnityClientBuild.log"
 .shared-ci/scripts/build.sh "workers/unity" UnityGameLogic cloud "$(pwd)/logs/UnityGameLogicBuild.log"
-<<<<<<< HEAD
-=======
 
 if isWindows; then
     .shared-ci/scripts/build.sh "workers/unity" AndroidClient local "$(pwd)/logs/AndroidClientBuild.log"
 fi
->>>>>>> Add a check if it's running on Windows
 
 if isMacOS; then
   .shared-ci/scripts/build.sh "workers/unity" iOSClient local "$(pwd)/logs/iOSClientBuild.log"
 fi
-
-.shared-ci/scripts/build.sh "workers/unity" AndroidClient local "$(pwd)/logs/AndroidClientBuild.log"
-
