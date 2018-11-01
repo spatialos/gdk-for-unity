f190eda6526b9247aad1c9a8aee1f51b1c751ae6
sami <sami@improbable.io>
2018-11-01 21:45:18 +0000

    started command system stuff

--- workers/unity/Packages/com.improbable.gdk.core/Worker/Worker.cs
+++ workers/unity/Packages/com.improbable.gdk.core/Worker/Worker.cs
@@ -267,6 +267,7 @@ namespace Improbable.Gdk.Core
             World.GetOrCreateManager<WorldCommandsSendSystem>();
             World.GetOrCreateManager<CommandRequestTrackerSystem>();
             World.GetOrCreateManager<AcknowledgeAuthorityLossSystem>();
+            World.GetOrCreateManager<CommandSenderSystem>();
         }
 
         public void Dispose()
