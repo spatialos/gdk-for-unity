977517686cf93dbc416056c30f1afc1aa23ae686
sami <sami@improbable.io>
2018-11-05 11:06:29 +0000

    Codegen for new stuff

--- workers/unity/Packages/com.improbable.gdk.tools/.CodeGenerator/GdkCodeGenerator/src/Generation/Generators/Parts/UnityCommandSenderReceiverGeneratorPart.cs
+++ workers/unity/Packages/com.improbable.gdk.tools/.CodeGenerator/GdkCodeGenerator/src/Generation/Generators/Parts/UnityCommandSenderReceiverGeneratorPart.cs
@@ -4,14 +4,14 @@ using System.Linq;
 namespace Improbable.Gdk.CodeGenerator
 {
     /// <summary>
-    ///     This class contains the data required to fill out the "UnityGameObjectComponentDispatcherGenerator.tt" templates.
-    ///     This template generates the GameObjectComponentDispatcher implementation for Components.
+    ///     This class contains the data required to fill out the "UnityCommandSenderReceiverGenerator.tt" templates.
+    ///     This generates the command senders and receivers as well as their subscriptions and callback managers.
     /// </summary>
-    public partial class UnityGameObjectComponentDispatcherGenerator
+    public partial class UnityCommandSenderReceiverGenerator
     {
         private string qualifiedNamespace;
         private UnityComponentDetails details;
-
+        
         public string Generate(UnityComponentDetails details, string package)
         {
             qualifiedNamespace = package;
@@ -41,3 +41,4 @@ namespace Improbable.Gdk.CodeGenerator
         }
     }
 }
+
