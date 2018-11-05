f05196ff17415f22fb409bd98fa30e258ee8c616
sami <sami@improbable.io>
2018-11-05 11:02:56 +0000

    started subscriptions - removed the old stuff

--- workers/unity/Packages/com.improbable.gdk.core/GameObjectRepresentation/Injection/IInjectable.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Improbable.Gdk.GameObjectRepresentation
-{
-    /// <summary>
-    ///     Tag interface for all objects which can be injected into MonoBehaviour fields using the [Require] attribute.
-    /// </summary>
-    public interface IInjectable
-    {
-    }
-}
