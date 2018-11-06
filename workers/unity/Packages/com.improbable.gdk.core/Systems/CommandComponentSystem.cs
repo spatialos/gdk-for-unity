679aa6525cec6fdcebcde2c4de0521578709a703
sami <sami@improbable.io>
2018-11-06 23:49:52 +0000

    Rebase command response components onto the command system

---
+++ workers/unity/Packages/com.improbable.gdk.core/Systems/CommandComponentSystem.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using Improbable.Gdk.Core.Commands;
+using Unity.Entities;
+
+namespace Improbable.Gdk.Core
+{
+    [DisableAutoCreation]
+    [AlwaysUpdateSystem]
+    [UpdateInGroup(typeof(SpatialOSReceiveGroup.InternalSpatialOSReceiveGroup))]
+    [UpdateAfter(typeof(SpatialOSReceiveSystem))]
+    public class CommandComponentSystem : ComponentSystem
+    {
+        private readonly List<ICommandComponentManager> managers = new List<ICommandComponentManager>();
+
+        protected override void OnCreateManager()
+        {
+            base.OnCreateManager();
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                foreach (var type in assembly.GetTypes())
+                {
+                    if (!typeof(ICommandComponentManager).IsAssignableFrom(type) || type.IsAbstract)
+                    {
+                        continue;
+                    }
+
+                    var instance = (ICommandComponentManager) Activator.CreateInstance(type);
+                    managers.Add(instance);
+                }
+            }
+        }
+
+        [Inject] private EntityManager entityManager;
+        [Inject] private CommandSystem commandSystem;
+
+        protected override void OnUpdate()
+        {
+            foreach (var manager in managers)
+            {
+                manager.PopulateCommandComponents(commandSystem, entityManager, World);
+            }
+        }
+    }
+}
