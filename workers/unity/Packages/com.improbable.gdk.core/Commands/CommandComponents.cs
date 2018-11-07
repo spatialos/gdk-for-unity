1240b63cf75bfc165189bbae65ac66619bd41a1f
sami <sami@improbable.io>
2018-11-07 14:03:00 +0000

    Some tidying up of type constraints and generated command types

---
+++ workers/unity/Packages/com.improbable.gdk.core/Commands/CommandComponents.cs
@@ -0,0 +1,18 @@
+namespace Improbable.Gdk.Core.Commands
+{
+    public interface ICommandRequest
+    {
+    }
+
+    public interface IReceivedCommandRequest
+    {
+    }
+
+    public interface ICommandResponse
+    {
+    }
+
+    public interface IReceivedCommandResponse
+    {
+    }
+}
