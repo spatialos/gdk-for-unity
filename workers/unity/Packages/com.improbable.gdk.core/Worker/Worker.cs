6841fa2aba7770777466773c0fb04c15915e792a
sami <sami@improbable.io>
2018-11-08 16:51:46 +0000

    Removed the command storage stuff - tests currently set to fail as they need a rewrite

--- workers/unity/Packages/com.improbable.gdk.core/Worker/Worker.cs
+++ workers/unity/Packages/com.improbable.gdk.core/Worker/Worker.cs
@@ -265,7 +265,6 @@ namespace Improbable.Gdk.Core
             World.GetOrCreateManager<CleanReactiveComponentsSystem>();
             World.GetOrCreateManager<WorldCommandsCleanSystem>();
             World.GetOrCreateManager<WorldCommandsSendSystem>();
-            World.GetOrCreateManager<CommandRequestTrackerSystem>();
             World.GetOrCreateManager<AcknowledgeAuthorityLossSystem>();
             World.GetOrCreateManager<CommandSystem>();
             World.GetOrCreateManager<CommandComponentSystem>();
