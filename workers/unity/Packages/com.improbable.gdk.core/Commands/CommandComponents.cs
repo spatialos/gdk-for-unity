d854bce93901489d68ce3a86a829d8058cf49b05
Sami Husain <sami.hs1@gmail.com>
2018-11-10 18:47:46 +0000

    Made a command callback system and rebased the command senders and receviers on it

--- workers/unity/Packages/com.improbable.gdk.core/Commands/CommandComponents.cs
+++ workers/unity/Packages/com.improbable.gdk.core/Commands/CommandComponents.cs
@@ -1,3 +1,5 @@
+using Improbable.Worker;
+
 namespace Improbable.Gdk.Core.Commands
 {
     public interface ICommandRequest
@@ -6,6 +8,17 @@ namespace Improbable.Gdk.Core.Commands
 
     public interface IReceivedCommandRequest
     {
+        /// <summary>
+        ///     Gets the request ID from the request. For use in generic methods.
+        /// </summary>
+        /// <returns> The request ID associated with the request </returns>
+        long GetRequestId();
+
+        /// <summary>
+        ///     Gets the target entity ID from the request. For use in generic methods.
+        /// </summary>
+        /// <returns> The target entity ID associated with the request </returns>
+        EntityId GetTargetEntityId();
     }
 
     public interface ICommandResponse
@@ -14,5 +27,10 @@ namespace Improbable.Gdk.Core.Commands
 
     public interface IReceivedCommandResponse
     {
+        /// <summary>
+        ///     Gets the request ID from the request. For use in generic methods.
+        /// </summary>
+        /// <returns> The request ID associated with the request </returns>
+        long GetRequestId();
     }
 }
--- workers/unity/Packages/com.improbable.gdk.core/Subscriptions/Systems/CommandCallbackSystem.cs
+++ workers/unity/Packages/com.improbable.gdk.core/Subscriptions/Systems/CommandCallbackSystem.cs
@@ -1,18 +1,16 @@
 using System;
 using System.Collections.Generic;
 using Improbable.Gdk.Core;
+using Improbable.Gdk.Core.Commands;
+using Improbable.Worker;
 using Unity.Entities;
 using UnityEngine;
 
 namespace Improbable.Gdk.Subscriptions
 {
-    // todo change this to be like the command system but for callbacks
     // meant to be an example of abstractions that can be made on top of stuff rather than core core stuff
     // for example a more efficient callback thing might have functors that can be reused to remove allocation
     // could also use this to create tasks out of commands rather than callbacks
-    // command senders and receivers should then just register things with this system rather than the stuff they are currently doing
-    // although what they are currently doing is wrong anyway; they should be given the stuff in the constructor rather than the silly back and forth
-    // they should just be an interface onto some other system, not the systems themselves
     [DisableAutoCreation]
     [UpdateInGroup(typeof(SpatialOSReceiveGroup.InternalSpatialOSReceiveGroup))]
     [UpdateAfter(typeof(SpatialOSReceiveSystem))]
@@ -21,33 +19,50 @@ namespace Improbable.Gdk.Subscriptions
         private readonly Dictionary<Type, ICommandRequestCallbackManager> typeToRequestCallbackManager =
             new Dictionary<Type, ICommandRequestCallbackManager>();
 
-        private readonly List<ICommandRequestCallbackManager> requestCallbackManagers = new List<ICommandRequestCallbackManager>();
+        private readonly List<ICommandRequestCallbackManager> requestCallbackManagers =
+            new List<ICommandRequestCallbackManager>();
 
         private readonly Dictionary<Type, ICommandResponseCallbackManager> typeToResponseCallbackManager =
             new Dictionary<Type, ICommandResponseCallbackManager>();
 
-        private readonly List<ICommandResponseCallbackManager> responseCallbackManagers = new List<ICommandResponseCallbackManager>();
+        private readonly List<ICommandResponseCallbackManager> responseCallbackManagers =
+            new List<ICommandResponseCallbackManager>();
 
-        public ICommandRequestCallbackManager GetCommandRequestCallbackManager(Type type)
+        public ulong RegisterCommandRequestCallback<T>(EntityId entityId, Action<T> callback)
+            where T : IReceivedCommandRequest
         {
-            return typeToRequestCallbackManager[type];
+            if (!typeToRequestCallbackManager.TryGetValue(typeof(T), out var manager))
+            {
+                manager = new CommandRequestCallbackManager<T>();
+                typeToRequestCallbackManager.Add(typeof(T), manager);
+                requestCallbackManagers.Add(manager);
+            }
+
+            return ((CommandRequestCallbackManager<T>) manager).RegisterCallback(entityId, callback);
         }
 
-        public ICommandResponseCallbackManager GetCommandResponseCallbackManager(Type type)
+        public ulong RegisterCommandResponseCallback<T>(long requestId, Action<T> callback)
+            where T : IReceivedCommandResponse
         {
-            return typeToResponseCallbackManager[type];
+            if (!typeToResponseCallbackManager.TryGetValue(typeof(T), out var manager))
+            {
+                manager = new CommandResponseCallbackManager<T>();
+                typeToResponseCallbackManager.Add(typeof(T), manager);
+                responseCallbackManagers.Add(manager);
+            }
+
+            return ((CommandResponseCallbackManager<T>) manager).RegisterCallback(requestId, callback);
         }
 
-        public void RegisterCommandRequestCallbackManager(Type type, ICommandRequestCallbackManager manager)
+        // todo caller shouldn't have to know the type
+        public bool UnregisterCommandRequestCallback<T>(ulong callbackKey) where T : IReceivedCommandRequest
         {
-            typeToRequestCallbackManager.Add(type, manager);
-            requestCallbackManagers.Add(manager);
-        }
+            if (!typeToRequestCallbackManager.TryGetValue(typeof(T), out var manager))
+            {
+                return false;
+            }
 
-        public void RegisterCommandResponseCallbackManager(Type type, ICommandResponseCallbackManager manager)
-        {
-            typeToResponseCallbackManager.Add(type, manager);
-            responseCallbackManagers.Add(manager);
+            return ((CommandRequestCallbackManager<T>) manager).UnregisterCallback(callbackKey);
         }
 
         [Inject] private CommandSystem commandSystem;
