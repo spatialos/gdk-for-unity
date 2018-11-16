37601f5d25ac49b5a54803c20e77bcd5cfea67f9
Jamie Brynes <jamiebrynes@improbable.io>
2018-11-16 16:05:14 +0000

    fix crash

---
+++ workers/unity/Packages/com.improbable.gdk.core/Commands/SpecialCommandSender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Improbable.Worker;
+using Unity.Entities;
+
+namespace Improbable.Gdk.Core
+{
+    public interface ISpecialCommandSender
+    {
+        void RegisterSelf(Dictionary<Type, Func<EntityManager, global::Unity.Entities.Entity, object, EntityId, object, object, uint?, bool, long>> callbacks);
+    }
+}
+
