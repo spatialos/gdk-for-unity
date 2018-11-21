16a0f17cac8e7ea337c7f8f780f1ad2076dda9a6
Jamie Brynes <jamiebrynes7@gmail.com>
2018-11-21 15:37:00 +0000

    Schema Bundle JSON Structure (#622)

---
+++ workers/unity/Packages/com.improbable.gdk.tools/.CodeGenerator/CodeGeneration/Model/SchemaBundleV1/ComponentDefinitionRaw.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Improbable.Gdk.CodeGeneration.Model.SchemaBundleV1
+{
+    public class ComponentDefinitionRaw
+    {
+        [JsonProperty("identifier")] public Identifier Identifier;
+        [JsonProperty("componentId")] public uint ComponentId;
+        [JsonProperty("dataDefinition")] public WrappedType Data;
+
+        [JsonProperty("eventDefinitions")] public List<EventDefinitionRaw> Events;
+        [JsonProperty("commandDefinitions")] public List<CommandDefinitionRaw> Commands;
+
+        public class WrappedType
+        {
+            [JsonProperty("type")] public UserType Type;
+        }
+
+        public class EventDefinitionRaw
+        {
+            [JsonProperty("identifier")] public Identifier Identifier;
+            [JsonProperty("eventIndex")] public uint EventIndex;
+            [JsonProperty("type")] public WrappedType Type;
+        }
+
+        public class CommandDefinitionRaw
+        {
+            [JsonProperty("identifier")] public Identifier Identifier;
+            [JsonProperty("commandIndex")] public uint CommandIndex;
+            [JsonProperty("requestType")] public WrappedType RequestType;
+            [JsonProperty("responseType")] public WrappedType ResponseType;
+        }
+    }
+}
---
+++ workers/unity/Packages/com.improbable.gdk.tools/.CodeGenerator/CodeGeneration/Tests/Model/SchemaBundleV1/JsonParsingTests.cs
@@ -0,0 +1,29 @@
+using System.IO;
+using System.Reflection;
+using Improbable.Gdk.CodeGeneration.Model.SchemaBundleV1;
+using NUnit.Framework;
+
+namespace CodeGeneration.Tests.Model.SchemaBundleV1
+{
+    [TestFixture]
+    public class JsonParsingTests
+    {
+        private const string BundleResourceName =
+            "CodeGeneration.Tests.Model.SchemaBundleV1.Resources.exhaustive_bundle.json";
+
+        private string bundleContents;
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            bundleContents = new StreamReader(assembly.GetManifestResourceStream(BundleResourceName)).ReadToEnd();
+        }
+
+        [Test]
+        public void ParsingSchemaBundleV1_does_not_throw()
+        {
+            Assert.DoesNotThrow(() => SchemaBundle.FromJson(bundleContents));
+        }
+    }
+}
