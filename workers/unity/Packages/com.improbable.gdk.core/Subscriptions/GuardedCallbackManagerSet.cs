9a57af11bf2b79358beeb5e967ad021260dd67c5
sami <sami@improbable.io>
2018-11-27 18:39:02 +0000

    Added a guarded callback manager object

---
+++ workers/unity/Packages/com.improbable.gdk.core/Subscriptions/GuardedCallbackManagerSet.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+
+namespace Improbable.Gdk.Subscriptions
+{
+    internal class GuardedCallbackManagerSet<TIndex, TManager>
+    {
+        private readonly Dictionary<TIndex, TManager> indexToCallbackManager =
+            new Dictionary<TIndex, TManager>();
+
+        private readonly List<TManager> callbackManagers = new List<TManager>();
+
+        private readonly List<TIndex> indexesToAdd = new List<TIndex>();
+
+        public bool TryGetManager(TIndex index, out TManager manager)
+        {
+            return indexToCallbackManager.TryGetValue(index, out manager);
+        }
+
+        public void AddCallbackManager(TIndex index, TManager manager)
+        {
+            indexToCallbackManager.Add(index, manager);
+            indexesToAdd.Add(index);
+        }
+
+        public List<TManager> GetManagers()
+        {
+            foreach (var indexToAdd in indexesToAdd)
+            {
+                callbackManagers.Add(indexToCallbackManager[indexToAdd]);
+            }
+
+            return callbackManagers;
+        }
+    }
+}
