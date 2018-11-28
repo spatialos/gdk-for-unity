5e207ade60f0f3dc92313fa350935f58fd0a59cf
sami <sami@improbable.io>
2018-11-28 15:08:34 +0000

    removed a codegen warning that was annoying me

--- workers/unity/Packages/com.improbable.gdk.tools/.CodeGenerator/GdkCodeGenerator/src/Generation/WorkerGenerationJob.cs
+++ workers/unity/Packages/com.improbable.gdk.tools/.CodeGenerator/GdkCodeGenerator/src/Generation/WorkerGenerationJob.cs
@@ -20,10 +20,9 @@ namespace Improbable.Gdk.CodeGenerator
         public WorkerGenerationJob(string outputDir, CodeGeneratorOptions options, IFileSystem fileSystem) : base(
             outputDir, fileSystem)
         {
-           
             InputFiles = new List<string>();
             OutputFiles = new List<string>();
-            
+
             workerTypesToGenerate = ExtractWorkerTypes(options.WorkerJsonDirectory);
 
             OutputFiles.Add(Path.Combine(relativeEditorPath, "Editor", workerFileName));
