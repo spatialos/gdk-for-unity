19c2c4301d446ee9ff68dea8247177df17b40aca
sami <sami@improbable.io>
2018-11-29 11:29:05 +0000

    moved the reader and writer subscriptions managers off the dispatcher

--- workers/unity/Packages/com.improbable.gdk.core/Worker/Worker.cs
+++ workers/unity/Packages/com.improbable.gdk.core/Worker/Worker.cs
@@ -256,11 +256,11 @@ namespace Improbable.Gdk.Core
             World.GetOrCreateManager<ComponentUpdateSystem>();
             World.GetOrCreateManager<CommandComponentSystem>();
             World.GetOrCreateManager<CommandCallbackSystem>();
-            World.GetOrCreateManager<SubscriptionSystem>();
             World.GetOrCreateManager<ComponentConstraintsCallbackSystem>();
             World.GetOrCreateManager<ComponentCallbackSystem>();
             World.GetOrCreateManager<ReaderLifecycleSystem>();
             World.GetOrCreateManager<ReactiveComponentSystem>();
+            World.GetOrCreateManager<SubscriptionSystem>();
         }
 
         public void Dispose()
