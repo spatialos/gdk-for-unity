7684453adba6978d3b518a3473a561692b1b5d6a
Jamie Brynes <jamiebrynes@improbable.io>
2019-01-22 15:52:04 +0000

    add pack shell script

---
+++ ci/pack.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e -u -o -x pipefail
+
+cd "$(dirname "$0")/../"
+
+# Get shared CI and prepare Unity
+ci/bootstrap.sh
+
+dotnet run -p ./.shared-ci/tools/Packer/Packer.csproj 
