8bc2abe499d9aad2496b78a98bee9bd7a1c0b0b2
Jamie Brynes <jamiebrynes@improbable.io>
2019-01-22 14:44:39 +0000

    launch a deployment

---
+++ ci/upload-launch.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e -u -o -x pipefail
+
+cd "$(dirname "$0")/../"
+
+source .shared-ci/scripts/pinned-tools.sh
+source .shared-ci/scripts/profiling.sh
+
+PREFIX="playground"
+
+.shared-ci/scripts/upload-assemblies.sh ${PREFIX}
+
+markStartOfBlock "Launching deployment"
+
+# Get first 8 characters of current git hash.
+GIT_HASH="$(git rev-parse HEAD | cut -c1-8)"
+
+spatial cloud launch "${PREFIX}_${GIT_HASH}" cloud_launch.json "${PREFIX}_${GIT_HASH}" --snapshot=snapshots/default.snapshot
+
+markEndOfBlock "Launching deployment"
