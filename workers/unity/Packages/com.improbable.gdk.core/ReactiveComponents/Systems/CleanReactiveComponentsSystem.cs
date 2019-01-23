e8911dbae691b001ac5a05ed1d041ffc8ead8345
sami <sami@improbable.io>
2019-01-23 14:46:53 +0000

    Removed part of the CleanReactiveComponentSystem that was also removing temporary components

--- workers/unity/Packages/com.improbable.gdk.core/ReactiveComponents/Systems/CleanReactiveComponentsSystem.cs
+++ workers/unity/Packages/com.improbable.gdk.core/ReactiveComponents/Systems/CleanReactiveComponentsSystem.cs
@@ -19,8 +19,6 @@ namespace Improbable.Gdk.ReactiveComponents
         // Here to prevent adding an action for the same type multiple times
         private readonly HashSet<Type> typesToRemove = new HashSet<Type>();
 
-        private readonly List<(ComponentGroup, ComponentType)> componentGroupsToRemove = new List<(ComponentGroup, ComponentType)>();
-
         protected override void OnCreateManager()
         {
             base.OnCreateManager();
@@ -31,26 +29,6 @@ namespace Improbable.Gdk.ReactiveComponents
         {
             foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
             {
-                // Find all components with the RemoveAtEndOfTick attribute
-                foreach (var type in assembly.GetTypes())
-                {
-                    if (type.GetCustomAttribute<RemoveAtEndOfTickAttribute>(false) == null)
-                    {
-                        continue;
-                    }
-
-                    if (!typeof(IComponentData).IsAssignableFrom(type)
-                        && !typeof(ISharedComponentData).IsAssignableFrom(type))
-                    {
-                        continue;
-                    }
-
-                    if (typesToRemove.Add(type))
-                    {
-                        componentGroupsToRemove.Add((GetComponentGroup(ComponentType.ReadOnly(type)), type));
-                    }
-                }
-
                 // Find all ComponentCleanupHandlers
                 foreach (var type in assembly.GetTypes())
                 {
@@ -72,31 +50,12 @@ namespace Improbable.Gdk.ReactiveComponents
 
         protected override void OnUpdate()
         {
-            var buffer = PostUpdateCommands;
-
             foreach (var cleanup in componentCleanups)
             {
                 Profiler.BeginSample("CleanReactiveComponents");
                 cleanup.Handler.CleanComponents(cleanup.ComponentsToCleanGroup, this, PostUpdateCommands);
                 Profiler.EndSample();
             }
-
-            Profiler.BeginSample("RemoveRemoveAtEndOfTick");
-            foreach ((var componentGroup, var componentType) in componentGroupsToRemove)
-            {
-                if (componentGroup.IsEmptyIgnoreFilter)
-                {
-                    continue;
-                }
-
-                var entityArray = componentGroup.GetEntityArray();
-                for (var i = 0; i < entityArray.Length; i++)
-                {
-                    PostUpdateCommands.RemoveComponent(entityArray[i], componentType);
-                }
-            }
-
-            Profiler.EndSample();
         }
 
         private struct ComponentCleanup
