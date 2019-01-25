395c8b870acadf1d7dc06b4382662e4aaa9dd856
Peter Mounce <petermounce@improbable.io>
2019-01-25 17:53:04 +0000

    While running CI, remove things to do in CI.

--- ci/build-test.sh
+++ ci/build-test.sh
@@ -11,10 +11,10 @@ ci/bootstrap.sh
 
 source ".shared-ci/scripts/pinned-tools.sh"
 
-ci/test.sh
-.shared-ci/scripts/build.sh "workers/unity" UnityClient local il2cpp "$(pwd)/logs/UnityClientBuild-il2cpp.log"
-.shared-ci/scripts/build.sh "workers/unity" UnityClient cloud mono "$(pwd)/logs/UnityClientBuild-mono.log"
-.shared-ci/scripts/build.sh "workers/unity" UnityGameLogic cloud mono "$(pwd)/logs/UnityGameLogicBuild.log"
+# ci/test.sh
+# .shared-ci/scripts/build.sh "workers/unity" UnityClient local il2cpp "$(pwd)/logs/UnityClientBuild-il2cpp.log"
+# .shared-ci/scripts/build.sh "workers/unity" UnityClient cloud mono "$(pwd)/logs/UnityClientBuild-mono.log"
+# .shared-ci/scripts/build.sh "workers/unity" UnityGameLogic cloud mono "$(pwd)/logs/UnityGameLogicBuild.log"
 .shared-ci/scripts/build.sh "workers/unity" AndroidClient local mono "$(pwd)/logs/AndroidClientBuild.log"
 
 if isMacOS; then
