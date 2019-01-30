3f7c56843fed2f8bafd73d6ca36ada1eb154a094
Jamie Brynes <jamiebrynes@improbable.io>
2019-01-30 17:46:04 +0000

    revert trigger.sh

--- .buildkite/trigger.sh
+++ .buildkite/trigger.sh
@@ -9,11 +9,4 @@ cd "$(dirname "$0")/../"
 
 # The step-definitions file is uploaded dynamically to preserve ability for historical builds
 # vs changes in CI pipeline configuration.
-
-# If docs branch then do docs premerge
-if [[ $BUILDKITE_BRANCH == docs/* ]]; then
-  STEPS=${1/premerge/docs-premerge}
-  buildkite-agent pipeline upload "$STEPS"
-else
-  buildkite-agent pipeline upload "$1"
-fi
+buildkite-agent pipeline upload "$1"
