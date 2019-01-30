4e6154a393c1d84a325eed4ae9afa29aca5cd0f0
Jamie Brynes <jamiebrynes@improbable.io>
2019-01-30 16:21:31 +0000

    modify trigger

--- .buildkite/trigger.sh
+++ .buildkite/trigger.sh
@@ -9,4 +9,10 @@ cd "$(dirname "$0")/../"
 
 # The step-definitions file is uploaded dynamically to preserve ability for historical builds
 # vs changes in CI pipeline configuration.
-buildkite-agent pipeline upload "$1"
+
+# If docs branch then do docs premerge
+if [[ $(git branch | sed -n -e 's/^\* \(.*\)/\1/p') == docs/\* ]]; then
+  buildkite-agent pipeline upload "docs-${1}"
+else 
+  buildkite-agent pipeline upload "$1"
+fi
