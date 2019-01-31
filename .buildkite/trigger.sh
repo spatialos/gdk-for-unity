617b98dbcff3618b32c8326ba6d8abde7ba9df14
Jamie Brynes <jamiebrynes@improbable.io>
2019-01-31 12:55:39 +0000

    test fix

--- .buildkite/trigger.sh
+++ .buildkite/trigger.sh
@@ -9,4 +9,4 @@ cd "$(dirname "$0")/../"
 
 # The step-definitions file is uploaded dynamically to preserve ability for historical builds
 # vs changes in CI pipeline configuration.
-buildkite-agent pipeline upload "$1"
+buildkite-agent pipeline upload "$1" --no-interpolation
