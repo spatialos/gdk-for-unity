071136cf04f95e644d4866f72a78bbaf46075c51
Jamie Brynes <jamiebrynes@improbable.io>
2019-02-04 14:22:03 +0000

    hacks on hacks

--- ci/upload-launch.sh
+++ ci/upload-launch.sh
@@ -12,6 +12,13 @@ source ".shared-ci/scripts/pinned-tools.sh"
 if [[ -n "${BUILDKITE}" ]]; then
     # In buildkite, download the artifacts and reconstruct the build/assemblies folder.
     buildkite-agent artifact download "build\assembly\**\*" .
+
+    # HACK: Since we don't control the schema descriptor right now, we need to fake a build to get all the packages
+    # so prepare-for-run does not fail.
+    # This is a no-op once it gets to actually building.
+    # This can probably be resolved with FPL.
+    (export WORKER_TYPE="AndroidClient" && export BUILD_TARGET="cloud" && export SCRIPTING_TYPE="mono" && ./ci/build.sh)
+
 else
     # In TeamCity, just build.
     ci/build-test.sh
