c15a013dd06e8b8b8dc63a81128830266c91ac27
Jamie Brynes <jamiebrynes@improbable.io>
2019-02-04 11:36:51 +0000

    modify upload-launch

--- ci/upload-launch.sh
+++ ci/upload-launch.sh
@@ -6,10 +6,16 @@ cd "$(dirname "$0")/../"
 
 PREFIX="playground"
 
-ci/build-test.sh
-
 source ".shared-ci/scripts/profiling.sh"
 source ".shared-ci/scripts/pinned-tools.sh"
+
+if [[ -n "${BUILDKITE}" ]]; then
+    # In buildkite, download the artifacts and reconstruct the build/assemblies folder.
+    buildkite-agent artifact download "build/assembly/*"
+else
+    # In TeamCity, just build.
+    ci/build-test.sh
+fi
 
 setAssemblyName "${PREFIX}"
 
