82cf64fadb38d980a09d5a0e4a6ae33db25a88ba
Jamie Brynes <jamiebrynes@improbable.io>
2019-02-04 15:05:32 +0000

    regex magic

--- ci/upload-launch.sh
+++ ci/upload-launch.sh
@@ -23,6 +23,18 @@ spatial cloud upload "${ASSEMBLY_NAME}" --log_level=debug --force --enable_pre_u
 
 markStartOfBlock "Launching deployment"
 
-spatial cloud launch "${ASSEMBLY_NAME}" cloud_launch.json "${ASSEMBLY_NAME}" --snapshot=snapshots/default.snapshot
+spatial cloud launch "${ASSEMBLY_NAME}" cloud_launch.json "${ASSEMBLY_NAME}" --snapshot=snapshots/default.snapshot | tee -a ./launch.log
+
+if [[ -n "${BUILDKITE}" ]]; then
+    CONSOLE_REGEX='.*Console URL:(.*)\\n"'
+    LAUNCH_LOG=$(cat ./launch.log)
+    if [[ $LAUNCH_LOG ~= $CONSOLE_REGEX ]]; then
+        CONSOLE_URL=${BASH_REMATCH[1]}
+        buildkite-agent annotate --style "success" "Deployment URL: ${CONSOLE_URL}"
+    fi
+    else
+        buildkite-agent annotate --style "warning" "Could not parse deployment URL from launch log."
+    fi
+fi
 
 markEndOfBlock "Launching deployment"
