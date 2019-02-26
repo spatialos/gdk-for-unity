f335ce5bc74e24f8e91094c9582c29653f9fd074
Jamie Brynes <jamiebrynes7@gmail.com>
2019-02-26 14:23:05 +0000

    MockConnectionHandler & Reader/Writer tests (#789)

--- workers/unity/Packages/com.improbable.gdk.core/Utility/EntityQuerySnapshot.cs
+++ workers/unity/Packages/com.improbable.gdk.core/Utility/EntityQuerySnapshot.cs
@@ -8,7 +8,7 @@ namespace Improbable.Gdk.Core
     ///     A  snapshot of a SpatialOS entity, containing the result of a entity query.
     /// </summary>
     /// <remarks>
-    ///     This copies entity components from <see cref="Improbable.Worker.CInterop.Entity"/> for long term storage.
+    ///     This copies entity components from <see cref="Improbable.Worker.CInterop.Entity" /> for long term storage.
     ///     This may only be a partial snapshot of an entity.
     ///     The components present depend on the component filter used when making the entity query.
     /// </remarks>
@@ -19,7 +19,8 @@ namespace Improbable.Gdk.Core
             public Entity EntitySnapshot;
             public Dictionary<uint, ISpatialComponentSnapshot> Components;
 
-            public void Accept<T>(uint componentId, DynamicSnapshot.SnapshotDeserializer<T> deserializeSnapshot, DynamicSnapshot.SnapshotSerializer<T> serializeSnapshot) where T : ISpatialComponentSnapshot
+            public void Accept<T>(uint componentId, DynamicSnapshot.SnapshotDeserializer<T> deserializeSnapshot,
+                DynamicSnapshot.SnapshotSerializer<T> serializeSnapshot) where T : struct, ISpatialComponentSnapshot
             {
                 var schemaObject = EntitySnapshot.Get(componentId).Value;
                 Components.Add(componentId, deserializeSnapshot(schemaObject));
---
+++ workers/unity/Packages/com.improbable.gdk.core/Worker/ConnectionHandlers/MockConnectionHandler.cs
@@ -0,0 +1,113 @@
+using Improbable.Worker.CInterop;
+
+namespace Improbable.Gdk.Core
+{
+    public class MockConnectionHandler : IConnectionHandler
+    {
+        private uint updateId;
+
+        private ViewDiff currentDiff => diffs[currentDiffIndex];
+
+        private readonly ViewDiff[] diffs = new[]
+        {
+            new ViewDiff(),
+            new ViewDiff()
+        };
+
+        private int currentDiffIndex = 0;
+
+        public void CreateEntity(long entityId, EntityTemplate template)
+        {
+            var handler = new EntityTemplateDynamicHandler(template, entityId, currentDiff);
+            DynamicConverter.ForEachComponent(handler);
+        }
+
+        public void ChangeAuthority(long entityId, uint componentId, Authority newAuthority)
+        {
+            currentDiff.SetAuthority(entityId, componentId, newAuthority);
+        }
+
+        public void UpdateComponent<T>(long entityId, uint componentId, T update) where T : ISpatialComponentUpdate
+        {
+            currentDiff.AddComponentUpdate(update, entityId, componentId, updateId++);
+        }
+
+        public void AddEvent<T>(long entityId, uint componentId, T ev) where T : IEvent
+        {
+            currentDiff.AddEvent(ev, entityId, componentId, updateId++);
+        }
+
+        public void UpdateComponentAndAddEvents<TUpdate, TEvent>(long entityId, uint componentId, TUpdate update,
+            params TEvent[] events)
+            where TUpdate : ISpatialComponentUpdate
+            where TEvent : IEvent
+        {
+            var thisUpdateId = updateId++;
+
+            currentDiff.AddComponentUpdate(update, entityId, componentId, thisUpdateId);
+            foreach (var ev in events)
+            {
+                currentDiff.AddEvent(ev, entityId, componentId, thisUpdateId);
+            }
+        }
+
+        // TODO: Commands
+
+        #region IConnectionHandler implementation
+
+        public ViewDiff GetMessagesReceived()
+        {
+            var diffToReturn = currentDiff;
+
+            currentDiffIndex = (currentDiffIndex + 1) % diffs.Length;
+            var nextDiff = currentDiff;
+            nextDiff.Clear();
+
+            return diffToReturn;
+        }
+
+        public void PushMessagesToSend(MessagesToSend messages)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsConnected()
+        {
+            return true;
+        }
+
+        #endregion
+
+        private class EntityTemplateDynamicHandler : DynamicConverter.IConverterHandler
+        {
+            private EntityTemplate template;
+            private ViewDiff viewDiff;
+            private long entityId;
+
+            public EntityTemplateDynamicHandler(EntityTemplate template, long entityId, ViewDiff viewDiff)
+            {
+                this.template = template;
+                this.viewDiff = viewDiff;
+                this.entityId = entityId;
+
+                viewDiff.AddEntity(this.entityId);
+            }
+
+            public void Accept<TSnapshot, TUpdate>(uint componentId,
+                DynamicConverter.SnapshotToUpdate<TSnapshot, TUpdate> snapshotToUpdate)
+                where TSnapshot : struct, ISpatialComponentSnapshot
+                where TUpdate : struct, ISpatialComponentUpdate
+            {
+                var maybeSnapshot = template.GetComponent<TSnapshot>();
+
+                if (!maybeSnapshot.HasValue)
+                {
+                    return;
+                }
+
+                var snapshot = maybeSnapshot.Value;
+                viewDiff.AddComponent(snapshotToUpdate(snapshot), entityId, componentId);
+            }
+        }
+    }
+}
