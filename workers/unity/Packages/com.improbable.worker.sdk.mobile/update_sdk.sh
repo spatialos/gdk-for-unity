1086d686cf1454d6a7672ac5eeadb1def35aac33
Martijn Gerkes <martijn@improbable.io>
2019-04-02 17:49:57 +0100

    Update scripts to download into the right folders

--- workers/unity/Packages/com.improbable.worker.sdk.mobile/update_sdk.sh
+++ workers/unity/Packages/com.improbable.worker.sdk.mobile/update_sdk.sh
@@ -1,7 +1,7 @@
 SDK_VERSION="13.6.2"
 
 spatial package get worker_sdk core-static-fullylinked-arm-ios $SDK_VERSION ./Plugins/Improbable/Core/iOS/arm --unzip -f
-spatial package get worker_sdk core-static-fullylinked-x86_64-ios $SDK_VERSION ./Plugins/Improbable/iOS/x86_64 --unzip -f
+spatial package get worker_sdk core-static-fullylinked-x86_64-ios $SDK_VERSION ./Plugins/Improbable/Core/iOS/x86_64 --unzip -f
 
 spatial package get worker_sdk core-dynamic-arm64-android $SDK_VERSION ./Plugins/Improbable/Core/Android/arm64 --unzip -f
 spatial package get worker_sdk core-dynamic-armeabi_v7a-android $SDK_VERSION ./Plugins/Improbable/Core/Android/armv7 --unzip -f
