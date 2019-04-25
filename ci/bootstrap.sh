31ca89af1b6454e8d96567e5018dbb9cc655dfb5
Peter Mounce <pete@neverrunwithscissors.com>
2019-04-25 10:33:58 +0100

    diagnostics

--- ci/bootstrap.sh
+++ ci/bootstrap.sh
@@ -8,8 +8,9 @@ CLONE_URL="git@github.com:spatialos/gdk-for-unity-shared-ci.git"
 
 # Clone the HEAD of the shared CI repo into ".shared-ci"
 
-if [ -d "${SHARED_CI_DIR}" ]; then
+if [[ -d "${SHARED_CI_DIR}" ]]; then
     rm -rf "${SHARED_CI_DIR}"
 fi
 
-git clone "${CLONE_URL}" "${SHARED_CI_DIR}"
+env
+git clone --verbose "${CLONE_URL}" "${SHARED_CI_DIR}"
