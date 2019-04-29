2aef0f63fa150644db2c07555d56bd5272910e20
Jamie Brynes <jamiebrynes@improbable.io>
2019-04-29 11:59:26 +0100

    try config changes

--- ci/bootstrap.sh
+++ ci/bootstrap.sh
@@ -12,6 +12,9 @@ if [[ -d "${SHARED_CI_DIR}" ]]; then
     rm -rf "${SHARED_CI_DIR}"
 fi
 
+git config --local remote.origin.receivepack "powershell git receive-pack"
+git config --local remote.origin.uploadpack "powershell git upload-pack"
+
 env | sort
 git config --global --includes --list | sort
 GIT_TRACE=1 GIT_TRACE_PACKET=1 GIT_TRACE_SETUP=1 GIT_CURL_VERBOSE=1 GIT_SSH_COMMAND='c:/Program\ Files/OpenSSH-Win64/ssh.exe -vvv' git clone --verbose --depth 1 "${CLONE_URL}" "${SHARED_CI_DIR}"
