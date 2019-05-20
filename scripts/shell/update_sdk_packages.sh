4054e0c8b0081bcc3e04d6b85b96cc1f81208a63
Paul Balaji <paulbalaji@improbable.io>
2019-05-20 14:48:22 +0100

    update sdk downloading scripts for 13.7.1

--- scripts/shell/update_sdk_packages.sh
+++ scripts/shell/update_sdk_packages.sh
@@ -3,7 +3,7 @@ set -e -u -o pipefail
 
 cd "$(dirname "$0")"
 
-SDK_VERSION="13.6.2"
+SDK_VERSION="13.7.1"
 
 PKG_ROOT="../../workers/unity/Packages"
 SDK_PATH="$PKG_ROOT/com.improbable.worker.sdk"
@@ -40,8 +40,8 @@ update_package tools schema_compiler-x86_64-linux "$SDK_PATH/.schema_compiler"
 update_package worker_sdk core-static-fullylinked-arm-ios "$SDK_MOBILE_PATH/Plugins/Improbable/Core/iOS/arm" "CoreSdkStatic.lib;libCoreSdkStatic.a.pic"
 update_package worker_sdk core-static-fullylinked-x86_64-ios "$SDK_MOBILE_PATH/Plugins/Improbable/Core/iOS/x86_64" "CoreSdkStatic.lib;libCoreSdkStatic.a.pic"
 
-update_package worker_sdk core-dynamic-arm64-android "$SDK_MOBILE_PATH/Plugins/Improbable/Core/Android/arm64"
-update_package worker_sdk core-dynamic-armeabi_v7a-android "$SDK_MOBILE_PATH/Plugins/Improbable/Core/Android/armv7"
-update_package worker_sdk core-dynamic-x86-android-android "$SDK_MOBILE_PATH/Plugins/Improbable/Core/Android/x86"
+update_package worker_sdk core-dynamic-arm64v8a-android "$SDK_MOBILE_PATH/Plugins/Improbable/Core/Android/arm64"
+update_package worker_sdk core-dynamic-armv7a-android "$SDK_MOBILE_PATH/Plugins/Improbable/Core/Android/armv7"
+update_package worker_sdk core-dynamic-x86-android "$SDK_MOBILE_PATH/Plugins/Improbable/Core/Android/x86"
 
 update_package worker_sdk csharp-c-interop-static "$SDK_MOBILE_PATH/Plugins/Improbable/Sdk/iOS"
