e6203995c813c128c6d2cfa1934dbe4a23f9871e
Paul Balaji <paulbalaji@improbable.io>
2019-05-21 15:15:20 +0100

    use a pinned commit from shared-ci

--- ci/bootstrap.sh
+++ ci/bootstrap.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 set -e -u -x -o pipefail
 
-cd "$(dirname "$0")/../"
-
 SHARED_CI_DIR="$(pwd)/.shared-ci"
 CLONE_URL="git@github.com:spatialos/gdk-for-unity-shared-ci.git"
+PINNED_SHARED_CI_VERSION=$(cat ./ci/shared-ci.pinned)
 
 # Clone the HEAD of the shared CI repo into ".shared-ci"
 
-if [[ -d "${SHARED_CI_DIR}" ]]; then
-    rm -rf "${SHARED_CI_DIR}"
-fi
+rm -rf "${SHARED_CI_DIR}"
 
-git clone --verbose --depth 1 "${CLONE_URL}" "${SHARED_CI_DIR}"
+mkdir "${SHARED_CI_DIR}"
+
+# Workaround for being unable to clone a specific commit with depth of 1.
+pushd "${SHARED_CI_DIR}"
+    git init
+    git remote add origin "${CLONE_URL}"
+    git fetch --depth 20 origin master
+    git checkout "${PINNED_SHARED_CI_VERSION}"
+popd
