48180909eb519a0027acb4bc7a95cbf162b85d96
Jakub Glowczyk <46607244+KazikBozia@users.noreply.github.com>
2019-07-05 22:48:43 +0200

    Update ScoreCollision.cs

--- workers/unity/Assets/Playground/Scripts/Cubes/ScoreCollision.cs
+++ workers/unity/Assets/Playground/Scripts/Cubes/ScoreCollision.cs
@@ -6,29 +6,5 @@ namespace Playground
     public class ScoreCollision : MonoBehaviour
     {
         private EntityManager entityManager;
-        // private SpatialOSComponent component;
-
-        private void Start()
-        {
-            // component = GetComponent<SpatialOSComponent>();
-            // entityManager = component.World.GetExistingManager<EntityManager>();
-        }
-
-        public void OnCollisionEnter(Collision col)
-        {
-            return;
-            // if (!col.gameObject || !col.gameObject.CompareTag("Cube"))
-            // {
-            //     return;
-            // }
-            //
-            // var otherComponent = col.gameObject.GetComponentInParent<SpatialOSComponent>();
-            // if (entityManager.HasComponent<Launchable.Component>(component.Entity)
-            //     && !entityManager.HasComponent<CollisionComponent>(component.Entity)
-            //     && otherComponent)
-            // {
-            //     entityManager.AddComponentData(component.Entity, new CollisionComponent(otherComponent.Entity));
-            // }
-        }
     }
 }
