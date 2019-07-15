8d96f9e9b575e81ccf562949651c1d31fb5bd0d7
Paul Balaji <paulbalaji@improbable.io>
2019-07-15 18:22:10 +0100

    Requireable WorkerId (#1016)

---
+++ workers/unity/Packages/io.improbable.gdk.core/Subscriptions/StandardSubscriptionManagers/WorkerIdSubscriptionManager.cs
@@ -0,0 +1,80 @@
+using System;
+using Improbable.Gdk.Core;
+using Unity.Entities;
+
+namespace Improbable.Gdk.Subscriptions
+{
+    [AutoRegisterSubscriptionManager]
+    public class WorkerIdSubscriptionManager : SubscriptionManager<WorkerId>
+    {
+        private readonly World world;
+        private Subscription<WorkerId> cachedSubscription;
+
+        public WorkerIdSubscriptionManager(World world)
+        {
+            this.world = world;
+        }
+
+        public override Subscription<WorkerId> Subscribe(EntityId entityId)
+        {
+            if (cachedSubscription != null)
+            {
+                return cachedSubscription;
+            }
+
+            cachedSubscription = new Subscription<WorkerId>(this, new EntityId(0));
+            var workerId = world.GetExistingSystem<WorkerSystem>().WorkerId;
+            cachedSubscription.SetAvailable(new WorkerId(workerId));
+
+            return cachedSubscription;
+        }
+
+        public override void Cancel(ISubscription subscription)
+        {
+        }
+
+        public override void ResetValue(ISubscription subscription)
+        {
+        }
+    }
+
+    public readonly struct WorkerId : IEquatable<string>, IEquatable<WorkerId>
+    {
+        public readonly string Id;
+
+        public WorkerId(string id)
+        {
+            Id = id;
+        }
+
+        public bool Equals(WorkerId other)
+        {
+            if (Id == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            return other.Id != null && Id.Equals(other.Id);
+        }
+
+        public bool Equals(string other)
+        {
+            if (Id == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            return other != null && Id.Equals(other);
+        }
+
+        public override string ToString()
+        {
+            return Id;
+        }
+
+        public static implicit operator string(WorkerId workerId)
+        {
+            return workerId.Id;
+        }
+    }
+}
