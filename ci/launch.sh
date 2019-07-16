21e339ec806b8d6885721f5cc7e63fb71844fb7b
Jamie Brynes <jamiebrynes@improbable.io>
2019-07-16 16:10:25 +0100

    launch script

--- ci/launch.sh
+++ ci/launch.sh
@@ -6,27 +6,22 @@ cd "$(dirname "$0")/../"
 
 source ".shared-ci/scripts/pinned-tools.sh"
 
-if [[ -n "${BUILDKITE-}" ]]; then
-    # In buildkite, download the artifacts and reconstruct the build/assemblies folder.
-    buildkite-agent artifact download "build\assembly\**\*" .
-else
-    # In TeamCity, just build.
-    ci/build-test.sh
-fi
+buildkite-agent artifact download "build\assembly\**\*" .
 
 uploadAssembly "${ASSEMBLY_PREFIX}" "${PROJECT_NAME}"
 
 echo "Launching deployment"
 
-spatial cloud launch "${ASSEMBLY_NAME}" cloud_launch.json "${ASSEMBLY_NAME}" --snapshot=snapshots/default.snapshot | tee -a ./launch.log
+dotnet run -p workers/unity/Packages/io.improbable.gdk.deploymentlauncher/.DeploymentLauncher/DeploymentLauncher.csproj -- \
+    create \
+    --project_name "${PROJECT_NAME}" \
+    --assembly_name "${ASSEMBLY_NAME}" \
+    --deployment_name "${ASSEMBLY_NAME}" \
+    --launch_json_path cloud_launch.json \
+    --snapshot_path snapshots/default.snapshot \
+    --region EU \
+    --tags "dev_login"
 
-if [[ -n "${BUILDKITE-}" ]]; then
-    CONSOLE_REGEX='.*Console URL:(.*)\\n"'
-    LAUNCH_LOG=$(cat ./launch.log)
-    if [[ $LAUNCH_LOG =~ $CONSOLE_REGEX ]]; then
-        CONSOLE_URL=${BASH_REMATCH[1]}
-        buildkite-agent annotate --style "success" "Deployment URL: ${CONSOLE_URL}"
-    else
-        buildkite-agent annotate --style "warning" "Could not parse deployment URL from launch log."
-    fi
-fi
+CONSOLE_URL="https://console.improbable.io/projects/${PROJECT_NAME}/deployments/${ASSEMBLY_NAME}/overview"
+
+buildkite-agent annotate --style "success" "Deployment URL: ${CONSOLE_URL}<br/>"
