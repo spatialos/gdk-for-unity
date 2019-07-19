afa3d669d550e9e015792afba6a79a90ff31c5ad
Martijn Gerkes <martijn@improbable.io>
2019-07-19 12:26:36 +0100

    Move the inspector to its own feature module package Upgrade to 0.2.5

--- workers/unity/Packages/io.improbable.gdk.debug/MonoBehaviourInspector.cs
+++ workers/unity/Packages/io.improbable.gdk.debug/MonoBehaviourInspector.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using Improbable.Gdk.Tools;
 using UnityEditor;
 using UnityEngine;
 
 namespace Improbable.Gdk.Subscriptions.Editor
 {
     [CustomEditor(typeof(MonoBehaviour), true)]
-    [CanEditMultipleObjects]
-    public class CustomBehaviourInspector : UnityEditor.Editor
+    public class MonoBehaviourInspector : UnityEditor.Editor
     {
         private MonoBehaviour script;
         private Type scriptType;
@@ -21,8 +21,8 @@ namespace Improbable.Gdk.Subscriptions.Editor
         private string workerType;
         private bool? isWorkerType;
 
-        private GUIStyle availableStyle;
-        private GUIStyle unavailableStyle;
+        private static GUIStyle availableStyle;
+        private static GUIStyle unavailableStyle;
 
         private bool foldout;
 
@@ -33,9 +33,15 @@ namespace Improbable.Gdk.Subscriptions.Editor
             unavailableStyle = new GUIStyle(EditorStyles.label) { normal = { textColor = Color.red } };
 
             // Get type info
-            script = target as MonoBehaviour;
+            script = (MonoBehaviour) target;
             scriptType = script.GetType();
             isSpatialBehaviour = HasWorkerTypeAttribute(scriptType) || HasRequireAttributes(scriptType);
+            var requiredWorkerTypes = GetRequiredWorkerTypes(scriptType);
+
+            subscriptions = new Dictionary<FieldInfo, ISubscription>();
+            var requiredFields = scriptType
+                .GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public)
+                .Where(field => Attribute.IsDefined(field, typeof(RequireAttribute), false));
 
             // Get subscription info when playing
             if (Application.isPlaying)
@@ -48,20 +54,30 @@ namespace Improbable.Gdk.Subscriptions.Editor
                 }
 
                 workerType = linkedEntityComponent.Worker.WorkerType;
-                var requiredWorkerTypes = GetRequiredWorkerTypes(scriptType);
                 isWorkerType = requiredWorkerTypes?.Contains(workerType);
                 if (isWorkerType.HasValue)
                 {
                     requiredWorkerTypesLabel = string.Join(" || ", requiredWorkerTypes);
                 }
 
-                subscriptions = new Dictionary<FieldInfo, ISubscription>();
                 var subscriptionSystem = linkedEntityComponent.World.GetExistingSystem<SubscriptionSystem>();
-                foreach (var fieldInfo in scriptType.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public)
-                    .Where(field => Attribute.IsDefined(field, typeof(RequireAttribute), false)))
+
+                foreach (var fieldInfo in requiredFields)
                 {
                     var subscription = subscriptionSystem.Subscribe(linkedEntityComponent.EntityId, fieldInfo.FieldType);
                     subscriptions.Add(fieldInfo, subscription);
+                }
+            }
+            else
+            {
+                if (requiredWorkerTypes != null)
+                {
+                    requiredWorkerTypesLabel = string.Join(" || ", requiredWorkerTypes);
+                }
+
+                foreach (var fieldInfo in requiredFields)
+                {
+                    subscriptions.Add(fieldInfo, null);
                 }
             }
         }
@@ -73,7 +89,7 @@ namespace Improbable.Gdk.Subscriptions.Editor
                 foreach (var pair in subscriptions)
                 {
                     var subscription = pair.Value;
-                    subscription.Cancel();
+                    subscription?.Cancel();
                 }
             }
         }
@@ -82,35 +98,67 @@ namespace Improbable.Gdk.Subscriptions.Editor
         {
             if (isSpatialBehaviour)
             {
-                foldout = EditorGUILayout.Foldout(foldout, "Spatial MonoBehaviour");
-                if (foldout && Application.isPlaying && subscriptions != null)
+                foldout = EditorGUILayout.Foldout(foldout, "SpatialOS", true);
+                if (foldout)
                 {
                     using (new EditorGUI.IndentLevelScope())
                     {
-                        if (isWorkerType.HasValue)
+                        if (Application.isPlaying)
                         {
-                            if (isWorkerType.Value)
-                            {
-                                EditorGUILayout.LabelField(workerType, availableStyle);
-                            }
-                            else
-                            {
-                                EditorGUILayout.LabelField(requiredWorkerTypesLabel, unavailableStyle);
-                            }
+                            DrawLiveInspector();
                         }
-
-                        foreach (var pair in subscriptions)
+                        else
                         {
-                            var field = pair.Key;
-                            var subscription = pair.Value;
-                            EditorGUILayout.LabelField(field.FieldType.Name,
-                                subscription.HasValue ? availableStyle : unavailableStyle);
+                            DrawEditorInspector();
                         }
                     }
                 }
             }
 
             DrawDefaultInspector();
+        }
+
+        private void DrawEditorInspector()
+        {
+            if (!string.IsNullOrEmpty(requiredWorkerTypesLabel))
+            {
+                EditorGUILayout.LabelField(requiredWorkerTypesLabel);
+            }
+
+            if (subscriptions != null)
+            {
+                foreach (var pair in subscriptions)
+                {
+                    var field = pair.Key;
+                    EditorGUILayout.LabelField(field.FieldType.Name);
+                }
+            }
+        }
+
+        private void DrawLiveInspector()
+        {
+            if (isWorkerType.HasValue)
+            {
+                if (isWorkerType.Value)
+                {
+                    EditorGUILayout.LabelField(workerType, availableStyle);
+                }
+                else
+                {
+                    EditorGUILayout.LabelField(requiredWorkerTypesLabel, unavailableStyle);
+                }
+            }
+
+            if (subscriptions != null)
+            {
+                foreach (var pair in subscriptions)
+                {
+                    var field = pair.Key;
+                    var subscription = pair.Value;
+                    EditorGUILayout.LabelField(field.FieldType.Name,
+                        subscription.HasValue ? availableStyle : unavailableStyle);
+                }
+            }
         }
 
         private static string[] GetRequiredWorkerTypes(Type targetType)
