a28278881352345342b253eb9287e329f341263d
Jamie Brynes <jamiebrynes@improbable.io>
2019-07-22 15:24:49 +0100

    fix tests

--- test-project/Assets/EditmodeTests/Components/ComponentWithNoFieldsTests.cs
+++ test-project/Assets/EditmodeTests/Components/ComponentWithNoFieldsTests.cs
@@ -1,5 +1,5 @@
 using System;
-using Improbable.Gdk.Tests.ComponentsWithNoFields;
+using Improbable.Gdk.Test;
 using NUnit.Framework;
 
 namespace Improbable.Gdk.EditmodeTests.Ecs
@@ -10,14 +10,14 @@ namespace Improbable.Gdk.EditmodeTests.Ecs
         [Test]
         public void IsDataDirty_returns_false_for_component_with_no_fields()
         {
-            var component = new ComponentWithNoFields.Component();
+            var component = new Empty.Component();
             Assert.IsFalse(component.IsDataDirty());
         }
 
         [Test]
         public void IsDataDirty_with_propertyIndex_throws_for_component_with_no_fields()
         {
-            var component = new ComponentWithNoFields.Component();
+            var component = new Empty.Component();
             Assert.Throws<InvalidOperationException>(() =>
             {
                 component.IsDataDirty(0);
@@ -31,7 +31,7 @@ namespace Improbable.Gdk.EditmodeTests.Ecs
         [Test]
         public void MarkDataDirty_throws_for_component_with_no_fields()
         {
-            var component = new ComponentWithNoFields.Component();
+            var component = new Empty.Component();
             Assert.Throws<InvalidOperationException>(() =>
             {
                 component.IsDataDirty(0);
