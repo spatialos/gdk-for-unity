6f31a188bb725c9718248e3c7ce6e75c7f97dd54
Jamie Brynes <jamiebrynes@improbable.io>
2019-07-22 12:25:13 +0100

    make entityquerysnapshot more general

---
+++ workers/unity/Packages/io.improbable.gdk.core/Utility/EntitySnapshot.cs
@@ -0,0 +1,137 @@
+using System.Collections.Generic;
+using Improbable.Worker.CInterop;
+
+namespace Improbable.Gdk.Core
+{
+    /// <summary>
+    ///     A snapshot of a SpatialOS entity.
+    /// </summary>
+    /// <remarks>
+    ///     This copies entity components from <see cref="Improbable.Worker.CInterop.Entity" /> for long term storage.
+    ///     This may only be a partial snapshot of an entity.
+    ///     The components present depend on the component filter used when making the entity query.
+    /// </remarks>
+    public struct EntitySnapshot
+    {
+        private readonly Dictionary<uint, ISpatialComponentSnapshot> components;
+
+        /// <summary>
+        ///     Get the SpatialOS component snapshot if present.
+        /// </summary>
+        /// <returns> The component snapshot, if it exists, or null otherwise.</returns>
+        /// <typeparam name="T">The component type.</typeparam>
+        public T? GetComponentSnapshot<T>() where T : struct, ISpatialComponentSnapshot
+        {
+            var id = Dynamic.GetSnapshotComponentId<T>();
+            if (components.TryGetValue(id, out var data))
+            {
+                return (T) data;
+            }
+
+            return null;
+        }
+
+        public void AddComponent<T>(T component) where T : struct, ISpatialComponentSnapshot
+        {
+            components[component.ComponentId] = component;
+        }
+
+        internal EntitySnapshot(Entity entitySnapshot)
+        {
+            components = new Dictionary<uint, ISpatialComponentSnapshot>();
+
+            var componentDataHandler = new QueryComponentDataHandler
+            {
+                EntitySnapshot = entitySnapshot,
+                Components = components
+            };
+
+            foreach (var componentId in entitySnapshot.GetComponentIds())
+            {
+                Dynamic.ForComponent(componentId, componentDataHandler);
+            }
+        }
+
+        internal EntitySnapshot(SchemaObject entityObject)
+        {
+            var handler = new SchemaEntityDeserializer(entityObject);
+            var ids = entityObject.GetUniqueFieldIds();
+
+            foreach (var id in ids)
+            {
+                Dynamic.ForComponent(id, handler);
+            }
+
+            components = handler.Components;
+        }
+
+        internal void SerializeToSchemaObject(SchemaObject inObj)
+        {
+            var handler = new SchemaEntitySerializer(this, inObj);
+
+            foreach (var componentId in components.Keys)
+            {
+                Dynamic.ForComponent(componentId, handler);
+            }
+        }
+    }
+
+    internal struct QueryComponentDataHandler : Dynamic.IHandler
+    {
+        public Entity EntitySnapshot;
+        public Dictionary<uint, ISpatialComponentSnapshot> Components;
+
+        public void Accept<TData, TUpdate, TSnapshot>(uint componentId, Dynamic.VTable<TData, TUpdate, TSnapshot> vtable)
+            where TData : struct, ISpatialComponentData
+            where TUpdate : struct, ISpatialComponentUpdate
+            where TSnapshot : struct, ISpatialComponentSnapshot
+        {
+            var schemaObject = EntitySnapshot.Get(componentId).Value;
+            Components.Add(componentId, vtable.DeserializeSnapshot(schemaObject));
+        }
+    }
+
+    internal struct SchemaEntityDeserializer : Dynamic.IHandler
+    {
+        public readonly Dictionary<uint, ISpatialComponentSnapshot> Components;
+
+        private SchemaObject entityObject;
+
+        public SchemaEntityDeserializer(SchemaObject entityObject)
+        {
+            Components = new Dictionary<uint, ISpatialComponentSnapshot>();
+            this.entityObject = entityObject;
+        }
+
+        public void Accept<TData, TUpdate, TSnapshot>(uint componentId, Dynamic.VTable<TData, TUpdate, TSnapshot> vtable)
+            where TData : struct, ISpatialComponentData
+            where TUpdate : struct, ISpatialComponentUpdate
+            where TSnapshot : struct, ISpatialComponentSnapshot
+        {
+            var componentObject = entityObject.GetObject(componentId);
+            Components[componentId] = vtable.DeserializeSnapshotRaw(componentObject);
+        }
+    }
+
+    internal struct SchemaEntitySerializer : Dynamic.IHandler
+    {
+        private EntitySnapshot entitySnapshot;
+        private SchemaObject targetObject;
+
+        public SchemaEntitySerializer(EntitySnapshot entitySnapshot, SchemaObject targetObject)
+        {
+            this.entitySnapshot = entitySnapshot;
+            this.targetObject = targetObject;
+        }
+
+        public void Accept<TData, TUpdate, TSnapshot>(uint componentId, Dynamic.VTable<TData, TUpdate, TSnapshot> vtable)
+            where TData : struct, ISpatialComponentData
+            where TUpdate : struct, ISpatialComponentUpdate
+            where TSnapshot : struct, ISpatialComponentSnapshot
+        {
+            // Okay to grab the value directly, we only call this for snapshots that actually exist.
+            var data = entitySnapshot.GetComponentSnapshot<TSnapshot>().Value;
+            vtable.SerializeSnapshotRaw(data, targetObject.AddObject(componentId));
+        }
+    }
+}
