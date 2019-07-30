79080852565bc9e08d0d5c4d24eaa9d4875e4396
Jamie Brynes <jamiebrynes@improbable.io>
2019-07-30 15:06:48 +0100

    update helper

--- workers/unity/Packages/io.improbable.gdk.transformsynchronization2d/TransformSynchronizationHelper2D.cs
+++ workers/unity/Packages/io.improbable.gdk.transformsynchronization2d/TransformSynchronizationHelper2D.cs
@@ -1,8 +1,4 @@
-using Improbable.Gdk.Core;
-using Improbable.Worker.CInterop;
 using Unity.Entities;
-using UnityEngine;
-using Entity = Unity.Entities.Entity;
 
 namespace Improbable.Gdk.TransformSynchronization
 {
@@ -10,70 +6,19 @@ namespace Improbable.Gdk.TransformSynchronization
     {
         public static void Enable2DMode(World world)
         {
+            var syncImpl = new Rigidbody2DTransformSync();
+
             var defaultApplyLatestTransformSystem = world.GetOrCreateSystem<DefaultApplyLatestTransformSystem>();
-            defaultApplyLatestTransformSystem.RegisterType<Rigidbody2D>(
-                (ref TransformToSet transformToSet, Rigidbody2D rigidbody) =>
-                {
-                    rigidbody.MovePosition(transformToSet.Position);
-                    rigidbody.MoveRotation(transformToSet.Orientation);
-                    rigidbody.AddForce((Vector2) transformToSet.Velocity - rigidbody.velocity, ForceMode2D.Impulse);
-                });
+            defaultApplyLatestTransformSystem.RegisterType(syncImpl);
 
             var defaultUpdateLatestTransformSystem = world.GetOrCreateSystem<DefaultUpdateLatestTransformSystem>();
-            defaultUpdateLatestTransformSystem.RegisterType<Rigidbody2D>(
-                (ref TransformToSend transformToSend, Rigidbody2D rigidbody) =>
-                {
-                    transformToSend = new TransformToSend
-                    {
-                        Position = rigidbody.position,
-                        Velocity = rigidbody.velocity,
-                        Orientation = Quaternion.Euler(0, 0, rigidbody.rotation)
-                    };
-                });
+            defaultUpdateLatestTransformSystem.RegisterType(syncImpl);
 
             var authorityGainedSystem = world.GetOrCreateSystem<ResetForAuthorityGainedSystem>();
-            authorityGainedSystem.RegisterType<Rigidbody2D>((
-                WorkerSystem worker,
-                Entity entity,
-                ref TransformInternal.Component transformInternal,
-                Rigidbody2D rigidbody) =>
-            {
-                rigidbody.MovePosition(transformInternal.Location.ToUnityVector() + worker.Origin);
-                rigidbody.MoveRotation(transformInternal.Rotation.ToUnityQuaternion());
-                rigidbody.AddForce((Vector2) transformInternal.Velocity.ToUnityVector() - rigidbody.velocity,
-                    ForceMode2D.Impulse);
-            });
+            authorityGainedSystem.RegisterType(syncImpl);
 
             var kinematicSystem = world.GetOrCreateSystem<SetKinematicFromAuthoritySystem>();
-            kinematicSystem.RegisterType<Rigidbody2D>(
-                (ref KinematicStateWhenAuth kinematicStateWhenAuth,
-                    Rigidbody2D rigidbody) =>
-                {
-                    kinematicStateWhenAuth = new KinematicStateWhenAuth
-                    {
-                        KinematicWhenAuthoritative = rigidbody.isKinematic
-                    };
-
-                    rigidbody.isKinematic = true;
-                },
-                (ref KinematicStateWhenAuth kinematicStateWhenAuth,
-                    AuthorityChangeReceived auth,
-                    Rigidbody2D rigidbody) =>
-                {
-                    switch (auth.Authority)
-                    {
-                        case Authority.NotAuthoritative:
-                            kinematicStateWhenAuth = new KinematicStateWhenAuth
-                            {
-                                KinematicWhenAuthoritative = rigidbody.isKinematic
-                            };
-                            rigidbody.isKinematic = true;
-                            break;
-                        case Authority.Authoritative:
-                            rigidbody.isKinematic = kinematicStateWhenAuth.KinematicWhenAuthoritative;
-                            break;
-                    }
-                });
+            kinematicSystem.RegisterType(syncImpl);
         }
     }
 }
