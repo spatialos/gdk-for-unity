65d61ab7b22a9089025147dbe609883daa0fe57e
Jamie Brynes <jamiebrynes@improbable.io>
2019-08-12 12:31:43 +0100

    typo

--- init.sh
+++ init.sh
@@ -9,7 +9,7 @@ SDK_MOBILE_PATH="${PKG_ROOT}/io.improbable.worker.sdk.mobile"
 TEST_SDK_PATH="test-project/Packages/io.improbable.worker.sdk.testschema"
 
 SDK_VERSION="$(cat "${SDK_PATH}"/package.json | jq -r '.version')"
-SPOT_VERSION="$(cat "${SDK_PATH"/.spot.version)"
+SPOT_VERSION="$(cat "${SDK_PATH}"/.spot.version)"
 
 update_package() {
     local type=$1
