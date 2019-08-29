2bdb6ee5b09b68d27ddc0dfb2b5489ee6001d8c4
Jamie Brynes <jamiebrynes@improbable.io>
2019-08-29 15:35:55 +0100

    network stats backing object store

--- workers/unity/Packages/io.improbable.gdk.core/NetworkStats/NetStats.cs
+++ workers/unity/Packages/io.improbable.gdk.core/NetworkStats/NetStats.cs
@@ -1,159 +1,193 @@
+using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using Improbable.Worker.CInterop;
 
 namespace Improbable.Gdk.Core.NetworkStats
 {
     public class NetStats
     {
-        public enum WorldCommand
+        private readonly int sequenceLength;
+        private readonly Dictionary<MessageTypeUnion, DataSequence> Data = new Dictionary<MessageTypeUnion, DataSequence>();
+        private int nextFrameIndex;
+
+        public NetStats(int sequenceLength)
         {
-            CreateEntity = 1,
-            DeleteEntity = 2,
-            ReserveEntityIds = 3,
-            EntityQuery = 4
+            this.sequenceLength = sequenceLength;
+            nextFrameIndex = sequenceLength - 1;
         }
 
-        public IReadOnlyDictionary<uint, DataPoint> Updates => updates;
-        public IReadOnlyDictionary<(uint, uint), DataPoint> CommandRequests => commandRequests;
-        public IReadOnlyDictionary<(uint, uint), DataPoint> CommandResponses => commandResponses;
-        public IReadOnlyDictionary<WorldCommand, DataPoint> WorldCommandRequests => worldCommandRequests;
-        public IReadOnlyDictionary<WorldCommand, DataPoint> WorldCommandResponses => worldCommandResponses;
-
-        private readonly Dictionary<uint, DataPoint> updates = new Dictionary<uint, DataPoint>();
-        private readonly Dictionary<(uint, uint), DataPoint> commandRequests = new Dictionary<(uint, uint), DataPoint>();
-        private readonly Dictionary<(uint, uint), DataPoint> commandResponses = new Dictionary<(uint, uint), DataPoint>();
-        private readonly Dictionary<WorldCommand, DataPoint> worldCommandRequests = new Dictionary<WorldCommand, DataPoint>();
-        private readonly Dictionary<WorldCommand, DataPoint> worldCommandResponses = new Dictionary<WorldCommand, DataPoint>();
-
-        private NetStats()
+        public void AddFrame(NetFrameStats frameData, Direction direction)
         {
-        }
-
-        [Conditional("UNITY_EDITOR")]
-        public void AddUpdate(in ComponentUpdate update)
-        {
-            var componentId = update.ComponentId;
-            var size = update.SchemaData.Value.GetFields().GetWriteBufferLength() +
-                update.SchemaData.Value.GetEvents().GetWriteBufferLength();
-
-            updates.TryGetValue(componentId, out var metrics);
-            metrics.Count += 1;
-            metrics.Size += size;
-            updates[componentId] = metrics;
-        }
-
-        [Conditional("UNITY_EDITOR")]
-        public void AddCommandRequest(in CommandRequest request)
-        {
-            var componentId = request.ComponentId;
-            var commandIndex = request.CommandIndex;
-            var size = request.SchemaData.Value.GetObject().GetWriteBufferLength();
-
-            var key = (componentId, commandIndex);
-            commandRequests.TryGetValue(key, out var metrics);
-            metrics.Count += 1;
-            metrics.Size += size;
-            commandRequests[key] = metrics;
-        }
-
-        [Conditional("UNITY_EDITOR")]
-        public void AddCommandResponse(in CommandResponse response, string message)
-        {
-            var componentId = response.ComponentId;
-            var commandIndex = response.CommandIndex;
-            uint size;
-
-            if (response.SchemaData.HasValue)
+            // First we need to zero out the data before processing the _sparse_ data in the NetFrameStats.
+            foreach (var pair in Data)
             {
-                size = response.SchemaData.Value.GetObject().GetWriteBufferLength();
-            }
-            else
-            {
-                // Approximation of on-wire size.
-                size = (uint) System.Text.Encoding.UTF8.GetByteCount(message);
+                pair.Value.Clear(nextFrameIndex, direction);
             }
 
-            var key = (componentId, commandIndex);
-            commandResponses.TryGetValue(key, out var metrics);
-            metrics.Count += 1;
-            metrics.Size += size;
-            commandResponses[key] = metrics;
-        }
-
-        [Conditional("UNITY_EDITOR")]
-        public void AddWorldCommandRequest(WorldCommand command)
-        {
-            worldCommandRequests.TryGetValue(command, out var metrics);
-            metrics.Count += 1;
-            worldCommandRequests[command] = metrics;
-        }
-
-        [Conditional("UNITY_EDITOR")]
-        public void AddWorldCommandResponse(WorldCommand command)
-        {
-            worldCommandResponses.TryGetValue(command, out var metrics);
-            metrics.Count += 1;
-            worldCommandResponses[command] = metrics;
-        }
-
-        internal void CopyFrom(NetStats other)
-        {
-            Clear();
-
-            foreach (var pair in other.updates)
+            foreach (var pair in frameData.Updates)
             {
-                updates[pair.Key] = pair.Value;
-            }
-
-            foreach (var pair in other.commandRequests)
-            {
-                commandRequests[pair.Key] = pair.Value;
-            }
-
-            foreach (var pair in other.commandResponses)
-            {
-                commandResponses[pair.Key] = pair.Value;
-            }
-
-            foreach (var pair in other.worldCommandRequests)
-            {
-                worldCommandRequests[pair.Key] = pair.Value;
-            }
-
-            foreach (var pair in other.worldCommandResponses)
-            {
-                worldCommandResponses[pair.Key] = pair.Value;
-            }
-        }
-
-        internal void Clear()
-        {
-            updates.Clear();
-            commandRequests.Clear();
-            commandResponses.Clear();
-            worldCommandRequests.Clear();
-            worldCommandResponses.Clear();
-        }
-
-        public static class Pool
-        {
-            private static readonly Queue<NetStats> data = new Queue<NetStats>();
-
-            public static NetStats Rent()
-            {
-                if (data.Count != 0)
+                var type = MessageTypeUnion.Update(pair.Key);
+                if (!Data.TryGetValue(type, out var data))
                 {
-                    return data.Dequeue();
+                    data = new DataSequence(sequenceLength);
                 }
 
-                return new NetStats();
+                data.AddFrame(nextFrameIndex, pair.Value, direction);
+                Data[type] = data;
             }
 
-            public static void Return(NetStats stats)
+            foreach (var pair in frameData.CommandRequests)
             {
-                stats.Clear();
-                data.Enqueue(stats);
+                var type = MessageTypeUnion.CommandRequest(pair.Key.Item1, pair.Key.Item2);
+                if (!Data.TryGetValue(type, out var data))
+                {
+                    data = new DataSequence(sequenceLength);
+                }
+
+                data.AddFrame(nextFrameIndex, pair.Value, direction);
+                Data[type] = data;
+            }
+
+            foreach (var pair in frameData.CommandResponses)
+            {
+                var type = MessageTypeUnion.CommandResponse(pair.Key.Item1, pair.Key.Item2);
+                if (!Data.TryGetValue(type, out var data))
+                {
+                    data = new DataSequence(sequenceLength);
+                }
+
+                data.AddFrame(nextFrameIndex, pair.Value, direction);
+                Data[type] = data;
+            }
+
+            foreach (var pair in frameData.WorldCommandRequests)
+            {
+                var type = MessageTypeUnion.WorldCommandRequest(pair.Key);
+                if (!Data.TryGetValue(type, out var data))
+                {
+                    data = new DataSequence(sequenceLength);
+                }
+
+                data.AddFrame(nextFrameIndex, pair.Value, direction);
+                Data[type] = data;
+            }
+
+            foreach (var pair in frameData.WorldCommandResponses)
+            {
+                var type = MessageTypeUnion.WorldCommandResponse(pair.Key);
+                if (!Data.TryGetValue(type, out var data))
+                {
+                    data = new DataSequence(sequenceLength);
+                }
+
+                data.AddFrame(nextFrameIndex, pair.Value, direction);
+                Data[type] = data;
+            }
+
+            if (--nextFrameIndex == -1)
+            {
+                nextFrameIndex += sequenceLength;
+            }
+        }
+
+        public DataPoint GetSummary(MessageTypeUnion messageType, int numFrames, Direction direction)
+        {
+            if (!Data.TryGetValue(messageType, out var data))
+            {
+                return new DataPoint();
+            }
+
+            DataPoint[] frameData;
+
+            switch (direction)
+            {
+                case Direction.Incoming:
+                    frameData = data.Incoming;
+                    break;
+                case Direction.Outgoing:
+                    frameData = data.Outgoing;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
+            }
+
+            var summary = new DataPoint();
+
+            for (var i = 1; i <= numFrames; i++)
+            {
+                var index = (nextFrameIndex + i) % sequenceLength;
+                summary += frameData[index];
+            }
+
+            return summary;
+        }
+
+        private struct DataSequence : IEquatable<DataSequence>
+        {
+            public readonly DataPoint[] Incoming;
+            public readonly DataPoint[] Outgoing;
+
+            public DataSequence(int size)
+            {
+                Incoming = new DataPoint[size];
+                Outgoing = new DataPoint[size];
+            }
+
+            public void AddFrame(int index, DataPoint data, Direction direction)
+            {
+                switch (direction)
+                {
+                    case Direction.Incoming:
+                        Incoming[index] = data;
+                        break;
+                    case Direction.Outgoing:
+                        Outgoing[index] = data;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
+                }
+            }
+
+            public void Clear(int index, Direction direction)
+            {
+                switch (direction)
+                {
+                    case Direction.Incoming:
+                        Incoming[index] = new DataPoint();
+                        break;
+                    case Direction.Outgoing:
+                        Outgoing[index] = new DataPoint();
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
+                }
+            }
+
+            public bool Equals(DataSequence other)
+            {
+                return Equals(Incoming, other.Incoming) && Equals(Outgoing, other.Outgoing);
+            }
+
+            public override bool Equals(object obj)
+            {
+                return obj is DataSequence other && Equals(other);
+            }
+
+            public override int GetHashCode()
+            {
+                unchecked
+                {
+                    return ((Incoming != null ? Incoming.GetHashCode() : 0) * 397) ^ (Outgoing != null ? Outgoing.GetHashCode() : 0);
+                }
+            }
+
+            public static bool operator ==(DataSequence left, DataSequence right)
+            {
+                return left.Equals(right);
+            }
+
+            public static bool operator !=(DataSequence left, DataSequence right)
+            {
+                return !left.Equals(right);
             }
         }
     }
