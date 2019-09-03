ea9d50b751015715c02a1bff52f886c653977474
Paul Balaji <paulbalaji@improbable.io>
2019-09-03 15:26:40 +0200

    Worker SDK 14.0.2 Upgrade (#1143)

--- init.ps1
+++ init.ps1
@@ -5,8 +5,7 @@ $SdkPath = $PkgRoot + "/io.improbable.worker.sdk"
 $SdkMobilePath = $PkgRoot + "/io.improbable.worker.sdk.mobile"
 $TestSdkPath="test-project/Packages/io.improbable.worker.sdk.testschema"
 
-$SdkVersion = "14.0.1-b1352-614f42b-WORKER-SNAPSHOT"
-# $SdkVersion = Get-Content ($SdkPath + "/package.json") | jq -r '.version'
+$SdkVersion = Get-Content ($SdkPath + "/package.json") | jq -r '.version'
 $SpotVersion = Get-Content ($SdkPath + "/.spot.version")
 
 function UpdatePackage($type, $identifier, $path, $removes)
