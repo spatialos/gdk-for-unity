ad155e7102ca522d125aed4cd648814f094d8240
Martijn Gerkes <martijn@improbable.io>
2019-09-05 17:02:21 +0100

    Refactor Updates list to allocate less. Change from List to ScrollView for better performance

--- workers/unity/Packages/io.improbable.gdk.debug/NetStatViewer.cs
+++ workers/unity/Packages/io.improbable.gdk.debug/NetStatViewer.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Improbable.Gdk.Core;
 using Improbable.Gdk.Core.NetworkStats;
 using Unity.Entities;
 using UnityEditor;
+using UnityEditor.UIElements;
 using UnityEngine.UIElements;
 
 namespace Improbable.Gdk.Debug
@@ -16,50 +18,69 @@ namespace Improbable.Gdk.Debug
         private World selectedWorld;
         private NetworkStatisticsSystem netStatSystem;
         private List<(string Name, uint ComponentId)> spatialComponents;
-
-        private static bool visible;
+        private Dictionary<int, VisualElement> listElements;
 
         [MenuItem("SpatialOS/Network Analyzer &n", false, 52)]
         public static void ShowWindow()
         {
             var inspectorWindowType = typeof(EditorWindow).Assembly.GetType("UnityEditor.InspectorWindow");
             var window = GetWindow<NetStatViewer>(inspectorWindowType);
-
-            if (visible)
-            {
-                window.Close();
-                window = GetWindow<NetStatViewer>(inspectorWindowType);
-            }
-
             window.titleContent.text = "Network Analyzer";
             window.titleContent.tooltip = "Tooltip";
             window.Show();
         }
 
-        public void OnDestroy()
-        {
-            visible = false;
-        }
-
         private void OnEnable()
         {
-            // Hack
-            selectedWorld = World.AllWorlds.FirstOrDefault(w => w.GetExistingSystem<NetworkStatisticsSystem>() != null);
-            if (selectedWorld == null)
-            {
-                return;
-            }
-
-            netStatSystem = selectedWorld.GetExistingSystem<NetworkStatisticsSystem>();
-
             // Generate list of types
             spatialComponents = ComponentDatabase.ComponentsToIds
                 .Select(pair => (pair.Key.DeclaringType.Name, pair.Value))
                 .ToList();
 
+            // Load UI
             SetupUI();
+            SetupWorldSelection();
+        }
 
-            visible = true;
+        private void SetupWorldSelection()
+        {
+            // Find spatial worlds
+            var spatialWorlds = World.AllWorlds.Where(w => w.GetExistingSystem<NetworkStatisticsSystem>() != null)
+                .ToList();
+
+            // Fill menu items
+            var worldMenu = rootVisualElement.Q<ToolbarMenu>("worldSelector").menu;
+            for (var i = 0; i < spatialWorlds.Count; i++)
+            {
+                var spatialWorld = spatialWorlds[i];
+                worldMenu.InsertAction(i, spatialWorld.Name, action =>
+                    {
+                        SelectWorld((World) action.userData);
+                    },
+                    action => selectedWorld == (World) action.userData
+                        ? DropdownMenuAction.Status.Checked
+                        : DropdownMenuAction.Status.Normal,
+                    spatialWorld);
+            }
+
+            // Trim excess items
+            var menuSize = worldMenu.MenuItems().Count;
+            for (var i = spatialWorlds.Count; i < menuSize; i++)
+            {
+                worldMenu.RemoveItemAt(spatialWorlds.Count);
+            }
+
+            if (selectedWorld == null || !selectedWorld.IsCreated)
+            {
+                SelectWorld(spatialWorlds.FirstOrDefault());
+            }
+        }
+
+        private void SelectWorld(World world)
+        {
+            selectedWorld = world;
+            rootVisualElement.Q<ToolbarMenu>("worldSelector").text = selectedWorld?.Name ?? "No SpatialOS worlds active";
+            netStatSystem = selectedWorld?.GetExistingSystem<NetworkStatisticsSystem>();
         }
 
         private void SetupUI()
@@ -69,11 +90,13 @@ namespace Improbable.Gdk.Debug
 
             var itemTemplate = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>(ItemUxmlPath);
 
-            var listView = rootVisualElement.Q<ListView>("DataContainer");
-            listView.selectionType = SelectionType.None;
-            listView.makeItem = () => itemTemplate.CloneTree();
-            listView.bindItem = BindItem;
-            listView.itemsSource = spatialComponents;
+            var scrollView = rootVisualElement.Q<ScrollView>("updatesContainer");
+            for (var i = 0; i < spatialComponents.Count; i++)
+            {
+                var element = itemTemplate.CloneTree();
+                UpdateElement(i, element);
+                scrollView.Add(element);
+            }
         }
 
         private (string Name, uint ComponentId) GetComponentInfoForIndex(int index)
@@ -81,10 +104,18 @@ namespace Improbable.Gdk.Debug
             return spatialComponents[index];
         }
 
-        private void BindItem(VisualElement element, int index)
+        private void UpdateElement(int index, VisualElement element)
         {
+            // Set component name
             var componentInfo = GetComponentInfoForIndex(index);
-            var componentName = componentInfo.Name;
+            element.Q<Label>("name").text = componentInfo.Name;
+
+            if (netStatSystem?.World == null || !netStatSystem.World.IsCreated)
+            {
+                return;
+            }
+
+            // Incoming stats
             var (dataIn, time) =
                 netStatSystem.GetSummary(MessageTypeUnion.Update(componentInfo.ComponentId), 60, Direction.Incoming);
 
@@ -93,10 +124,10 @@ namespace Improbable.Gdk.Debug
                 time = 1;
             }
 
-            element.Q<Label>("name").text = componentName;
             element.Q<Label>("opsIn").text = (dataIn.Count / time).ToString("F1");
             element.Q<Label>("sizeIn").text = (dataIn.Size / 1024f / time).ToString("F3");
 
+            // Outgoing stats
             var (dataOut, _) =
                 netStatSystem.GetSummary(MessageTypeUnion.Update(componentInfo.ComponentId), 60, Direction.Outgoing);
 
@@ -104,15 +135,29 @@ namespace Improbable.Gdk.Debug
             element.Q<Label>("sizeOut").text = (dataOut.Size / 1024f / time).ToString("F3");
         }
 
+        private void RefreshUpdates()
+        {
+            var scrollView = rootVisualElement.Q<ScrollView>("updatesContainer");
+            var count = scrollView.childCount;
+
+            for (int i = 0; i < count; i++)
+            {
+                var element = scrollView[i];
+                if (element.visible)
+                {
+                    UpdateElement(i, element);
+                }
+                else
+                {
+                    UnityEngine.Debug.Log("YEET");
+                }
+            }
+        }
+
         private void OnInspectorUpdate()
         {
-            if (netStatSystem?.World == null || !netStatSystem.World.IsCreated)
-            {
-                return;
-            }
-
-            var listView = rootVisualElement.Q<ListView>("DataContainer");
-            listView.Refresh();
+            SetupWorldSelection();
+            RefreshUpdates();
         }
     }
 }
