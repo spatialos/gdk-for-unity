fb8d2eb132062cabda2e98c76cd7721ca1ef8489
Jamie Brynes <jamiebrynes@improbable.io>
2019-09-05 13:38:51 +0100

    refactor tests slightly

---
+++ test-project/Assets/EditmodeTests/Subscriptions/InjectionCriteriaTests.cs
@@ -0,0 +1,144 @@
+using Improbable.Gdk.Core;
+using Improbable.Gdk.Subscriptions;
+using Improbable.TestSchema;
+using Improbable.Worker.CInterop;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Improbable.Gdk.EditmodeTests.Subscriptions
+{
+    /// <summary>
+    ///     This tests the injection criteria for a reader or writer.
+    /// </summary>
+    [TestFixture]
+    public class ReaderWriterInjectionCriteriaTests : SubscriptionsTestBase
+    {
+        private const long EntityId = 100;
+
+        private GameObject createdGameObject;
+
+        [SetUp]
+        public override void Setup()
+        {
+            base.Setup();
+
+            var template = new EntityTemplate();
+            template.AddComponent(new Position.Snapshot(), "worker");
+            ConnectionHandler.CreateEntity(EntityId, template);
+            ReceiveSystem.Update();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            base.Setup();
+            Object.DestroyImmediate(createdGameObject);
+        }
+
+        [Test]
+        public void Reader_is_not_injected_when_entity_component_is_not_checked_out()
+        {
+            createdGameObject = CreateAndLinkGameObjectWithComponent<ExhaustiveSingularReaderBehaviour>(EntityId);
+            var readerBehaviour = createdGameObject.GetComponent<ExhaustiveSingularReaderBehaviour>();
+
+            Assert.IsFalse(readerBehaviour.enabled);
+            Assert.IsNull(readerBehaviour.Reader);
+        }
+
+        [Test]
+        public void Reader_is_injected_when_entity_component_is_checked_out()
+        {
+            createdGameObject = CreateAndLinkGameObjectWithComponent<PositionReaderBehaviour>(EntityId);
+            var readerBehaviour = createdGameObject.GetComponent<PositionReaderBehaviour>();
+
+            Assert.IsTrue(readerBehaviour.enabled);
+            Assert.IsNotNull(readerBehaviour.Reader);
+        }
+
+        [Test]
+        public void Writer_is_not_injected_when_entity_component_is_not_checked_out()
+        {
+            createdGameObject = CreateAndLinkGameObjectWithComponent<ExhaustiveSingularWriterBehaviour>(EntityId);
+            var writerBehaviour = createdGameObject.GetComponent<ExhaustiveSingularWriterBehaviour>();
+
+            Assert.IsFalse(writerBehaviour.enabled);
+            Assert.IsNull(writerBehaviour.Writer);
+        }
+
+        [Test]
+        public void Writer_is_not_injected_when_entity_component_is_checked_out_and_unauthoritative()
+        {
+            createdGameObject = CreateAndLinkGameObjectWithComponent<PositionWriterBehaviour>(EntityId);
+            var writerBehaviour = createdGameObject.GetComponent<PositionWriterBehaviour>();
+
+            Assert.IsFalse(writerBehaviour.enabled);
+            Assert.IsNull(writerBehaviour.Writer);
+        }
+
+        [Test]
+        public void Writer_is_injected_when_entity_component_is_checked_out_and_authoritative()
+        {
+            ConnectionHandler.ChangeAuthority(EntityId, Position.ComponentId, Authority.Authoritative);
+            ReceiveSystem.Update();
+
+            createdGameObject = CreateAndLinkGameObjectWithComponent<PositionWriterBehaviour>(EntityId);
+            var writerBehaviour = createdGameObject.GetComponent<PositionWriterBehaviour>();
+
+            Assert.IsTrue(writerBehaviour.enabled);
+            Assert.IsNotNull(writerBehaviour.Writer);
+        }
+
+        [Test]
+        public void Injection_does_not_happen_if_not_all_constraints_are_satisfied()
+        {
+            createdGameObject = CreateAndLinkGameObjectWithComponent<CompositeBehaviour>(EntityId);
+            var compositeBehaviour = createdGameObject.GetComponent<CompositeBehaviour>();
+
+            Assert.IsFalse(compositeBehaviour.enabled);
+            Assert.IsNull(compositeBehaviour.Reader);
+            Assert.IsNull(compositeBehaviour.Writer);
+        }
+
+        [Test]
+        public void Injection_happens_if_all_constraints_are_satisfied()
+        {
+            ConnectionHandler.ChangeAuthority(EntityId, Position.ComponentId, Authority.Authoritative);
+            ReceiveSystem.Update();
+
+            createdGameObject = CreateAndLinkGameObjectWithComponent<CompositeBehaviour>(EntityId);
+            var compositeBehaviour = createdGameObject.GetComponent<CompositeBehaviour>();
+
+            Assert.IsTrue(compositeBehaviour.enabled);
+            Assert.IsNotNull(compositeBehaviour.Reader);
+            Assert.IsNotNull(compositeBehaviour.Writer);
+        }
+
+#pragma warning disable 649
+        private class PositionReaderBehaviour : MonoBehaviour
+        {
+            [Require] public PositionReader Reader;
+        }
+
+        private class PositionWriterBehaviour : MonoBehaviour
+        {
+            [Require] public PositionWriter Writer;
+        }
+
+        private class ExhaustiveSingularReaderBehaviour : MonoBehaviour
+        {
+            [Require] public ExhaustiveSingularReader Reader;
+        }
+
+        private class ExhaustiveSingularWriterBehaviour : MonoBehaviour
+        {
+            [Require] public ExhaustiveSingularWriter Writer;
+        }
+
+        private class CompositeBehaviour : MonoBehaviour
+        {
+            [Require] public PositionReader Reader;
+            [Require] public PositionWriter Writer;
+        }
+#pragma warning restore 649
+    }
+}
--- test-project/Assets/EditmodeTests/Subscriptions/WorkerTypeCriteriaTests.cs
+++ test-project/Assets/EditmodeTests/Subscriptions/WorkerTypeCriteriaTests.cs
@@ -7,53 +7,37 @@ using UnityEngine;
 namespace Improbable.Gdk.EditmodeTests.Subscriptions
 {
     [TestFixture]
-    public class WorkerTypeCriteriaTests
+    public class WorkerTypeCriteriaTests : SubscriptionsTestBase
     {
         private const string WorkerType = "TestWorkerType";
         private const long EntityId = 100;
 
-        private WorkerInWorld workerInWorld;
-        private SpatialOSReceiveSystem receiveSystem;
-        private RequireLifecycleSystem requireLifecycleSystem;
-
-        private MockConnectionHandler connectionHandler;
-        private EntityGameObjectLinker linker;
-
         private GameObject createdGameObject;
 
         [SetUp]
-        public void Setup()
+        public override void Setup()
         {
-            var connectionBuilder = new MockConnectionHandlerBuilder();
-            connectionHandler = connectionBuilder.ConnectionHandler;
-
-            workerInWorld = WorkerInWorld
-                .CreateWorkerInWorldAsync(connectionBuilder, WorkerType, new LoggingDispatcher(), Vector3.zero)
-                .Result;
-
-            var world = workerInWorld.World;
-            receiveSystem = world.GetExistingSystem<SpatialOSReceiveSystem>();
-            requireLifecycleSystem = world.GetExistingSystem<RequireLifecycleSystem>();
-            linker = new EntityGameObjectLinker(world);
+            base.Setup();
 
             var template = new EntityTemplate();
             template.AddComponent(new Position.Snapshot(), "worker");
-            connectionHandler.CreateEntity(EntityId, template);
-            receiveSystem.Update();
+            ConnectionHandler.CreateEntity(EntityId, template);
+            ReceiveSystem.Update();
         }
 
         [TearDown]
-        public void TearDown()
+        public override void TearDown()
         {
+            base.TearDown();
+
             Object.DestroyImmediate(createdGameObject);
-            workerInWorld.Dispose();
         }
 
         [Test]
         public void Monobehaviour_is_enabled_with_matching_WorkerType()
         {
-            var go = CreateAndLinkGameObjectWithComponent<MatchingWorkerType>(EntityId);
-            var behaviour = go.GetComponent<MatchingWorkerType>();
+            createdGameObject = CreateAndLinkGameObjectWithComponent<MatchingWorkerType>(EntityId);
+            var behaviour = createdGameObject.GetComponent<MatchingWorkerType>();
 
             Assert.IsTrue(behaviour.enabled);
         }
@@ -61,8 +45,8 @@ namespace Improbable.Gdk.EditmodeTests.Subscriptions
         [Test]
         public void Monobehaviour_is_disabled_with_non_matching_WorkerType()
         {
-            var go = CreateAndLinkGameObjectWithComponent<NonMatchingWorkerType>(EntityId);
-            var behaviour = go.GetComponent<NonMatchingWorkerType>();
+            createdGameObject = CreateAndLinkGameObjectWithComponent<NonMatchingWorkerType>(EntityId);
+            var behaviour = createdGameObject.GetComponent<NonMatchingWorkerType>();
 
             Assert.IsFalse(behaviour.enabled);
         }
@@ -70,8 +54,8 @@ namespace Improbable.Gdk.EditmodeTests.Subscriptions
         [Test]
         public void Monobehaviour_is_disabled_with_matching_WorkerType_but_unsatisfied_requireable()
         {
-            var go = CreateAndLinkGameObjectWithComponent<MatchingWorkerTypeWithRequireable>(EntityId);
-            var behaviour = go.GetComponent<MatchingWorkerTypeWithRequireable>();
+            createdGameObject = CreateAndLinkGameObjectWithComponent<MatchingWorkerTypeWithRequireable>(EntityId);
+            var behaviour = createdGameObject.GetComponent<MatchingWorkerTypeWithRequireable>();
 
             Assert.IsFalse(behaviour.enabled);
             Assert.IsNull(behaviour.Writer);
@@ -80,11 +64,11 @@ namespace Improbable.Gdk.EditmodeTests.Subscriptions
         [Test]
         public void Monobehaviour_is_enabled_with_matching_WorkerType_and_satisfied_requireable()
         {
-            connectionHandler.ChangeAuthority(EntityId, Position.ComponentId, Authority.Authoritative);
-            receiveSystem.Update();
+            ConnectionHandler.ChangeAuthority(EntityId, Position.ComponentId, Authority.Authoritative);
+            ReceiveSystem.Update();
 
-            var go = CreateAndLinkGameObjectWithComponent<MatchingWorkerTypeWithRequireable>(EntityId);
-            var behaviour = go.GetComponent<MatchingWorkerTypeWithRequireable>();
+            createdGameObject = CreateAndLinkGameObjectWithComponent<MatchingWorkerTypeWithRequireable>(EntityId);
+            var behaviour = createdGameObject.GetComponent<MatchingWorkerTypeWithRequireable>();
 
             Assert.IsTrue(behaviour.enabled);
             Assert.IsNotNull(behaviour.Writer);
@@ -93,8 +77,8 @@ namespace Improbable.Gdk.EditmodeTests.Subscriptions
         [Test]
         public void Monobehaviour_is_disabled_with_non_matching_WorkerType_and_unsatisfied_requireable()
         {
-            var go = CreateAndLinkGameObjectWithComponent<NonMatchingWorkerTypeWithRequireable>(EntityId);
-            var behaviour = go.GetComponent<NonMatchingWorkerTypeWithRequireable>();
+            createdGameObject = CreateAndLinkGameObjectWithComponent<NonMatchingWorkerTypeWithRequireable>(EntityId);
+            var behaviour = createdGameObject.GetComponent<NonMatchingWorkerTypeWithRequireable>();
 
             Assert.IsFalse(behaviour.enabled);
             Assert.IsNull(behaviour.Writer);
@@ -103,30 +87,17 @@ namespace Improbable.Gdk.EditmodeTests.Subscriptions
         [Test]
         public void Monobehaviour_is_disabled_with_non_matching_WorkerType_and_satisfied_requireable()
         {
-            connectionHandler.ChangeAuthority(EntityId, Position.ComponentId, Authority.Authoritative);
-            receiveSystem.Update();
+            ConnectionHandler.ChangeAuthority(EntityId, Position.ComponentId, Authority.Authoritative);
+            ReceiveSystem.Update();
 
-            var go = CreateAndLinkGameObjectWithComponent<NonMatchingWorkerTypeWithRequireable>(EntityId);
-            var behaviour = go.GetComponent<NonMatchingWorkerTypeWithRequireable>();
+            createdGameObject = CreateAndLinkGameObjectWithComponent<NonMatchingWorkerTypeWithRequireable>(EntityId);
+            var behaviour = createdGameObject.GetComponent<NonMatchingWorkerTypeWithRequireable>();
 
             Assert.IsFalse(behaviour.enabled);
             Assert.IsNull(behaviour.Writer);
         }
 
-        private GameObject CreateAndLinkGameObjectWithComponent<T>(long entityId) where T : MonoBehaviour
-        {
-            var gameObject = new GameObject("TestGameObject");
-            createdGameObject = gameObject;
-            gameObject.AddComponent<T>();
-            gameObject.GetComponent<T>().enabled = false;
-
-            linker.LinkGameObjectToSpatialOSEntity(new EntityId(entityId), gameObject);
-            requireLifecycleSystem.Update();
-
-            return gameObject;
-        }
-
-        #pragma warning disable 649
+#pragma warning disable 649
 
         [WorkerType(WorkerType)]
         private class MatchingWorkerType : MonoBehaviour
@@ -150,6 +121,6 @@ namespace Improbable.Gdk.EditmodeTests.Subscriptions
             [Require] public PositionWriter Writer;
         }
 
-        #pragma warning restore 649
+#pragma warning restore 649
     }
 }
