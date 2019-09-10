0489217f0198bfac6581c2fc0c362c62c344b5ea
Jamie Brynes <jamiebrynes7@gmail.com>
2019-09-10 14:41:44 +0100

    add partial test

--- test-project/Assets/EditmodeTests/ModularCodegenTests.cs
+++ test-project/Assets/EditmodeTests/ModularCodegenTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Reflection;
 using NUnit.Framework;
 
 namespace Improbable.Gdk.EditmodeTests
@@ -7,15 +8,30 @@ namespace Improbable.Gdk.EditmodeTests
     [TestFixture]
     public class ModularCodegenTests
     {
+        private Assembly generatedCodeAssembly;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            // Assembly.FullName == "<name> <version> <culture> <publickeytoken>", so we just .Contains() it.
+            generatedCodeAssembly = AppDomain.CurrentDomain.GetAssemblies()
+                .First(assembly => assembly.FullName.Contains("Improbable.Gdk.Generated"));
+        }
+
         // This checks that the codegen module was correctly picked up.
         [Test]
         public void Improbable_Gdk_ModularCodegenTests_Test_exists()
         {
-            // Assembly.FullName == "<name> <version> <culture> <publickeytoken>", so we just .Contains() it.
-            var generatedCodeAssembly = AppDomain.CurrentDomain.GetAssemblies()
-                .First(assembly => assembly.FullName.Contains("Improbable.Gdk.Generated"));
-
             Assert.IsTrue(generatedCodeAssembly.GetTypes().Any(type => type.FullName == "Improbable.Gdk.ModularCodegenTests.Test"));
+        }
+
+        [Test]
+        public void Improbable_Tests_ModularTarget_has_partial_injected()
+        {
+            var modularTargetType = generatedCodeAssembly.GetTypes()
+                .First(type => type.FullName == "Improbable.Tests.ModularTarget");
+
+            modularTargetType.GetNestedTypes().Any(type => type.Name == "InteriorClass");
         }
     }
 }
