5e48c8235fbaa84fbec66f271ebd0d56db64d58e
Paul Balaji <paulbalaji@improbable.io>
2019-09-18 16:49:28 +0100

    re-add tests
    Signed-off-by: Paul Balaji <paulbalaji@improbable.io>

--- ci/test.sh
+++ ci/test.sh
@@ -14,36 +14,36 @@ PROJECT_DIR="$(pwd)"
 TEST_RESULTS_DIR="${PROJECT_DIR}/logs/nunit"
 mkdir -p "${TEST_RESULTS_DIR}"
 
-# echo "--- Testing Code Generator :gear:"
+echo "--- Testing Code Generator :gear:"
 
-# dotnet test \
-#     --logger:"nunit;LogFilePath=${TEST_RESULTS_DIR}/code-gen-lib-test-results.xml" \
-#     workers/unity/Packages/io.improbable.gdk.tools/.CodeGenTemplate/CodeGenerationLib/CodeGenerationLib.csproj
+dotnet test \
+    --logger:"nunit;LogFilePath=${TEST_RESULTS_DIR}/code-gen-lib-test-results.xml" \
+    workers/unity/Packages/io.improbable.gdk.tools/.CodeGenTemplate/CodeGenerationLib/CodeGenerationLib.csproj
 
-# echo "--- Testing Unity: Editmode :writing_hand:"
+echo "--- Testing Unity: Editmode :writing_hand:"
 
-# pushd "workers/unity"
-#     dotnet run -p "${PROJECT_DIR}/.shared-ci/tools/RunUnity/RunUnity.csproj" -- \
-#         -batchmode \
-#         -projectPath "${PROJECT_DIR}/workers/unity" \
-#         -runEditorTests \
-#         -logfile "${PROJECT_DIR}/logs/unity-editmode-test-run.log" \
-#         -editorTestsResultFile "${TEST_RESULTS_DIR}/editmode-test-results.xml"
-# popd
+pushd "workers/unity"
+    dotnet run -p "${PROJECT_DIR}/.shared-ci/tools/RunUnity/RunUnity.csproj" -- \
+        -batchmode \
+        -projectPath "${PROJECT_DIR}/workers/unity" \
+        -runEditorTests \
+        -logfile "${PROJECT_DIR}/logs/unity-editmode-test-run.log" \
+        -editorTestsResultFile "${TEST_RESULTS_DIR}/editmode-test-results.xml"
+popd
 
-# cleanUnity "$(pwd)/workers/unity"
+cleanUnity "$(pwd)/workers/unity"
 
-# echo "--- Testing Unity: Playmode :joystick:"
+echo "--- Testing Unity: Playmode :joystick:"
 
-# pushd "workers/unity"
-#     dotnet run -p "${PROJECT_DIR}/.shared-ci/tools/RunUnity/RunUnity.csproj" -- \
-#         -batchmode \
-#         -projectPath "${PROJECT_DIR}/workers/unity" \
-#         -runTests \
-#         -testPlatform playmode \
-#         -logfile "${PROJECT_DIR}/logs/unity-playmode-test-run.log" \
-#         -testResults "${TEST_RESULTS_DIR}/playmode-test-results.xml"
-# popd
+pushd "workers/unity"
+    dotnet run -p "${PROJECT_DIR}/.shared-ci/tools/RunUnity/RunUnity.csproj" -- \
+        -batchmode \
+        -projectPath "${PROJECT_DIR}/workers/unity" \
+        -runTests \
+        -testPlatform playmode \
+        -logfile "${PROJECT_DIR}/logs/unity-playmode-test-run.log" \
+        -testResults "${TEST_RESULTS_DIR}/playmode-test-results.xml"
+popd
 
 echo "--- Testing Unity: Test Project Editmode :microscope:"
 
