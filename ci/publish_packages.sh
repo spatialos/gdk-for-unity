26f81426ba414067e951337ed9acf631ee7c73bc
Jamie Brynes <jamiebrynes@improbable.io>
2019-09-18 14:09:23 +0100

    update publish packages script (#1158)

--- ci/publish_packages.sh
+++ ci/publish_packages.sh
@@ -2,18 +2,33 @@
 
 set -e -u -o pipefail
 
+if [[ -n "${DEBUG-}" ]]; then
+  set -x
+fi
+
 cd "$(dirname "$0")/../"
 
 PKG_ROOT="workers/unity/Packages"
-REGISTRY="https://npm.improbable.io/gdk-for-unity/"
+
+if [[ -n "${DRY_RUN-}" ]]; then
+    EXTRA_CLOUDSMITH_ARGS="--dry-run"
+else
+    EXTRA_CLOUDSMITH_ARGS=""
+fi
+
+./init.sh
 
 # Re-publish all packages
-pushd $PKG_ROOT
-    for dir in */;
+pushd "${PKG_ROOT}" > /dev/null
+    for package_dir in */;
     do
-        echo $dir
-        pushd $dir
-            npm publish --registry="${REGISTRY}"
-        popd
+        echo "${package_dir}"
+        pushd "${package_dir}" > /dev/null
+            # Created package is the last line of output.
+            PACKAGE_NAME=$(npm pack | tail -n 1)
+            cloudsmith push npm spatialos/gdk-for-unity "${PACKAGE_NAME}" --republish "${EXTRA_CLOUDSMITH_ARGS}"
+            rm "${PACKGE_NAME}"
+        popd > /dev/null
     done
-popd
+popd > /dev/null
+
