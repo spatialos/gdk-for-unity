55b0f1e25a85487d4dc624ce7619074205bc694d
Martijn Gerkes <martijn@improbable.io>
2019-10-31 13:51:22 +0000

    Added test for inherited requires

--- test-project/Assets/EditmodeTests/Subscriptions/InjectionCriteriaTests.cs
+++ test-project/Assets/EditmodeTests/Subscriptions/InjectionCriteriaTests.cs
@@ -114,7 +114,30 @@ namespace Improbable.Gdk.EditmodeTests.Subscriptions
             Assert.IsNotNull(compositeBehaviour.Writer);
         }
 
+        [Test]
+        public void Injection_happens_if_inherited_constraints_are_satisfied()
+        {
+            ConnectionHandler.ChangeAuthority(EntityId, Position.ComponentId, Authority.Authoritative);
+            ReceiveSystem.Update();
+
+            createdGameObject = CreateAndLinkGameObjectWithComponent<InheritanceBehaviour>(EntityId);
+            var inheritanceBehaviour = createdGameObject.GetComponent<InheritanceBehaviour>();
+
+            Assert.IsTrue(inheritanceBehaviour.enabled);
+            Assert.IsNotNull(inheritanceBehaviour.OwnReader);
+        }
+
 #pragma warning disable 649
+        private class InheritanceBehaviour : BaseBehaviour
+        {
+            public PositionReader OwnReader => Reader;
+        }
+
+        private class BaseBehaviour : MonoBehaviour
+        {
+            [Require] protected PositionReader Reader;
+        }
+
         private class PositionReaderBehaviour : MonoBehaviour
         {
             [Require] public PositionReader Reader;
