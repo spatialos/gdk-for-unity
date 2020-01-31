23e034ae331476393038cb6891d98693cd1b70ec
Martijn Gerkes <martijn@improbable.io>
2020-01-31 18:00:30 +0000

    Generated code

--- test-project/Assets/Generated/Source/improbable/dependentschema/DependentDataComponentCommandSenderReceiver.cs
+++ test-project/Assets/Generated/Source/improbable/dependentschema/DependentDataComponentCommandSenderReceiver.cs
@@ -7,243 +7,44 @@ using System.Collections.Generic;
 using Unity.Entities;
 using Improbable.Gdk.Core;
 using Improbable.Gdk.Subscriptions;
-using Improbable.Worker.CInterop;
 using Entity = Unity.Entities.Entity;
 
 namespace Improbable.DependentSchema
 {
     [AutoRegisterSubscriptionManager]
-    public class DependentDataComponentCommandSenderSubscriptionManager : SubscriptionManager<DependentDataComponentCommandSender>
+    public class DependentDataComponentCommandSenderSubscriptionManager : CommandSenderSubscriptionManagerBase<DependentDataComponentCommandSender>
     {
-        private Dictionary<EntityId, HashSet<Subscription<DependentDataComponentCommandSender>>>
-            entityIdToSenderSubscriptions =
-                new Dictionary<EntityId, HashSet<Subscription<DependentDataComponentCommandSender>>>();
-
         public DependentDataComponentCommandSenderSubscriptionManager(World world) : base(world)
         {
-            var constraintSystem = world.GetExistingSystem<ComponentConstraintsCallbackSystem>();
-
-            constraintSystem.RegisterEntityAddedCallback(entityId =>
-            {
-                if (!entityIdToSenderSubscriptions.TryGetValue(entityId, out var subscriptions))
-                {
-                    return;
-                }
-
-                workerSystem.TryGetEntity(entityId, out var entity);
-                foreach (var subscription in subscriptions)
-                {
-                    if (!subscription.HasValue)
-                    {
-                        subscription.SetAvailable(new DependentDataComponentCommandSender(entity, world));
-                    }
-                }
-            });
-
-            constraintSystem.RegisterEntityRemovedCallback(entityId =>
-            {
-                if (!entityIdToSenderSubscriptions.TryGetValue(entityId, out var subscriptions))
-                {
-                    return;
-                }
-
-                foreach (var subscription in subscriptions)
-                {
-                    if (subscription.HasValue)
-                    {
-                        ResetValue(subscription);
-                        subscription.SetUnavailable();
-                    }
-                }
-            });
         }
 
-        public override Subscription<DependentDataComponentCommandSender> Subscribe(EntityId entityId)
+        protected override DependentDataComponentCommandSender CreateSender(Entity entity, World world)
         {
-            if (entityIdToSenderSubscriptions == null)
-            {
-                entityIdToSenderSubscriptions = new Dictionary<EntityId, HashSet<Subscription<DependentDataComponentCommandSender>>>();
-            }
-
-            if (entityId.Id < 0)
-            {
-                throw new ArgumentException("EntityId can not be < 0");
-            }
-
-            var subscription = new Subscription<DependentDataComponentCommandSender>(this, entityId);
-
-            if (!entityIdToSenderSubscriptions.TryGetValue(entityId, out var subscriptions))
-            {
-                subscriptions = new HashSet<Subscription<DependentDataComponentCommandSender>>();
-                entityIdToSenderSubscriptions.Add(entityId, subscriptions);
-            }
-
-            if (workerSystem.TryGetEntity(entityId, out var entity))
-            {
-                subscription.SetAvailable(new DependentDataComponentCommandSender(entity, world));
-            }
-            else if (entityId.Id == 0)
-            {
-                subscription.SetAvailable(new DependentDataComponentCommandSender(Entity.Null, world));
-            }
-
-            subscriptions.Add(subscription);
-            return subscription;
-        }
-
-        public override void Cancel(ISubscription subscription)
-        {
-            var sub = ((Subscription<DependentDataComponentCommandSender>) subscription);
-            if (sub.HasValue)
-            {
-                var sender = sub.Value;
-                sender.IsValid = false;
-            }
-
-            var subscriptions = entityIdToSenderSubscriptions[sub.EntityId];
-            subscriptions.Remove(sub);
-            if (subscriptions.Count == 0)
-            {
-                entityIdToSenderSubscriptions.Remove(sub.EntityId);
-            }
-        }
-
-        public override void ResetValue(ISubscription subscription)
-        {
-            var sub = ((Subscription<DependentDataComponentCommandSender>) subscription);
-            if (sub.HasValue)
-            {
-                sub.Value.RemoveAllCallbacks();
-            }
+            return new DependentDataComponentCommandSender(entity, world);
         }
     }
 
     [AutoRegisterSubscriptionManager]
-    public class DependentDataComponentCommandReceiverSubscriptionManager : SubscriptionManager<DependentDataComponentCommandReceiver>
+    public class DependentDataComponentCommandReceiverSubscriptionManager : CommandReceiverSubscriptionManagerBase<DependentDataComponentCommandReceiver>
     {
-        private readonly ComponentUpdateSystem componentUpdateSystem;
-
-        private Dictionary<EntityId, HashSet<Subscription<DependentDataComponentCommandReceiver>>> entityIdToReceiveSubscriptions;
-
-        private HashSet<EntityId> entitiesMatchingRequirements = new HashSet<EntityId>();
-        private HashSet<EntityId> entitiesNotMatchingRequirements = new HashSet<EntityId>();
-
-        public DependentDataComponentCommandReceiverSubscriptionManager(World world) : base(world)
+        public DependentDataComponentCommandReceiverSubscriptionManager(World world) : base(world, DependentDataComponent.ComponentId)
         {
-            componentUpdateSystem = world.GetExistingSystem<ComponentUpdateSystem>();
-
-            var constraintSystem = world.GetExistingSystem<ComponentConstraintsCallbackSystem>();
-
-            constraintSystem.RegisterAuthorityCallback(DependentDataComponent.ComponentId, authorityChange =>
-            {
-                if (authorityChange.Authority == Authority.Authoritative)
-                {
-                    if (!entitiesNotMatchingRequirements.Contains(authorityChange.EntityId))
-                    {
-                        return;
-                    }
-
-                    workerSystem.TryGetEntity(authorityChange.EntityId, out var entity);
-
-                    foreach (var subscription in entityIdToReceiveSubscriptions[authorityChange.EntityId])
-                    {
-                        subscription.SetAvailable(new DependentDataComponentCommandReceiver(world, entity, authorityChange.EntityId));
-                    }
-
-                    entitiesMatchingRequirements.Add(authorityChange.EntityId);
-                    entitiesNotMatchingRequirements.Remove(authorityChange.EntityId);
-                }
-                else if (authorityChange.Authority == Authority.NotAuthoritative)
-                {
-                    if (!entitiesMatchingRequirements.Contains(authorityChange.EntityId))
-                    {
-                        return;
-                    }
-
-                    workerSystem.TryGetEntity(authorityChange.EntityId, out var entity);
-
-                    foreach (var subscription in entityIdToReceiveSubscriptions[authorityChange.EntityId])
-                    {
-                        ResetValue(subscription);
-                        subscription.SetUnavailable();
-                    }
-
-                    entitiesNotMatchingRequirements.Add(authorityChange.EntityId);
-                    entitiesMatchingRequirements.Remove(authorityChange.EntityId);
-                }
-            });
         }
 
-        public override Subscription<DependentDataComponentCommandReceiver> Subscribe(EntityId entityId)
+        protected override DependentDataComponentCommandReceiver CreateReceiver(World world, Entity entity, EntityId entityId)
         {
-            if (entityIdToReceiveSubscriptions == null)
-            {
-                entityIdToReceiveSubscriptions = new Dictionary<EntityId, HashSet<Subscription<DependentDataComponentCommandReceiver>>>();
-            }
-
-            var subscription = new Subscription<DependentDataComponentCommandReceiver>(this, entityId);
-
-            if (!entityIdToReceiveSubscriptions.TryGetValue(entityId, out var subscriptions))
-            {
-                subscriptions = new HashSet<Subscription<DependentDataComponentCommandReceiver>>();
-                entityIdToReceiveSubscriptions.Add(entityId, subscriptions);
-            }
-
-            if (workerSystem.TryGetEntity(entityId, out var entity)
-                && componentUpdateSystem.HasComponent(DependentDataComponent.ComponentId, entityId)
-                && componentUpdateSystem.GetAuthority(entityId, DependentDataComponent.ComponentId) != Authority.NotAuthoritative)
-            {
-                entitiesMatchingRequirements.Add(entityId);
-                subscription.SetAvailable(new DependentDataComponentCommandReceiver(world, entity, entityId));
-            }
-            else
-            {
-                entitiesNotMatchingRequirements.Add(entityId);
-            }
-
-            subscriptions.Add(subscription);
-            return subscription;
-        }
-
-        public override void Cancel(ISubscription subscription)
-        {
-            var sub = ((Subscription<DependentDataComponentCommandReceiver>) subscription);
-            if (sub.HasValue)
-            {
-                var receiver = sub.Value;
-                receiver.IsValid = false;
-                receiver.RemoveAllCallbacks();
-            }
-
-            var subscriptions = entityIdToReceiveSubscriptions[sub.EntityId];
-            subscriptions.Remove(sub);
-            if (subscriptions.Count == 0)
-            {
-                entityIdToReceiveSubscriptions.Remove(sub.EntityId);
-                entitiesMatchingRequirements.Remove(sub.EntityId);
-                entitiesNotMatchingRequirements.Remove(sub.EntityId);
-            }
-        }
-
-        public override void ResetValue(ISubscription subscription)
-        {
-            var sub = ((Subscription<DependentDataComponentCommandReceiver>) subscription);
-            if (sub.HasValue)
-            {
-                sub.Value.RemoveAllCallbacks();
-            }
+            return new DependentDataComponentCommandReceiver(world, entity, entityId);
         }
     }
 
-    public class DependentDataComponentCommandSender
+    public class DependentDataComponentCommandSender : ICommandSender
     {
-        public bool IsValid;
-
         private readonly Entity entity;
         private readonly CommandSystem commandSender;
         private readonly CommandCallbackSystem callbackSystem;
-
         private int callbackEpoch;
+
+        public bool IsValid { get; set; }
 
         internal DependentDataComponentCommandSender(Entity entity, World world)
         {
@@ -286,13 +87,13 @@ namespace Improbable.DependentSchema
         }
     }
 
-    public class DependentDataComponentCommandReceiver
+    public class DependentDataComponentCommandReceiver : ICommandReceiver
     {
-        public bool IsValid;
-
         private readonly EntityId entityId;
         private readonly CommandCallbackSystem callbackSystem;
         private readonly CommandSystem commandSystem;
+
+        public bool IsValid { get; set; }
 
         private Dictionary<Action<global::Improbable.DependentSchema.DependentDataComponent.BarCommand.ReceivedRequest>, ulong> barCommandCallbackToCallbackKey;
 
