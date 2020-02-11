c057b7b091bab912fcafa98e3d5fb690245165a4
Paul Balaji <paulbalaji@improbable.io>
2020-02-11 12:10:37 +0000

    comments

--- workers/unity/Packages/io.improbable.gdk.tools/.CodeGenTemplate/CodeGenerationLib/Model/Details/Details.cs
+++ workers/unity/Packages/io.improbable.gdk.tools/.CodeGenTemplate/CodeGenerationLib/Model/Details/Details.cs
@@ -4,8 +4,19 @@ namespace Improbable.Gdk.CodeGeneration.Model.Details
 {
     public abstract class Details
     {
+        /// <summary>
+        /// The raw name as defined in the schema bundle.
+        /// </summary>
         public readonly string Name;
+
+        /// <summary>
+        /// The raw name converted to PascalCase.
+        /// </summary>
         public readonly string PascalCaseName;
+
+        /// <summary>
+        /// The PascalCase name converted to camelCase.
+        /// </summary>
         public readonly string CamelCaseName;
 
         protected Details(Definition definition)
--- workers/unity/Packages/io.improbable.gdk.tools/.CodeGenTemplate/CodeGenerationLib/Model/Details/TypeDetails/GeneratorInputDetails.cs
+++ workers/unity/Packages/io.improbable.gdk.tools/.CodeGenTemplate/CodeGenerationLib/Model/Details/TypeDetails/GeneratorInputDetails.cs
@@ -4,10 +4,31 @@ namespace Improbable.Gdk.CodeGeneration.Model.Details
 {
     public abstract class GeneratorInputDetails : Details
     {
+        /// <summary>
+        /// The namespace for the code generated from the given details.
+        /// </summary>
         public readonly string Namespace;
+
+        /// <summary>
+        /// The target path for code generated from the given details.
+        /// </summary>
+        /// <remarks>
+        /// The path is relative to the target directory defined of the codegen job that calls generators
+        /// with these details.
+        /// </remarks>
         public readonly string NamespacePath;
 
+        /// <summary>
+        /// The qualified name as defined by the the schema bundle.
+        /// </summary>
         public readonly string QualifiedName;
+
+        /// <summary>
+        /// A properly formatted variant of the QualifiedName.
+        /// </summary>
+        /// <remarks>
+        /// Includes the "global::" prefix.
+        /// </remarks>
         public readonly string FullyQualifiedName;
 
         protected GeneratorInputDetails(string package, QualifiedDefinition qualifiedDefinition) : base(qualifiedDefinition)
