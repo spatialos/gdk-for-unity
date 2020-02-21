ab27953595466a839b99479480e667e1f6366543
Jamie Brynes <jamiebrynes@improbable.io>
2020-02-21 13:30:48 +0000

    update launch script

--- ci/launch.sh
+++ ci/launch.sh
@@ -18,6 +18,12 @@ uploadAssembly "${ASSEMBLY_PREFIX}" "${PROJECT_NAME}"
 
 echo "--- Launching deployment :airplane_departure:"
 
+# If the RUNTIME_VERSION env variable is already set, i.e. - through the Buildkite menu
+# then skip reading the file.
+if [[ -n $"{RUNTIME_VERSION:-}" ]]; then
+  export RUNTIME_VERSION="$(cat workers/unity/Packages/io.improbable.gdk.tools/runtime.pinned)"
+fi
+
 dotnet run -p workers/unity/Packages/io.improbable.gdk.deploymentlauncher/.DeploymentLauncher/DeploymentLauncher.csproj -- \
     create \
     --project_name "${PROJECT_NAME}" \
@@ -26,7 +32,8 @@ dotnet run -p workers/unity/Packages/io.improbable.gdk.deploymentlauncher/.Deplo
     --launch_json_path cloud_launch.json \
     --snapshot_path snapshots/default.snapshot \
     --region EU \
-    --tags "dev_login"
+    --tags "dev_login" \
+    --runtime_version "${RUNTIME_VERSION}"
 
 CONSOLE_URL="https://console.improbable.io/projects/${PROJECT_NAME}/deployments/${ASSEMBLY_NAME}/overview"
 
