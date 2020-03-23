d65faa1c9bcb27d6af3bae62bce0c84350129ee4
Paul Balaji <paulbalaji@improbable.io>
2020-03-23 13:01:39 +0000

    CR

--- workers/unity/Packages/io.improbable.gdk.gameobjectcreation/AssemblyInfo.cs
+++ workers/unity/Packages/io.improbable.gdk.gameobjectcreation/AssemblyInfo.cs
@@ -1,10 +1,6 @@
 using System.Runtime.CompilerServices;
-using Improbable.Gdk.GameObjectCreation;
-using Unity.Entities;
 
 [assembly: InternalsVisibleTo("Improbable.Gdk.Core.EditmodeTests")]
 [assembly: InternalsVisibleTo("Improbable.Gdk.GameObjectCreation.EditmodeTests")]
 [assembly: InternalsVisibleTo("Improbable.Gdk.EditmodeTests")]
 [assembly: InternalsVisibleTo("Improbable.Gdk.PlaymodeTests")]
-
-[assembly: RegisterGenericComponentType(typeof(GameObjectInitSystemStateComponent))]
