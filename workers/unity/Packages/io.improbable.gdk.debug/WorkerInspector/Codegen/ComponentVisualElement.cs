843c8502159b62fc7238d0491be795f68e439dcf
Jamie Brynes <jamiebrynes7@gmail.com>
2020-06-05 14:26:26 +0100

    add ability to inject icons

--- workers/unity/Packages/io.improbable.gdk.debug/WorkerInspector/Codegen/ComponentVisualElement.cs
+++ workers/unity/Packages/io.improbable.gdk.debug/WorkerInspector/Codegen/ComponentVisualElement.cs
@@ -1,5 +1,7 @@
+using System.Linq;
 using Unity.Entities;
 using UnityEditor;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace Improbable.Gdk.Debug.WorkerInspector.Codegen
@@ -19,6 +21,25 @@ namespace Improbable.Gdk.Debug.WorkerInspector.Codegen
 
             ComponentFoldout = this.Q<Foldout>(className: "component-foldout");
             AuthoritativeToggle = this.Q<Toggle>(className: "is-auth-toggle");
+        }
+
+        protected void InjectComponentIcon(string iconName)
+        {
+            var iconContent = EditorGUIUtility.IconContent(iconName);
+            var iconVisualElement = new VisualElement();
+            iconVisualElement.style.backgroundImage = new StyleBackground((Texture2D) iconContent.image);
+            iconVisualElement.AddToClassList("component-icon");
+
+            // We want to inject the icon after the toggle icon and before the text.
+            var foldoutToggle = ComponentFoldout.Q<VisualElement>(className: "unity-toggle__input");
+            var children = foldoutToggle.Children().ToList();
+            children.Insert(1, iconVisualElement);
+
+            foldoutToggle.Clear();
+            foreach (var child in children)
+            {
+                foldoutToggle.Add(child);
+            }
         }
 
         public abstract ComponentType ComponentType { get; }
