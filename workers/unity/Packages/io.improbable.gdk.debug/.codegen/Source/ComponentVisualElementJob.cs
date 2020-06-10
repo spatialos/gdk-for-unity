abe35616e0910ab05953abefad018e7278f07656
Jamie Brynes <jamiebrynes7@gmail.com>
2020-06-10 12:02:05 +0100

    generate types

--- workers/unity/Packages/io.improbable.gdk.debug/.codegen/Source/ComponentVisualElementJob.cs
+++ workers/unity/Packages/io.improbable.gdk.debug/.codegen/Source/ComponentVisualElementJob.cs
@@ -20,8 +20,25 @@ namespace Improbable.Gdk.CodeGenerator
             AddJobTarget(Path.Combine(relativeOutputPath, DebugAsmdefFileName), () => DebugAssemblyGenerator.Generate());
 
             var componentsToGenerate = detailsStore.Components.Values.ToList();
-            AddGenerators(relativeOutputPath, componentsToGenerate, component => ($"{component.Name}Renderer.cs", ComponentVisualElementGenerator.Generate));
+            var componentGenerator = new ComponentVisualElementGenerator(detailsStore);
+            AddGenerators(relativeOutputPath, componentsToGenerate, component => ($"{component.Name}Renderer.cs", componentGenerator.Generate));
             Logger.Trace($"Added job targets for {componentsToGenerate.Count} components");
+
+            // Types
+            Logger.Trace("Gathering nested types.");
+            var allNestedTypes = detailsStore.Types
+                .SelectMany(kv => detailsStore.GetNestedTypes(kv.Key))
+                .ToHashSet();
+
+            Logger.Trace("Gathering types details.");
+            var typesToGenerate = detailsStore.Types
+                .Where(kv => !allNestedTypes.Contains(kv.Key))
+                .Select(kv => kv.Value)
+                .ToList();
+
+            var typeGenerator = new TypeVisualElementGenerator(detailsStore);
+            AddGenerators(relativeOutputPath, typesToGenerate, type => ($"{type.Name}Renderer.cs", typeGenerator.Generate));
+            Logger.Trace($"Added job targets for {typesToGenerate.Count} types");
         }
     }
 }
---
+++ workers/unity/Packages/io.improbable.gdk.debug/.codegen/Source/Generators/DebugExtensions/TypeVisualElementGenerator.cs
@@ -0,0 +1,70 @@
+using System.Linq;
+using Improbable.Gdk.CodeGeneration.CodeWriter;
+using Improbable.Gdk.CodeGeneration.CodeWriter.Scopes;
+using Improbable.Gdk.CodeGeneration.Model.Details;
+
+namespace Improbable.Gdk.CodeGenerator
+{
+    public class TypeVisualElementGenerator
+    {
+        private readonly FieldTypeHandler fieldTypeHandler;
+
+        public TypeVisualElementGenerator(DetailsStore detailsStore)
+        {
+            fieldTypeHandler = new FieldTypeHandler(detailsStore);
+        }
+
+        public CodeWriter Generate(UnityTypeDetails details)
+        {
+            return CodeWriter.Populate(cgw =>
+            {
+                cgw.UsingDirectives(
+                    "UnityEngine.UIElements",
+                    "UnityEditor.UIElements",
+                    "Improbable.Gdk.Debug.WorkerInspector.Codegen"
+                );
+
+                cgw.Namespace(details.Namespace, ns =>
+                {
+                    ns.Type(GenerateType(details));
+                });
+            });
+        }
+
+        private TypeBlock GenerateType(UnityTypeDetails details)
+        {
+            return Scope.Type($"public class {details.Name}Renderer : SchemaTypeVisualElement<{details.FullyQualifiedName}>",
+                type =>
+                {
+                    type.TextList(details.FieldDetails.Select(fieldTypeHandler.ToFieldDeclaration));
+
+                    GenerateConstructor(type, details);
+                    GenerateUpdateMethod(type, details);
+
+                    foreach (var nestedType in details.ChildTypes)
+                    {
+                        type.Type(GenerateType(nestedType));
+                    }
+                });
+        }
+
+        private void GenerateConstructor(TypeBlock typeBlock, UnityTypeDetails details)
+        {
+            typeBlock.Method($"public {details.Name}Renderer(string label) : base(label)", mb =>
+            {
+                foreach (var field in details.FieldDetails)
+                {
+                    mb.TextList(fieldTypeHandler.ToFieldInitialisation(field, "Container"));
+                }
+            });
+        }
+
+        private void GenerateUpdateMethod(TypeBlock typeBlock, UnityTypeDetails details)
+        {
+            typeBlock.Method($"public override void Update({details.FullyQualifiedName} data)", mb =>
+            {
+                mb.TextList(details.FieldDetails.Select(fd => fieldTypeHandler.ToUiFieldUpdate(fd, "data")));
+            });
+        }
+    }
+}
