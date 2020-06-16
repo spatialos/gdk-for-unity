26f19b2ab95535ffd88b38768a12f8b4e355ee82
Paul Balaji <paulbalaji@improbable.io>
2020-06-16 16:21:43 +0100

    updated way of running playmode tests

--- ci/perf-test.sh
+++ ci/perf-test.sh
@@ -17,10 +17,35 @@ TEST_RESULTS_DIR="${PROJECT_DIR}/logs/nunit"
 mkdir -p "${TEST_RESULTS_DIR}"
 
 TEST_SETTINGS_DIR="${PROJECT_DIR}/workers/unity/Packages/io.improbable.gdk.testutils/TestSettings"
-MONO_BACKEND="${TEST_SETTINGS_DIR}/Mono.json"
-IL2CPP_BACKEND="${TEST_SETTINGS_DIR}/IL2CPP.json"
 
-traceStart "Testing Unity: Editmode :writing_hand:"
+function runPlaymodeTests {
+    local burst=$1
+    local scriptingBackend=$2
+    local apiProfile=$3
+    local category=$4
+    local platform=$5
+
+    local args=()
+    args+=("-batchmode -runTests")
+    args+=("-projectPath ${PROJECT_DIR}/workers/unity ")
+    args+=("-logfile ${PROJECT_DIR}/logs/playmode-${burst}-${scriptingBackend}-${apiProfile}-perftest-run.log")
+    args+=("-testResults ${TEST_RESULTS_DIR}/playmode-${burst}-${scriptingBackend}-${apiProfile}-perftest-results.xml")
+    args+=("-testSettingsFile ${TEST_SETTINGS_DIR}/${scriptingBackend}-${apiProfile}.json")
+
+    if [[ "${burst}" == "burst-disabled" ]]; then
+        args+=('--burst-disable-compilation')
+    fi
+
+    traceStart "${burst} ${scriptingBackend} ${apiProfile}"
+        dotnet run -p "${PROJECT_DIR}/.shared-ci/tools/RunUnity/RunUnity.csproj" -- \
+            "${ACCELERATOR_ARGS}" \
+            -testCategory "${category}" \
+            -testPlatform "${platform}" \
+            "${args[@]}"
+    traceEnd
+}
+
+traceStart "Performance Testing: Editmode :writing_hand:"
     pushd "workers/unity"
         traceStart "Burst default"
             dotnet run -p "${PROJECT_DIR}/.shared-ci/tools/RunUnity/RunUnity.csproj" -- \
@@ -41,66 +66,24 @@ traceStart "Testing Unity: Editmode :writing_hand:"
                 -testCategory "Performance" \
                 -logfile "${PROJECT_DIR}/logs/editmode-burst-disabled-perftest-run.log" \
                 -editorTestsResultFile "${TEST_RESULTS_DIR}/editmode-burst-disabled-perftest-results.xml" \
-                "${ACCELERATOR_ARGS}"
+                "${ACCELERATOR_ARGS}" \
+                --burst-disable-compilation
         traceEnd
     popd
 traceEnd
 
-traceStart "Testing Unity: Playmode :joystick:"
+traceStart "Performance Testing: Playmode :joystick:"
     pushd "workers/unity"
-        traceStart "Burst default - Mono backend"
-            dotnet run -p "${PROJECT_DIR}/.shared-ci/tools/RunUnity/RunUnity.csproj" -- \
-                -batchmode \
-                -projectPath "${PROJECT_DIR}/workers/unity" \
-                -runTests \
-                -testPlatform playmode \
-                -testCategory "Performance" \
-                -logfile "${PROJECT_DIR}/logs/playmode-burst-default-mono-perftest-run.log" \
-                -testResults "${TEST_RESULTS_DIR}/playmode-burst-default-mono-perftest-results.xml" \
-                "${ACCELERATOR_ARGS}" \
-                -testSettingsFile $MONO_BACKEND
-        traceEnd
-
-        traceStart "Burst default - IL2CPP backend"
-            dotnet run -p "${PROJECT_DIR}/.shared-ci/tools/RunUnity/RunUnity.csproj" -- \
-                -batchmode \
-                -projectPath "${PROJECT_DIR}/workers/unity" \
-                -runTests \
-                -testPlatform playmode \
-                -testCategory "Performance" \
-                -logfile "${PROJECT_DIR}/logs/playmode-burst-default-il2cpp-perftest-run.log" \
-                -testResults "${TEST_RESULTS_DIR}/playmode-burst-default-il2cpp-perftest-results.xml" \
-                "${ACCELERATOR_ARGS}" \
-                -testSettingsFile $IL2CPP_BACKEND
-        traceEnd
-
-        traceStart "Burst disabled - Mono backend"
-            dotnet run -p "${PROJECT_DIR}/.shared-ci/tools/RunUnity/RunUnity.csproj" -- \
-                -batchmode \
-                -projectPath "${PROJECT_DIR}/workers/unity" \
-                -runTests \
-                -testPlatform playmode \
-                -testCategory "Performance" \
-                -logfile "${PROJECT_DIR}/logs/playmode-burst-disabled-mono-perftest-run.log" \
-                -testResults "${TEST_RESULTS_DIR}/playmode-burst-disabled-mono-perftest-results.xml" \
-                "${ACCELERATOR_ARGS}" \
-                -testSettingsFile $MONO_BACKEND \
-                --burst-disable-compilation
-        traceEnd
-
-        traceStart "Burst disabled - IL2CPP backend"
-            dotnet run -p "${PROJECT_DIR}/.shared-ci/tools/RunUnity/RunUnity.csproj" -- \
-                -batchmode \
-                -projectPath "${PROJECT_DIR}/workers/unity" \
-                -runTests \
-                -testPlatform playmode \
-                -testCategory "Performance" \
-                -logfile "${PROJECT_DIR}/logs/playmode-burst-disabled-il2cpp-perftest-run.log" \
-                -testResults "${TEST_RESULTS_DIR}/playmode-burst-disabled-il2cpp-perftest-results.xml" \
-                "${ACCELERATOR_ARGS}" \
-                -testSettingsFile $IL2CPP_BACKEND \
-                --burst-disable-compilation
-        traceEnd
+        for burst in burst-default burst-disabled
+        do
+            for scriptingBackend in mono il2cpp winrt
+            do
+                for apiProfile in dotnet-std-2 dotnet-4
+                do
+                    runPlaymodeTests $burst $scriptingBackend $apiProfile "Performance" "StandaloneWindows64"
+                done
+            done
+        done
     popd
 traceEnd
 
