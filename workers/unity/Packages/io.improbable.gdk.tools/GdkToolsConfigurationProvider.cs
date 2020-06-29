5add98448f3651973b652bdb09f9a73f330593bb
Sean Parker <seanparker@improbable.io>
2020-06-29 10:28:03 +0100

    Use .GetFields() reflection method to get all field name of class

--- workers/unity/Packages/io.improbable.gdk.tools/GdkToolsConfigurationProvider.cs
+++ workers/unity/Packages/io.improbable.gdk.tools/GdkToolsConfigurationProvider.cs
@@ -49,9 +49,10 @@ namespace Improbable.Gdk.Tools
         {
             var provider = new GdkToolsConfigurationProvider(ProjectSettingsPath, SettingsScope.Project);
 
-            // Extract all public and private properties from the GdkToolsConfiguration class
-            // Use the names of the discovered properties as the keyword search terms in Project Settings panel
-            var gdkToolsConfigProperties = typeof(GdkToolsConfiguration).GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            // Extract all public fields from the GdkToolsConfiguration class
+            // Use the names of the discovered fields as the keyword search terms in Project Settings panel
+            var gdkToolsConfigProperties = typeof(GdkToolsConfiguration).GetFields(BindingFlags.Public | BindingFlags.Instance);
+
             provider.keywords = gdkToolsConfigProperties.Select(property => property.Name).ToList();
             return provider;
         }
