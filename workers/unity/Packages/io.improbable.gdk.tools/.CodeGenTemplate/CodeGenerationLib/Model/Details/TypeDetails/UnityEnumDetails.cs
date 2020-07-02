006fe3868d5e63c9c4815f3260ed07748eb82ea3
Bryan Wong <bryanwong@improbable.io>
2020-07-02 16:43:11 +0100

    no more sandwich

--- workers/unity/Packages/io.improbable.gdk.tools/.CodeGenTemplate/CodeGenerationLib/Model/Details/TypeDetails/UnityEnumDetails.cs
+++ workers/unity/Packages/io.improbable.gdk.tools/.CodeGenTemplate/CodeGenerationLib/Model/Details/TypeDetails/UnityEnumDetails.cs
@@ -17,13 +17,14 @@ namespace Improbable.Gdk.CodeGeneration.Model.Details
         {
             var min = rawEnumDefinition.Values.Select(list => list.Value).Min();
             EnumMinimums[FullyQualifiedName] = min;
-            Values = rawEnumDefinition.Values.Select(value => (value.Value - min, value.Name)).ToList();
             if (min != 0)
             {
                 Logger.Warn($"The enum, {Name}, is defined with a minimum value of {min}, which is greater than 0. Shifting the enum values to start from 0. " +
                     "This will lead to inconsistencies in the values used in Unity and the values captured in snapshots. " +
                     $"This inconsistency is handled in the serialization/deserialization process but please consider redefining the values in {Name} to start from 0");
             }
+
+            Values = rawEnumDefinition.Values.Select(value => (value.Value - min, value.Name)).ToList();
         }
 
         public static uint GetEnumMinimum(string fullQualifiedName)
