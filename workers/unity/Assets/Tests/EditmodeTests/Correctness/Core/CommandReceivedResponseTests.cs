e3caa70d95f6932c4cc835697338cbcf2f42fc46
Bryan Wong <bryanwong@improbable.io>
2020-07-29 15:02:59 +0100

    multiline tests

--- workers/unity/Assets/Tests/EditmodeTests/Correctness/Core/CommandReceivedResponseTests.cs
+++ workers/unity/Assets/Tests/EditmodeTests/Correctness/Core/CommandReceivedResponseTests.cs
@@ -15,9 +15,8 @@ namespace Improbable.Gdk.EditmodeTests.Core
         [Test]
         public void ReceivedResponse_isNull_when_Response_is_not_received()
         {
-<<<<<<< HEAD
+
             World.Step(world =>
-<<<<<<< HEAD
                 {
                     world.Connection.CreateEntity(EntityId, GetTemplate());
                 })
@@ -30,27 +29,13 @@ namespace Improbable.Gdk.EditmodeTests.Core
                     Assert.IsFalse(world.GetSystem<CommandSystem>()
                         .GetResponse<Launcher.LaunchEntity.ReceivedResponse>(id).HasValue);
                 });
-=======
-            {
-                world.Connection.CreateEntity(EntityId, GetTemplate());
-            }).Step(world => world.GetSystem<CommandSystem>().SendCommand(GetRequest())).Step((world, id) =>
-            {
-                Assert.IsFalse(world.GetSystem<CommandSystem>().GetResponse<Launcher.LaunchEntity.ReceivedResponse>(id).HasValue);
-            });
->>>>>>> Adjust access modifiers
-=======
-            World.Step(world => world.Connection.CreateEntity(EntityId, GetTemplate()))
-                .Step(world => world.GetSystem<CommandSystem>().SendCommand(GetRequest()))
-                .Step((world, id) => Assert.IsFalse(world.GetSystem<CommandSystem>().GetResponse<Launcher.LaunchEntity.ReceivedResponse>(id).HasValue));
->>>>>>> Next frame response
         }
 
         [Test]
         public void ReceivedResponse_isNotNull_when_Response_is_received()
         {
-<<<<<<< HEAD
             World.Step(world =>
-<<<<<<< HEAD
+
                 {
                     world.Connection.CreateEntity(EntityId, GetTemplate());
                 })
@@ -58,10 +43,6 @@ namespace Improbable.Gdk.EditmodeTests.Core
                 {
                     return world.GetSystem<CommandSystem>().SendCommand(GetRequest());
                 })
-=======
-            World.Step(world => world.Connection.CreateEntity(EntityId, GetTemplate()))
-                .Step(world => world.GetSystem<CommandSystem>().SendCommand(GetRequest()))
->>>>>>> Next frame response
                 .Step((world, id) =>
                 {
                     world.Connection
@@ -77,22 +58,6 @@ namespace Improbable.Gdk.EditmodeTests.Core
                     Assert.AreEqual(response.Value.EntityId, GetRequest().TargetEntityId);
                     Assert.AreEqual(response.Value.RequestPayload, GetRequest().Payload);
                 });
-<<<<<<< HEAD
-=======
-            {
-                world.Connection.CreateEntity(EntityId, GetTemplate());
-            }).Step(world =>
-            {
-                var id = world.GetSystem<CommandSystem>().SendCommand(GetRequest());
-                world.Connection.GenerateResponse<Launcher.LaunchEntity.Request, Launcher.LaunchEntity.ReceivedResponse>(id, ResponseGenerator);
-                return id;
-            }).Step((world, id) =>
-            {
-                Assert.IsTrue(world.GetSystem<CommandSystem>().GetResponse<Launcher.LaunchEntity.ReceivedResponse>(id).HasValue);
-            });
->>>>>>> Adjust access modifiers
-=======
->>>>>>> Next frame response
         }
 
         private static Launcher.LaunchEntity.ReceivedResponse ResponseGenerator(CommandRequestId id, Launcher.LaunchEntity.Request request)
