spatialos/gdk-for-unity

commit ae4c5fc750faa29223ee38409f87dd14da57f621

Jobify serialization (#1454)
* Serialize component updates in jobs

* Fix early job schedule due to combined dependencies
Filter on changed components to avoid scheduling jobs for unchanged chunks
Don't reuse queue, dispose and re-create

* Remove UnityComponentSenderGenerator and the ComponentSendSystem
Hack MessagesToSend to store pre-serialized component updates

* Add more profile markers

* Dispose Native collections in right places
Fixes unit tests

* For performance tests by changing profiling marks
Add DisableAutoCreate to ReplicationSystems

* Fix code smells

* Changelog

* Fix netstats for component updates
Rename AddComponentUpdate to AddComponentEvent

* Better profile markers

workers/unity/Packages/io.improbable.gdk.core/Worker/SerializedMessagesToSend.cs
@@ -91,6 +91,8 @@ namespace Improbable.Gdk.Core
 
         public void SerializeFrom(MessagesToSend messages, CommandMetaData commandMetaData)
         {
+            messages.GetSerializedComponentUpdates().CopyTo(updates);
+
             foreach (var serializer in commandSerializers)
             {
                 serializer.Serialize(messages, this, commandMetaData);
@@ -130,6 +132,7 @@ namespace Improbable.Gdk.Core
             {
                 ref readonly var update = ref updates[i];
                 connection.SendComponentUpdate(update.EntityId, update.Update, UpdateParams);
+                netFrameStats.AddUpdate(update.Update);
             }
 
             for (var i = 0; i < requests.Count; ++i)
@@ -201,10 +204,9 @@ namespace Improbable.Gdk.Core
             Clear();
         }
 
-        public void AddComponentUpdate(ComponentUpdate update, long entityId)
+        public void AddComponentEvent(ComponentUpdate update, long entityId)
         {
             updates.Add(new UpdateToSend(update, entityId));
-            netFrameStats.AddUpdate(update);
         }
 
         public void AddRequest(CommandRequest request, uint commandId, long entityId, uint? timeout, CommandRequestId requestId)
@@ -272,7 +274,7 @@ namespace Improbable.Gdk.Core
 
         #region Containers
 
-        private readonly struct UpdateToSend
+        public readonly struct UpdateToSend
         {
             public readonly ComponentUpdate Update;
             public readonly long EntityId;

workers/unity/Assets/Playground/Config/EntityTemplates.cs
@@ -69,7 +69,7 @@ namespace Playground
             var template = new EntityTemplate();
             template.AddComponent(new Position.Snapshot(location.ToCoordinates()), WorkerUtils.UnityGameLogic);
             template.AddComponent(new Metadata.Snapshot("Cube"), WorkerUtils.UnityGameLogic);
-            template.AddComponent(new Persistence.Snapshot(), WorkerUtils.UnityGameLogic);
+            template.AddComponent(new Persistence.Snapshot());
             template.AddComponent(new CubeColor.Snapshot(), WorkerUtils.UnityGameLogic);
             template.AddComponent(new CubeTargetVelocity.Snapshot(new Vector3f(-2.0f, 0, 0)),
                 WorkerUtils.UnityGameLogic);
@@ -99,7 +99,7 @@ namespace Playground
             template.AddComponent(new Position.Snapshot(coords), WorkerUtils.UnityGameLogic);
             template.AddComponent(new Metadata.Snapshot("Spinner"), WorkerUtils.UnityGameLogic);
             template.AddComponent(transform, WorkerUtils.UnityGameLogic);
-            template.AddComponent(new Persistence.Snapshot(), WorkerUtils.UnityGameLogic);
+            template.AddComponent(new Persistence.Snapshot());
             template.AddComponent(new Collisions.Snapshot(), WorkerUtils.UnityGameLogic);
             template.AddComponent(new SpinnerColor.Snapshot(Color.BLUE), WorkerUtils.UnityGameLogic);
             template.AddComponent(new SpinnerRotation.Snapshot(), WorkerUtils.UnityGameLogic);
@@ -121,9 +121,9 @@ namespace Playground
         public static EntityTemplate CreatePlayerSpawnerEntityTemplate(Coordinates playerSpawnerLocation)
         {
             var template = new EntityTemplate();
-            template.AddComponent(new Position.Snapshot(playerSpawnerLocation), WorkerUtils.UnityGameLogic);
-            template.AddComponent(new Metadata.Snapshot("PlayerCreator"), WorkerUtils.UnityGameLogic);
-            template.AddComponent(new Persistence.Snapshot(), WorkerUtils.UnityGameLogic);
+            template.AddComponent(new Position.Snapshot(playerSpawnerLocation));
+            template.AddComponent(new Metadata.Snapshot("PlayerCreator"));
+            template.AddComponent(new Persistence.Snapshot());
             template.AddComponent(new PlayerCreator.Snapshot(), WorkerUtils.UnityGameLogic);
 
             return template;

workers/unity/Assets/Tests/PlaymodeTests/Performance/ComponentUpdateTests.cs
@@ -26,11 +26,10 @@ namespace Improbable.Gdk.PlaymodeTests
         {
             var markers = new[]
             {
-                "GatherAllChunks",
-                "ExecuteReplication",
-                "Position.SendUpdates",
+                "SpatialOSSendSystem.CompletingAllJobs",
+                "SpatialOSSendSystem.QueueSerializedMessages",
                 "WorkerSystem.SendMessages",
-                "Position.ComponentSerializer.Serialize"
+                "PositionSerializationJob"
             };
 
             var currentState = World.Step(world =>

workers/unity/Packages/io.improbable.gdk.core/.codegen/Source/CoreCodegenJob.cs
@@ -52,7 +52,7 @@ namespace Improbable.Gdk.CodeGenerator.Core
             Logger.Trace("Adding job targets for components.");
             AddGenerators(componentsToGenerate,
                 c => ($"{c.Name}.cs", UnityComponentDataGenerator.Generate),
-                c => ($"{c.Name}UpdateSender.cs", UnityComponentSenderGenerator.Generate),
+                c => ($"{c.Name}ReplicationSystem.cs", UnityComponentReplicationSystemGenerator.Generate),
                 c => ($"{c.Name}EcsViewManager.cs", UnityEcsViewManagerGenerator.Generate),
                 c => ($"{c.Name}ComponentDiffStorage.cs", ComponentDiffStorageGenerator.Generate),
                 c => ($"{c.Name}ComponentDiffDeserializer.cs", ComponentDiffDeserializerGenerator.Generate),

workers/unity/Packages/io.improbable.gdk.core/.codegen/Source/Generators/Core/ComponentDiffDeserializerGenerator.cs
@@ -109,20 +109,7 @@ public uint GetComponentId()
                     {
                         m.ProfileScope("serializeMarker", s =>
                         {
-                            s.Line(@"
-var storage = messages.GetComponentDiffStorage(ComponentId);
-
-var updates = ((IDiffUpdateStorage<Update>) storage).GetUpdates();
-
-for (int i = 0; i < updates.Count; ++i)
-{
-    ref readonly var update = ref updates[i];
-    var schemaUpdate = SchemaComponentUpdate.Create();
-    var componentUpdate = new ComponentUpdate(ComponentId, schemaUpdate);
-    Serialization.SerializeUpdate(update.Update, schemaUpdate);
-    serializedMessages.AddComponentUpdate(componentUpdate, update.EntityId.Id);
-}
-");
+                            s.Line("var storage = messages.GetComponentDiffStorage(ComponentId);");
 
                             foreach (var ev in eventDetailsList)
                             {
@@ -138,7 +125,7 @@ for (int i = 0; i < events.Count; ++i)
     var componentUpdate = new ComponentUpdate(ComponentId, schemaUpdate);
     var obj = schemaUpdate.GetEvents().AddObject({ev.EventIndex});
     {ev.FqnPayloadType}.Serialization.Serialize(ev.Event.Payload, obj);
-    serializedMessages.AddComponentUpdate(componentUpdate, ev.EntityId.Id);
+    serializedMessages.AddComponentEvent(componentUpdate, ev.EntityId.Id);
 }}
 "
                                 });

workers/unity/Packages/io.improbable.gdk.core/.codegen/Source/Generators/Core/MetaclassGenerator.cs
@@ -40,7 +40,7 @@ public Type Authority {{ get; }} = typeof({rootNamespace}.HasAuthority);
 public Type Snapshot {{ get; }} = typeof({rootNamespace}.Snapshot);
 public Type Update {{ get; }} = typeof({rootNamespace}.Update);
 
-public Type ReplicationHandler {{ get; }} = typeof({rootNamespace}.ComponentReplicator);
+public Type ReplicationSystem {{ get; }} = typeof({rootNamespace}.ReplicationSystem);
 public Type Serializer {{ get; }} = typeof({rootNamespace}.ComponentSerializer);
 public Type DiffDeserializer {{ get; }} = typeof({rootNamespace}.DiffComponentDeserializer);
 

workers/unity/Packages/io.improbable.gdk.core/.codegen/Source/Generators/Core/UnityComponentReplicationSystemGenerator.cs
@@ -1,0 +1,98 @@
+using Improbable.Gdk.CodeGeneration.CodeWriter;
+using Improbable.Gdk.CodeGeneration.Model.Details;
+using NLog;
+
+namespace Improbable.Gdk.CodeGenerator
+{
+    public static class UnityComponentReplicationSystemGenerator
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        public static CodeWriter Generate(UnityComponentDetails componentDetails)
+        {
+            var componentNamespace = $"global::{componentDetails.Namespace}.{componentDetails.Name}";
+
+            Logger.Trace($"Generating {componentDetails.Namespace}.{componentDetails.Name}.ReplicationSystem internal class.");
+
+            return CodeWriter.Populate(cgw =>
+            {
+                cgw.UsingDirectives(
+                    "Improbable.Gdk.Core",
+                    "Improbable.Worker.CInterop",
+                    "Unity.Collections",
+                    "Unity.Entities",
+                    "Unity.Profiling"
+                );
+
+                cgw.Namespace(componentDetails.Namespace, ns =>
+                {
+                    ns.Type($"public partial class {componentDetails.Name}", partial =>
+                    {
+                        partial.Annotate("DisableAutoCreation, UpdateInGroup(typeof(SpatialOSSendGroup)), UpdateBefore(typeof(SpatialOSSendGroup.InternalSpatialOSSendGroup))")
+                            .Type("internal class ReplicationSystem : SystemBase", system =>
+                            {
+                                system.Line($@"
+private NativeQueue<SerializedMessagesToSend.UpdateToSend> dirtyComponents;
+private SpatialOSSendSystem spatialOsSendSystem;
+
+private ProfilerMarker foreachMarker = new ProfilerMarker(""{componentDetails.Name}SerializationJob"");
+
+protected override void OnCreate()
+{{
+    spatialOsSendSystem = World.GetExistingSystem<SpatialOSSendSystem>();
+}}
+");
+                                system.Method("protected override void OnUpdate()", m =>
+                                {
+                                    m.Line(new[]
+                                    {
+                                        "dirtyComponents = new NativeQueue<SerializedMessagesToSend.UpdateToSend>(Allocator.TempJob);",
+                                        "var dirtyComponentsWriter = dirtyComponents.AsParallelWriter();",
+                                        "var marker = foreachMarker;",
+                                    });
+
+                                    m.Line($@"
+Dependency = Entities.WithName(""{componentDetails.Name}Replication"")");
+                                    if (!componentDetails.IsBlittable)
+                                    {
+                                        m.Line(@"
+    .WithoutBurst()");
+                                    }
+
+                                    m.Line(@"
+    .WithAll<HasAuthority>()
+    .WithChangeFilter<Component>()
+    .ForEach((ref Component component, in SpatialEntityId entity) =>
+    {
+        marker.Begin();
+        if (!component.IsDataDirty())
+        {
+            marker.End();
+            return;
+        }
+
+        // Serialize component
+        var schemaUpdate = SchemaComponentUpdate.Create();
+        Serialization.SerializeUpdate(component, schemaUpdate);
+
+        component.MarkDataClean();
+
+        // Schedule update
+        var componentUpdate = new ComponentUpdate(ComponentId, schemaUpdate);
+        var update = new SerializedMessagesToSend.UpdateToSend(componentUpdate, entity.EntityId.Id);
+        dirtyComponentsWriter.Enqueue(update);
+        marker.End();
+    })
+    .ScheduleParallel(Dependency);
+
+spatialOsSendSystem.AddReplicationJobProducer(Dependency, dirtyComponents);
+dirtyComponents = default;
+");
+                                });
+                            });
+                    });
+                });
+            });
+        }
+    }
+}

workers/unity/Packages/io.improbable.gdk.core/.codegen/Source/Generators/Core/UnityComponentSenderGenerator.cs
@@ -1,105 +1,0 @@
-using Improbable.Gdk.CodeGeneration.CodeWriter;
-using Improbable.Gdk.CodeGeneration.Model.Details;
-using NLog;
-
-namespace Improbable.Gdk.CodeGenerator
-{
-    public static class UnityComponentSenderGenerator
-    {
-        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
-
-        public static CodeWriter Generate(UnityComponentDetails componentDetails)
-        {
-            var componentNamespace = $"global::{componentDetails.Namespace}.{componentDetails.Name}";
-
-            Logger.Trace($"Generating {componentDetails.Namespace}.{componentDetails.Name}.ComponentReplicator internal class.");
-
-            return CodeWriter.Populate(cgw =>
-            {
-                cgw.UsingDirectives(
-                    "Unity.Entities",
-                    "Unity.Collections",
-                    "Improbable.Gdk.Core",
-                    "Improbable.Gdk.Core.CodegenAdapters",
-                    "Unity.Profiling"
-                );
-
-                cgw.Namespace(componentDetails.Namespace, ns =>
-                {
-                    ns.Type($"public partial class {componentDetails.Name}", partial =>
-                    {
-                        partial.Type("internal class ComponentReplicator : IComponentReplicationHandler", replicator =>
-                        {
-                            replicator.Line($@"
-private ProfilerMarker componentMarker = new ProfilerMarker(""{componentDetails.Name}.SendUpdates"");
-
-public uint ComponentId => {componentDetails.ComponentId};
-
-public EntityQueryDesc ComponentUpdateQuery => new EntityQueryDesc
-{{
-    All = new[]
-    {{
-        ComponentType.ReadWrite<{componentNamespace}.Component>(),
-        ComponentType.ReadOnly<{componentNamespace}.HasAuthority>(),
-        ComponentType.ReadOnly<SpatialEntityId>()
-    }},
-}};
-");
-                            replicator.Method(@"
-public void SendUpdates(
-    NativeArray<ArchetypeChunk> chunkArray,
-    ComponentSystemBase system,
-    EntityManager entityManager,
-    ComponentUpdateSystem componentUpdateSystem)
-", m =>
-                            {
-                                m.ProfileScope("componentMarker", s =>
-                                {
-                                    s.Line(new[]
-                                    {
-                                        "var spatialOSEntityType = system.GetArchetypeChunkComponentType<SpatialEntityId>(true);",
-                                        $"var componentType = system.GetArchetypeChunkComponentType<{componentNamespace}.Component>();",
-                                    });
-
-                                    s.Loop("foreach (var chunk in chunkArray)", outerLoop =>
-                                    {
-                                        outerLoop.Line(new[]
-                                        {
-                                            "var entityIdArray = chunk.GetNativeArray(spatialOSEntityType);",
-                                            "var componentArray = chunk.GetNativeArray(componentType);",
-                                        });
-
-                                        outerLoop.Loop("for (var i = 0; i < componentArray.Length; i++)", innerLoop =>
-                                        {
-                                            innerLoop.Line("var data = componentArray[i];");
-
-                                            innerLoop.If("data.IsDataDirty()", componentDirtyThen =>
-                                            {
-                                                componentDirtyThen.Line("var update = new Update();");
-                                                for (var i = 0; i < componentDetails.FieldDetails.Count; i++)
-                                                {
-                                                    var fieldDetails = componentDetails.FieldDetails[i];
-                                                    componentDirtyThen.If($"data.IsDataDirty({i})", () => new[]
-                                                    {
-                                                        $"update.{fieldDetails.PascalCaseName} = data.{fieldDetails.PascalCaseName};"
-                                                    });
-                                                }
-
-                                                componentDirtyThen.Line(new[]
-                                                {
-                                                    "componentUpdateSystem.SendUpdate(in update, entityIdArray[i].EntityId);",
-                                                    "data.MarkDataClean();",
-                                                    "componentArray[i] = data;"
-                                                });
-                                            });
-                                        });
-                                    });
-                                });
-                            });
-                        });
-                    });
-                });
-            });
-        }
-    }
-}

commit 34c57a4218311f06481e43481f4b196958781731

fix regression introduced in #1454

workers/unity/Packages/io.improbable.gdk.core/Worker/SerializedMessagesToSend.cs
@@ -131,8 +131,8 @@ namespace Improbable.Gdk.Core
             for (var i = 0; i < updates.Count; ++i)
             {
                 ref readonly var update = ref updates[i];
-                connection.SendComponentUpdate(update.EntityId, update.Update, UpdateParams);
                 netFrameStats.AddUpdate(update.Update);
+                connection.SendComponentUpdate(update.EntityId, update.Update, UpdateParams);
             }
 
             for (var i = 0; i < requests.Count; ++i)