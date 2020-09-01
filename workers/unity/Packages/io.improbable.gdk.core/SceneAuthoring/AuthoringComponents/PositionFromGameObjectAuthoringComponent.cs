67f779effb05f8fc1de088eba528f72bb287edeb
Jamie Brynes <jamiebrynes7@gmail.com>
2020-09-01 11:52:42 +0100

    pragma disable 649

--- workers/unity/Packages/io.improbable.gdk.core/SceneAuthoring/AuthoringComponents/PositionFromGameObjectAuthoringComponent.cs
+++ workers/unity/Packages/io.improbable.gdk.core/SceneAuthoring/AuthoringComponents/PositionFromGameObjectAuthoringComponent.cs
@@ -5,7 +5,9 @@ namespace Improbable.Gdk.Core.SceneAuthoring.AuthoringComponents
     [AddComponentMenu("SpatialOS/Authoring Components/Position From GameObject Authoring Component")]
     public class PositionFromGameObjectAuthoringComponent : MonoBehaviour, ISpatialOsAuthoringComponent
     {
+#pragma warning disable 649
         [SerializeField] private string writeAccess;
+#pragma warning restore 649
 
         public void WriteTo(EntityTemplate template)
         {
