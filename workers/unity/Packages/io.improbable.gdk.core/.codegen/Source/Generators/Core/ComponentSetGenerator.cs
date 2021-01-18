ef7125cc7cafcc0c756c3de88bf861f7e7352f50
Jamie Brynes <jamiebrynes7@gmail.com>
2021-01-18 11:45:42 +0000

    tidy up component set generation

--- workers/unity/Packages/io.improbable.gdk.core/.codegen/Source/Generators/Core/ComponentSetGenerator.cs
+++ workers/unity/Packages/io.improbable.gdk.core/.codegen/Source/Generators/Core/ComponentSetGenerator.cs
@@ -26,10 +26,10 @@ namespace Improbable.Gdk.CodeGenerator
                         foreach (var set in componentSets)
                         {
                             tb.Line(
-                                $"public static readonly ComponentSet {set.Name} = new ComponentSet({set.ComponentSetId}, new uint[] {{ {string.Join(", ", set.ComponentIds.Select(id => id.ToString()))} }});");
+                                $"public static readonly ComponentSet {set.Name} = new ComponentSet({set.ComponentSetId}, new uint[] {{ {string.Join(", ", set.ComponentIdReferences)} }});");
                         }
 
-                        tb.Initializer("private static readonly IReadOnlyDictionary<uint, ComponentSet> keyedComponentSets = new Dictionary<uint, ComponentSet>", () => componentSets.Select(set => $"{{ {set.ComponentSetId}, {set.Name} }}"));
+                        tb.Initializer("private static readonly IReadOnlyDictionary<uint, ComponentSet> keyedComponentSets = new Dictionary<uint, ComponentSet>", () => componentSets.Select(set => $"{{ {set.Name}.ComponentSetId, {set.Name} }}"));
 
                         tb.Method("public bool TryGetComponentSet(uint componentSetId, out ComponentSet componentSet)",
                             mb =>
--- workers/unity/Packages/io.improbable.gdk.tools/.CodeGenTemplate/CodeGenerationLib/Model/Details/ComponentSetDetails.cs
+++ workers/unity/Packages/io.improbable.gdk.tools/.CodeGenTemplate/CodeGenerationLib/Model/Details/ComponentSetDetails.cs
@@ -5,13 +5,16 @@ namespace Improbable.Gdk.CodeGeneration.Model.Details
     public class ComponentSetDetails : Details
     {
         public uint ComponentSetId { get; }
-        public uint[] ComponentIds { get; }
+
+        public string[] ComponentIdReferences { get; }
 
         public ComponentSetDetails(ComponentSetDefinition componentSetDefinition, DetailsStore detailsStore) : base(componentSetDefinition)
         {
             ComponentSetId = componentSetDefinition.ComponentSetId;
-            ComponentIds = componentSetDefinition.ComponentList.Components
-                .Select(componentRef => detailsStore.Components[componentRef.Component].ComponentId).ToArray();
+
+            ComponentIdReferences = componentSetDefinition.ComponentList.Components
+                .Select(componentRef => $"{detailsStore.Components[componentRef.Component].FullyQualifiedName}.ComponentId")
+                .ToArray();
         }
     }
 }
