8c80463e5d44bd8de56c56c90f463a2e21011847
Jamie Brynes <jamiebrynes7@gmail.com>
2021-01-20 15:03:12 +0000

    spatial deployment manager requires runtime version

--- workers/unity/Packages/io.improbable.gdk.testutils/Editor/Tests/SpatialDeploymentManagerTests.cs
+++ workers/unity/Packages/io.improbable.gdk.testutils/Editor/Tests/SpatialDeploymentManagerTests.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Linq;
 using System.Net;
+using Improbable.Gdk.Tools;
 using NUnit.Framework;
 
 namespace Improbable.Gdk.TestUtils.Editor.Tests
@@ -9,10 +10,12 @@ namespace Improbable.Gdk.TestUtils.Editor.Tests
     [TestFixture]
     public class SpatialDeploymentManagerTests
     {
+        private static string RuntimeVersion = GdkToolsConfiguration.GetOrCreateInstance().RuntimeVersion;
+
         [Test]
         public void Start_throws_an_exception_if_deployment_config_does_not_exist()
         {
-            var task = SpatialDeploymentManager.Start("does_not_exist.json", "snapshots/default.snapshot");
+            var task = SpatialDeploymentManager.Start("does_not_exist.json", "snapshots/default.snapshot", RuntimeVersion);
             Assert.IsTrue(task.IsFaulted);
             Assert.IsInstanceOf<ArgumentException>(task.Exception.InnerExceptions.First());
         }
@@ -20,7 +23,7 @@ namespace Improbable.Gdk.TestUtils.Editor.Tests
         [Test]
         public void Start_throws_an_exception_if_snapshot_does_not_exist()
         {
-            var task = SpatialDeploymentManager.Start("default_launch.json", "does_not_exist.snapshot");
+            var task = SpatialDeploymentManager.Start("default_launch.json", "does_not_exist.snapshot", RuntimeVersion);
             Assert.IsTrue(task.IsFaulted);
             Assert.IsInstanceOf<ArgumentException>(task.Exception.InnerExceptions.First());
         }
@@ -28,7 +31,7 @@ namespace Improbable.Gdk.TestUtils.Editor.Tests
         [Test]
         public void Start_correctly_starts_a_spatial_deployment()
         {
-            using (SpatialDeploymentManager.Start("default_launch.json", "snapshots/default.snapshot").Result)
+            using (SpatialDeploymentManager.Start("default_launch.json", "snapshots/default.snapshot", RuntimeVersion).Result)
             {
                 var request = WebRequest.Create("http://localhost:21000/inspector");
                 Assert.DoesNotThrow(() => request.GetResponse());
@@ -38,7 +41,7 @@ namespace Improbable.Gdk.TestUtils.Editor.Tests
         [Test]
         public void Deployment_is_dead_after_Dispose()
         {
-            using (SpatialDeploymentManager.Start("default_launch.json", "snapshots/default.snapshot").Result)
+            using (SpatialDeploymentManager.Start("default_launch.json", "snapshots/default.snapshot", RuntimeVersion).Result)
             {
             }
 
@@ -49,9 +52,9 @@ namespace Improbable.Gdk.TestUtils.Editor.Tests
         [Test]
         public void Start_throws_if_deployment_fails_to_start()
         {
-            using (SpatialDeploymentManager.Start("default_launch.json", "snapshots/default.snapshot").Result)
+            using (SpatialDeploymentManager.Start("default_launch.json", "snapshots/default.snapshot", RuntimeVersion).Result)
             {
-                var task = SpatialDeploymentManager.Start("default_launch.json", "snapshots/default.snapshot");
+                var task = SpatialDeploymentManager.Start("default_launch.json", "snapshots/default.snapshot", RuntimeVersion);
                 Assert.Throws<AggregateException>(() => task.Wait());
             }
         }
