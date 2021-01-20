9bfea87faffa7f35854b2f9b77e981a6fa56566e
Jamie Brynes <jamiebrynes7@gmail.com>
2021-01-20 14:38:18 +0000

    update EcsViewManager to handle new semantics from ops

--- workers/unity/Packages/io.improbable.gdk.core/UpdatesAndEvents/ComponentManagers.cs
+++ workers/unity/Packages/io.improbable.gdk.core/UpdatesAndEvents/ComponentManagers.cs
@@ -9,10 +9,131 @@ namespace Improbable.Gdk.Core
         void Init(World world);
         void Clean();
 
-        uint GetComponentId();
-
-        ComponentType[] GetInitialComponents();
-
         void ApplyDiff(ViewDiff diff);
+    }
+
+    public abstract class EcsViewManager<TComponent, TUpdate, TAuthority> : IEcsViewManager
+        where TComponent : struct, ISpatialComponentData, IComponentData
+        where TUpdate : struct, ISpatialComponentUpdate
+        where TAuthority : struct, IComponentData
+    {
+        protected EntityManager EntityManager;
+        private WorkerSystem workerSystem;
+        private SpatialOSReceiveSystem spatialOSReceiveSystem;
+        private readonly uint componentId;
+
+        protected EcsViewManager()
+        {
+            componentId = ComponentDatabase.GetComponentId<TComponent>();
+        }
+
+        public void Init(World world)
+        {
+            EntityManager = world.EntityManager;
+            workerSystem = world.GetExistingSystem<WorkerSystem>();
+
+            if (workerSystem == null)
+            {
+                throw new ArgumentException("World instance is not running a valid SpatialOS worker");
+            }
+
+            spatialOSReceiveSystem = world.GetExistingSystem<SpatialOSReceiveSystem>();
+
+            if (spatialOSReceiveSystem == null)
+            {
+                throw new ArgumentException("Could not find SpatialOS Receive System in the current world instance");
+            }
+        }
+
+        public virtual void Clean()
+        {
+        }
+
+        public void ApplyDiff(ViewDiff diff)
+        {
+            var diffStorage = (DiffComponentStorage<TUpdate>) diff.GetComponentDiffStorage(componentId);
+
+            foreach (var entityId in diffStorage.GetComponentsAdded())
+            {
+                AddComponent(entityId);
+            }
+
+            var updates = diffStorage.GetUpdates();
+            var dataFromEntity = spatialOSReceiveSystem.GetComponentDataFromEntity<TComponent>();
+            for (var i = 0; i < updates.Count; ++i)
+            {
+                ref readonly var update = ref updates[i];
+
+                if (!workerSystem.TryGetEntity(update.EntityId, out var entity) || !dataFromEntity.HasComponent(entity))
+                {
+                    return;
+                }
+
+                var data = dataFromEntity[entity];
+                ApplyUpdate(ref data, in update.Update);
+                dataFromEntity[entity] = data;
+            }
+
+            var authChanges = diffStorage.GetAuthorityChanges();
+            for (var i = 0; i < authChanges.Count; ++i)
+            {
+                ref readonly var change = ref authChanges[i];
+                SetAuthority(change.EntityId, change.Authority);
+            }
+
+            foreach (var entityId in diffStorage.GetComponentsRemoved())
+            {
+                RemoveComponent(entityId);
+            }
+        }
+
+        private void AddComponent(EntityId entityId)
+        {
+            if (!workerSystem.TryGetEntity(entityId, out var entity) || EntityManager.HasComponent<TComponent>(entity))
+            {
+                return;
+            }
+
+            EntityManager.AddComponentData(entity, CreateEmptyComponent());
+        }
+
+        private void SetAuthority(EntityId entityId, Authority authority)
+        {
+            if (!workerSystem.TryGetEntity(entityId, out var entity))
+            {
+                return;
+            }
+
+            switch (authority)
+            {
+                case Authority.Authoritative:
+                    EntityManager.AddComponent<TAuthority>(entity);
+                    break;
+                case Authority.NotAuthoritative:
+                    EntityManager.RemoveComponent<TAuthority>(entity);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(authority), authority, null);
+            }
+        }
+
+        private void RemoveComponent(EntityId entityId)
+        {
+            if (!workerSystem.TryGetEntity(entityId, out var entity) || !EntityManager.HasComponent<TComponent>(entity))
+            {
+                return;
+            }
+
+            var data = EntityManager.GetComponentData<TComponent>(entity);
+            DisposeData(data);
+            EntityManager.RemoveComponent<TComponent>(entity);
+        }
+
+        protected virtual void DisposeData(TComponent data)
+        {
+        }
+
+        protected abstract TComponent CreateEmptyComponent();
+        protected abstract void ApplyUpdate(ref TComponent data, in TUpdate update);
     }
 }
