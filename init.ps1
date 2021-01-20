9e786d8023f0a92c7090efd9eb512bec8fa338db
Jamie Brynes <jamiebrynes7@gmail.com>
2021-01-20 14:17:27 +0000

    update SDK & Runtime versions

--- init.ps1
+++ init.ps1
@@ -40,7 +40,7 @@ function UpdateSpot($identifier, $path)
     spatial package get spot $identifier $SpotVersion "$path" --force --json_output $EnvironmentArgs
 }
 
-UpdatePackage worker_sdk c-dynamic-x86_64-gcc510-linux "$SdkPath/Plugins/Improbable/Core/Linux/x86_64"
+UpdatePackage worker_sdk c-dynamic-x86_64-clang1000-linux "$SdkPath/Plugins/Improbable/Core/Linux/x86_64"
 UpdatePackage worker_sdk c-bundle-x86_64-clang-macos "$SdkPath/Plugins/Improbable/Core/OSX"
 UpdatePackage worker_sdk c-dynamic-x86_64-vc141_mt-win32 "$SdkPath/Plugins/Improbable/Core/Windows/x86_64" "improbable_worker.lib"
 
@@ -59,7 +59,7 @@ UpdateSpot spot-macos "$SdkPath/.spot/spot"
 UpdatePackage worker_sdk c-static-arm-clang-ios "$SdkMobilePath/Plugins/Improbable/Core/iOS/arm"
 UpdatePackage worker_sdk c-static-x86_64-clang-ios "$SdkMobilePath/Plugins/Improbable/Core/iOS/x86_64"
 
-UpdatePackage worker_sdk c-dynamic-arm64v8a-clang_ndk21-android "$SdkMobilePath/Plugins/Improbable/Core/Android/arm64"
-UpdatePackage worker_sdk c-dynamic-armv7a-clang_ndk21-android "$SdkMobilePath/Plugins/Improbable/Core/Android/armv7"
+UpdatePackage worker_sdk c-dynamic-arm64v8a-clang_ndk21d-android "$SdkMobilePath/Plugins/Improbable/Core/Android/arm64"
+UpdatePackage worker_sdk c-dynamic-armv7a-clang_ndk21d-android "$SdkMobilePath/Plugins/Improbable/Core/Android/armv7"
 
 UpdatePackage worker_sdk csharp_cinterop_static "$SdkMobilePath/Plugins/Improbable/Sdk/iOS"
